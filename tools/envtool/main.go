c01b89997cf0f0fa3bd1e8017bf938ded441de58
MangoDB Developer <mangodb.developer@mangodb.io>
2021-11-25 11:07:20 +0300

    Add a hack for fast computers.
    Refs #92.

--- tools/envtool/main.go
+++ tools/envtool/main.go
@@ -72,6 +72,10 @@ func waitForPort(ctx context.Context, port uint16) error {
 		conn, err := net.Dial("tcp", fmt.Sprintf("127.0.0.1:%d", port))
 		if err == nil {
 			conn.Close()
+
+			// FIXME https://github.com/MangoDB-io/MangoDB/issues/92
+			time.Sleep(time.Second)
+
 			return nil
 		}
 
