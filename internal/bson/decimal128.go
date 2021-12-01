4ce98ec248aeb7d1a7931b1ad45f74c80b60a6db
OpenSauce <lawleycobhc@gmail.com>
2021-12-01 20:19:19 +0000

    Beginning to look into Decimal128

---
+++ internal/bson/decimal128.go
@@ -0,0 +1,100 @@
+// Copyright 2021 FerretDB Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package bson
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/binary"
+	"encoding/json"
+
+	"github.com/FerretDB/FerretDB/internal/util/lazyerrors"
+)
+
+// Decimal128 represents BSON Decimal128 data type.
+type Decimal128 uint64
+
+func (d *Decimal128) bsontype() {}
+
+// ReadFrom implements bsontype interface.
+func (d *Decimal128) ReadFrom(r *bufio.Reader) error {
+	if err := binary.Read(r, binary.LittleEndian, d); err != nil {
+		return lazyerrors.Errorf("bson.Decimal128.ReadFrom (binary.Read): %w", err)
+	}
+
+	return nil
+}
+
+// WriteTo implements bsontype interface.
+func (d Decimal128) WriteTo(w *bufio.Writer) error {
+	v, err := d.MarshalBinary()
+	if err != nil {
+		return lazyerrors.Errorf("bson.Decimal128.WriteTo: %w", err)
+	}
+
+	_, err = w.Write(v)
+	if err != nil {
+		return lazyerrors.Errorf("bson.Decimal128.WriteTo: %w", err)
+	}
+
+	return nil
+}
+
+// MarshalBinary implements bsontype interface.
+func (d Decimal128) MarshalBinary() ([]byte, error) {
+	var buf bytes.Buffer
+
+	binary.Write(&buf, binary.LittleEndian, d)
+
+	return buf.Bytes(), nil
+}
+
+type decimal128JSON struct {
+	D uint64 `json:"$numberDecimal,string"`
+}
+
+// UnmarshalJSON implements bsontype interface.
+func (d *Decimal128) UnmarshalJSON(data []byte) error {
+	if bytes.Equal(data, []byte("null")) {
+		panic("null data")
+	}
+
+	r := bytes.NewReader(data)
+	dec := json.NewDecoder(r)
+	dec.DisallowUnknownFields()
+
+	var o decimal128JSON
+	if err := dec.Decode(&o); err != nil {
+		return err
+	}
+	if err := checkConsumed(dec, r); err != nil {
+		return lazyerrors.Errorf("bson.Decimal128.UnmarshalJSON: %s", err)
+	}
+
+	*d = Decimal128(o.D)
+	return nil
+}
+
+// MarshalJSON implements bsontype interface.
+func (d Decimal128) MarshalJSON() ([]byte, error) {
+	return json.Marshal(decimal128JSON{
+		D: uint64(d),
+	})
+}
+
+// check interfaces
+var (
+	_ bsontype = (*Decimal128)(nil)
+)
---
+++ internal/bson/decimal128_test.go
@@ -0,0 +1,59 @@
+// Copyright 2021 FerretDB Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package bson
+
+import (
+	"testing"
+
+	"github.com/AlekSi/pointer"
+)
+
+var decimal128TestCases = []testCase{{
+	name: "100.50",
+	v:    pointer.To(Decimal128(100.5)),
+	b:    []byte{0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+	j:    `{"$numberDecimal":"100.5"}`,
+}, {
+	name: "0",
+	v:    pointer.To(Decimal128(0)),
+	b:    []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+	j:    `{"$numberDecimal":"0"}`,
+}}
+
+func TestDecimal128(t *testing.T) {
+	t.Parallel()
+
+	t.Run("Binary", func(t *testing.T) {
+		t.Parallel()
+		testBinary(t, decimal128TestCases, func() bsontype { return new(Decimal128) })
+	})
+
+	t.Run("JSON", func(t *testing.T) {
+		t.Parallel()
+		testJSON(t, decimal128TestCases, func() bsontype { return new(Decimal128) })
+	})
+}
+
+func FuzzDecimal128Binary(f *testing.F) {
+	fuzzBinary(f, decimal128TestCases, func() bsontype { return new(Decimal128) })
+}
+
+func FuzzDecimal128JSON(f *testing.F) {
+	fuzzJSON(f, decimal128TestCases, func() bsontype { return new(Decimal128) })
+}
+
+func BenchmarkDecimal128(b *testing.B) {
+	benchmark(b, decimal128TestCases, func() bsontype { return new(Decimal128) })
+}
