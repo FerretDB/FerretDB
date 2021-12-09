98963dcdd0d5d936fcbfcfe66c456fbef77e466d
OpenSauce <lawleycobhc@gmail.com>
2021-12-09 20:50:12 +0000

    Working base

--- internal/handlers/handler_test.go
+++ internal/handlers/handler_test.go
@@ -16,6 +16,7 @@ package handlers
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 	"time"
 
@@ -705,102 +706,127 @@ func TestDelete(t *testing.T) {
 		Metrics:       NewMetrics(),
 	})
 
-	type testCase struct {
-		req  types.Document
-		resp types.Document
+	reqHeader := wire.MsgHeader{
+		RequestID: 1,
+		OpCode:    wire.OP_MSG,
 	}
-	testCases := map[string]testCase{
-		"NothingToDelete": {
-			req: types.MustMakeDocument(
-				"delete", "fruit",
-			),
-			resp: types.MustMakeDocument(
-				"n", int32(0),
-				"ok", float64(1),
-			),
-		},
-		"DeleteLimit1": {
-			req: types.MustMakeDocument(
-				"delete", "car",
-				"deletes", types.Array{
-					types.MustMakeDocument(
-						"q", types.MustMakeDocument(
-							"car", "ford",
+
+	for _, schema := range []string{"monila", "pagila"} {
+		t.Run(schema, func(t *testing.T) {
+			var msg wire.OpMsg
+			err := msg.SetSections(wire.OpMsgSection{
+				Documents: []types.Document{types.MustMakeDocument(
+					"insert", "testcar",
+					"documents", types.Array{
+						types.MustMakeDocument(
+							"_id", types.ObjectID{byte(1)},
+							"car", "ford"+strconv.Itoa(1),
+							"colour", "red",
 						),
-						"limit", int32(1),
-					),
-				},
-			),
-			resp: types.MustMakeDocument(
-				"n", int32(1),
-				"ok", float64(1),
-			),
-		},
-		"DeleteLimit0": {
-			req: types.MustMakeDocument(
-				"delete", "animal",
-				"deletes", types.Array{
-					types.MustMakeDocument(
-						"q", types.MustMakeDocument(
-							"animal", "test",
-						),
-						"limit", int32(0),
-					),
-				},
-			),
-			resp: types.MustMakeDocument(
-				"n", int32(5),
-				"ok", float64(1),
-			),
-		},
-	}
-
-	_, err := pool.Exec(ctx, fmt.Sprintf(
-		`CREATE TABLE %s (_jsonb jsonb)`,
-		pgx.Identifier{"pagila", "car"}.Sanitize(),
-	))
-
-	for i := 0; i < 2; i++ {
-		_, err = pool.Exec(ctx, fmt.Sprintf(`INSERT INTO pagila.car(_jsonb) VALUES ('{"car": "ford"}')`))
-		require.NoError(t, err)
-	}
-
-	_, err = pool.Exec(ctx, fmt.Sprintf(
-		`CREATE TABLE %s (_jsonb jsonb)`,
-		pgx.Identifier{"pagila", "animal"}.Sanitize(),
-	))
-
-	for i := 0; i < 5; i++ {
-		_, err = pool.Exec(ctx, fmt.Sprintf(`INSERT INTO pagila.animal(_jsonb) VALUES ('{"animal": "test"}')`))
-		require.NoError(t, err)
-	}
-
-	for name, tc := range testCases { //nolint:paralleltest // false positive
-		tc := tc
-		t.Run(name, func(t *testing.T) {
-			t.Parallel()
-			tc.req.Set("$db", "pagila")
-
-			reqHeader := wire.MsgHeader{
-				RequestID: 1,
-				OpCode:    wire.OP_MSG,
-			}
-
-			var reqMsg wire.OpMsg
-			err := reqMsg.SetSections(wire.OpMsgSection{
-				Documents: []types.Document{tc.req},
+					},
+					"$db", schema,
+				)},
 			})
 			require.NoError(t, err)
 
-			_, resBody, closeConn := handler.Handle(ctx, &reqHeader, &reqMsg)
+			_, _, closeConn := handler.Handle(ctx, &reqHeader, &msg)
 			require.False(t, closeConn)
 
-			actual, err := resBody.(*wire.OpMsg).Document()
-			require.NoError(t, err)
+			for i := 1; i <= 5; i++ {
+				var msg wire.OpMsg
+				err := msg.SetSections(wire.OpMsgSection{
+					Documents: []types.Document{types.MustMakeDocument(
+						"insert", "testcat",
+						"documents", types.Array{
+							types.MustMakeDocument(
+								"_id", types.ObjectID{byte(i)},
+								"animal", "cat",
+								"name", "test"+strconv.Itoa(i),
+							),
+						},
+						"$db", schema,
+					)},
+				})
+				require.NoError(t, err)
 
-			expected := tc.resp
-			assert.Equal(t, expected, actual)
+				_, _, closeConn := handler.Handle(ctx, &reqHeader, &msg)
+				require.False(t, closeConn)
+			}
+
+			type testCase struct {
+				req  types.Document
+				resp types.Document
+			}
+
+			testCases := map[string]testCase{
+				"NothingToDelete": {
+					req: types.MustMakeDocument(
+						"delete", "test",
+					),
+					resp: types.MustMakeDocument(
+						"n", int32(0),
+						"ok", float64(1),
+					),
+				},
+				"DeleteLimit1": {
+					req: types.MustMakeDocument(
+						"delete", "testcar",
+						"deletes", types.Array{
+							types.MustMakeDocument(
+								"q", types.MustMakeDocument(
+									"colour", "red",
+								),
+								"limit", int32(1),
+							),
+						},
+					),
+					resp: types.MustMakeDocument(
+						"n", int32(1),
+						"ok", float64(1),
+					),
+				},
+				"DeleteLimit0Cat": {
+					req: types.MustMakeDocument(
+						"delete", "testcat",
+						"deletes", types.Array{
+							types.MustMakeDocument(
+								"q", types.MustMakeDocument(
+									"animal", "cat",
+								),
+								"limit", int32(0),
+							),
+						},
+					),
+					resp: types.MustMakeDocument(
+						"n", int32(5),
+						"ok", float64(1),
+					),
+				},
+			}
+
+			for name, tc := range testCases {
+				tc := tc
+				t.Run(name, func(t *testing.T) {
+					t.Parallel()
+					tc.req.Set("$db", schema)
+
+					var reqMsg wire.OpMsg
+					err := reqMsg.SetSections(wire.OpMsgSection{
+						Documents: []types.Document{tc.req},
+					})
+					require.NoError(t, err)
+
+					_, resBody, closeConn := handler.Handle(ctx, &reqHeader, &reqMsg)
+					require.False(t, closeConn)
+
+					actual, err := resBody.(*wire.OpMsg).Document()
+					require.NoError(t, err)
+
+					expected := tc.resp
+					assert.Equal(t, expected, actual)
+				})
+
+			}
 		})
-
 	}
 }
