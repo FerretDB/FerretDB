4a06d8ac3c4c6b04d79072ff99f5a2d3be43ca70
Eugene Kalinin <ekalinin@users.noreply.github.com>
2021-12-26 19:28:24 +0300

    Fix GetLog & add missed test for it (#211)
    Fixes #210.

--- internal/handlers/shared/msg_getlog.go
+++ internal/handlers/shared/msg_getlog.go
@@ -75,7 +75,7 @@ func (h *Handler) MsgGetLog(ctx context.Context, msg *wire.OpMsg) (*wire.OpMsg, 
 	err = reply.SetSections(wire.OpMsgSection{
 		Documents: []types.Document{types.MustMakeDocument(
 			"totalLinesWritten", int32(log.Len()),
-			"log", log,
+			"log", &log,
 			"ok", float64(1),
 		)},
 	})
