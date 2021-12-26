d9e199f9d63ef29449ff362c9a6a57806a58ceca
Alexey Palazhchenko <alexey.palazhchenko@ferretdb.io>
2021-12-26 09:00:49 +0300

    WIP

---
+++ internal/fjson/cstring_test.go
@@ -0,0 +1,44 @@
+// Copyright 2021 FerretDB Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package fjson
+
+import (
+	"testing"
+
+	"github.com/AlekSi/pointer"
+)
+
+var cstringTestCases = []testCase{{
+	name: "foo",
+	v:    pointer.To(CString("foo")),
+	j:    `{"$c":"foo"}`,
+}, {
+	name: "empty",
+	v:    pointer.To(CString("")),
+	j:    `{"$c":""}`,
+}}
+
+func TestCString(t *testing.T) {
+	t.Parallel()
+	testJSON(t, cstringTestCases, func() fjsontype { return new(CString) })
+}
+
+func FuzzCStringJSON(f *testing.F) {
+	fuzzJSON(f, cstringTestCases, func() fjsontype { return new(CString) })
+}
+
+func BenchmarkCString(b *testing.B) {
+	benchmark(b, cstringTestCases, func() fjsontype { return new(CString) })
+}
---
+++ internal/fjson/regex_test.go
@@ -0,0 +1,44 @@
+// Copyright 2021 FerretDB Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package fjson
+
+import (
+	"testing"
+
+	"github.com/AlekSi/pointer"
+)
+
+var regexTestCases = []testCase{{
+	name: "normal",
+	v:    pointer.To(Regex{Pattern: "hoffman", Options: "i"}),
+	j:    `{"$r":"hoffman","o":"i"}`,
+}, {
+	name: "empty",
+	v:    pointer.To(Regex{Pattern: "", Options: ""}),
+	j:    `{"$r":"","o":""}`,
+}}
+
+func TestRegex(t *testing.T) {
+	t.Parallel()
+	testJSON(t, regexTestCases, func() fjsontype { return new(Regex) })
+}
+
+func FuzzRegex(f *testing.F) {
+	fuzzJSON(f, regexTestCases, func() fjsontype { return new(Regex) })
+}
+
+func BenchmarkRegex(b *testing.B) {
+	benchmark(b, regexTestCases, func() fjsontype { return new(Regex) })
+}
