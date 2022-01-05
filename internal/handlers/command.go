18803c9a257f80d9582840e4ac0c39482227526a
OpenSauce <lawleycobhc@gmail.com>
2022-01-05 22:11:04 +0000

    Added command help text

--- internal/handlers/command.go
+++ internal/handlers/command.go
@@ -34,128 +34,128 @@ type command struct {
 var commands = map[string]command{
 	"buildinfo": {
 		name:    "buildinfo",
-		help:    "",
+		help:    "Returns a summary of the build information.",
 		handler: (*Handler).MsgBuildInfo,
 	},
 	"collstats": {
 		name:    "collstats",
-		help:    "",
+		help:    "Storage data for a collection.",
 		handler: (*Handler).MsgCollStats,
 	},
 	"createindexes": {
 		name:           "createindexes",
-		help:           "",
+		help:           "Creates indexes on a collection.",
 		storageHandler: (common.Storage).MsgCreateIndexes,
 	},
 	"create": {
 		name:    "create",
-		help:    "",
+		help:    "Creates the collection.",
 		handler: (*Handler).MsgCreate,
 	},
 	"drop": {
 		name:    "drop",
-		help:    "",
+		help:    "Drops the collection.",
 		handler: (*Handler).MsgDrop,
 	},
 	"dropdatabase": {
 		name:    "dropdatabase",
-		help:    "",
+		help:    "Deletes the database.",
 		handler: (*Handler).MsgDropDatabase,
 	},
 	"getcmdlineopts": {
 		name:    "getcmdlineopts",
-		help:    "",
+		help:    "Returns a summary of all runtime and configuration options.",
 		handler: (*Handler).MsgGetCmdLineOpts,
 	},
 	"getlog": {
 		name:    "getlog",
-		help:    "",
+		help:    "Returns the most recent logged events from memory.",
 		handler: (*Handler).MsgGetLog,
 	},
 	"getparameter": {
 		name:    "getparameter",
-		help:    "",
+		help:    "Returns the value of the parameter.",
 		handler: (*Handler).MsgGetParameter,
 	},
 	"hostinfo": {
 		name:    "hostInfo",
-		help:    "",
+		help:    "Returns a summary of the system information.",
 		handler: (*Handler).MsgHostInfo,
 	},
 	"ismaster": {
 		name:    "ismaster",
-		help:    "",
+		help:    "Returns the role of the FerretDB instance.",
 		handler: (*Handler).MsgHello,
 	},
 	"hello": {
 		name:    "hello",
-		help:    "",
+		help:    "Returns the role of the FerretDB instance.",
 		handler: (*Handler).MsgHello,
 	},
 	"listcollections": {
 		name:    "listcollections",
-		help:    "",
+		help:    "Returns the information of the collections and views in the database.",
 		handler: (*Handler).MsgListCollections,
 	},
 	"listdatabases": {
 		name:    "listdatabases",
-		help:    "",
+		help:    "Returns a summary of all the databases.",
 		handler: (*Handler).MsgListDatabases,
 	},
 	"listcommands": {
 		name: "listcommands",
-		help: "",
+		help: "Returns information about the currently supported commands.",
 	},
 	"ping": {
-		name:    "listcommands",
-		help:    "",
+		name:    "ping",
+		help:    "Returns a pong response. Used for testing purposes.",
 		handler: (*Handler).MsgPing,
 	},
 	"whatsmyuri": {
 		name:    "whatsmyuri",
-		help:    "",
+		help:    "An internal command.",
 		handler: (*Handler).MsgWhatsMyURI,
 	},
 	"serverstatus": {
 		name:    "serverstatus",
-		help:    "",
+		help:    "Returns an overview of the databases state.",
 		handler: (*Handler).MsgServerStatus,
 	},
 	"delete": {
 		name:           "delete",
-		help:           "",
+		help:           "Deletes documents matched by the query.",
 		storageHandler: (common.Storage).MsgDelete,
 	},
 	"find": {
 		name:           "find",
-		help:           "",
+		help:           "Returns documents matched by the custom query.",
 		storageHandler: (common.Storage).MsgFindOrCount,
 	},
 	"count": {
 		name:           "count",
-		help:           "",
+		help:           "Returns the count of documents that's matched by the query.",
 		storageHandler: (common.Storage).MsgFindOrCount,
 	},
 	"insert": {
 		name:           "insert",
-		help:           "",
+		help:           "Inserts documents into the database. ",
 		storageHandler: (common.Storage).MsgInsert,
 	},
 	"update": {
 		name:           "update",
-		help:           "",
+		help:           "Updates documents that are matched by the query.",
 		storageHandler: (common.Storage).MsgUpdate,
 	},
 	"debug_error": {
 		name: "debug_error",
-		help: "",
+		help: "Used for debugging purposes.",
 		handler: func(*Handler, context.Context, *wire.OpMsg) (*wire.OpMsg, error) {
 			return nil, errors.New("debug_error")
 		},
 	},
 	"debug_panic": {
 		name: "debug_panic",
-		help: "",
+		help: "Used for debugging purposes.",
 		handler: func(*Handler, context.Context, *wire.OpMsg) (*wire.OpMsg, error) {
 			panic("debug_panic")
 		},
@@ -164,20 +164,19 @@ var commands = map[string]command{
 
 func SupportedCommands(context.Context, *wire.OpMsg) (*wire.OpMsg, error) {
 	var reply wire.OpMsg
+
+	commandList := types.MakeArray(len(commands))
+	for _, v := range commands {
+		commandList.Append(types.MustMakeDocument(
+			v.name, types.MustMakeDocument(
+				"help", v.help,
+			),
+		))
+	}
+
 	err := reply.SetSections(wire.OpMsgSection{
 		Documents: []types.Document{types.MustMakeDocument(
-			"commands", func() []types.Document {
-				var commandList []types.Document
-				for _, v := range commands {
-					commandList = append(commandList, types.MustMakeDocument(
-						v.name, types.MustMakeDocument(
-							"help", v.help,
-						),
-					))
-				}
-
-				return commandList
-			},
+			"commands", commandList,
 			"ok", float64(1),
 		)},
 	})
