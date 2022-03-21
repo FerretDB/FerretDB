3bd4eddd2e86779cb2f76a2d37fe1ed81cb1c85e
Ekaterina Blech <blech.kate@ya.ru>
2022-03-21 11:09:44 +0200

    remove sort revert

--- internal/handlers/jsonb1/msg_count.go
+++ internal/handlers/jsonb1/msg_count.go
@@ -99,7 +99,9 @@ func (s *storage) MsgCount(ctx context.Context, msg *wire.OpMsg) (*wire.OpMsg, e
 
 		resDocs = append(resDocs, doc)
 	}
-
+	if err = common.SortDocuments(resDocs, sort); err != nil {
+		return nil, err
+	}
 	if resDocs, err = common.LimitDocuments(resDocs, limit); err != nil {
 		return nil, err
 	}
