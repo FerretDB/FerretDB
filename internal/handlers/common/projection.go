b6643c4967813c41ae9d052b518d9a87b067bd2a
Ekaterina Blech <blech.kate@ya.ru>
2022-03-21 11:06:57 +0200

    lint

--- internal/handlers/common/projection.go
+++ internal/handlers/common/projection.go
@@ -14,9 +14,7 @@
 
 package common
 
-import (
-	"github.com/FerretDB/FerretDB/internal/types"
-)
+import "github.com/FerretDB/FerretDB/internal/types"
 
 // ProjectDocuments modifies given documents in places according to the given projection.
 func ProjectDocuments(docs []*types.Document, projection *types.Document) error {
