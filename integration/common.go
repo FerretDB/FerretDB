ac4701a901ddef562bc5382ac9a995272457c638
Alexey Palazhchenko <alexey.palazhchenko@ferretdb.io>
2022-03-24 19:47:31 +0300

    WIP

--- integration/common.go
+++ integration/common.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package integration provides FerretDB integration tests.
 package integration
 
 import (
