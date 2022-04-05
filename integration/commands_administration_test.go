632890776a47e030b77ddf6cd8ca455e3a883950
w84thesun <dmitry.eremenko@ferretdb.io>
2022-04-05 19:10:47 +0300

    restore

---
+++ integration/commands_administration_test.go
@@ -0,0 +1,74 @@
+// Copyright 2021 FerretDB Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package integration
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func TestCommandsAdministrationCreateDropList(t *testing.T) {
+	t.Parallel()
+	ctx, collection := setup(t)
+	db := collection.Database()
+	name := collection.Name()
+
+	names, err := db.ListCollectionNames(ctx, bson.D{})
+	require.NoError(t, err)
+	assert.Contains(t, names, name)
+
+	err = collection.Drop(ctx)
+	require.NoError(t, err)
+
+	// error is consumed by the driver
+	err = collection.Drop(ctx)
+	require.NoError(t, err)
+	err = db.Collection(name).Drop(ctx)
+	require.NoError(t, err)
+
+	// drop manually to check error
+	var actual bson.D
+	err = db.RunCommand(ctx, bson.D{{"drop", name}}).Decode(&actual)
+	expectedErr := mongo.CommandError{
+		Code:    26,
+		Name:    "NamespaceNotFound",
+		Message: `ns not found`,
+	}
+	require.Equal(t, expectedErr, err)
+
+	names, err = db.ListCollectionNames(ctx, bson.D{})
+	require.NoError(t, err)
+	assert.NotContains(t, names, name)
+
+	err = db.CreateCollection(ctx, name)
+	require.NoError(t, err)
+
+	err = db.CreateCollection(ctx, name)
+	expectedErr = mongo.CommandError{
+		Code: 48,
+		Name: "NamespaceExists",
+		Message: `Collection already exists. ` +
+			`NS: testcommandsadministrationcreatedroplist.testcommandsadministrationcreatedroplist`,
+	}
+	require.Equal(t, expectedErr, err)
+
+	names, err = db.ListCollectionNames(ctx, bson.D{})
+	require.NoError(t, err)
+	assert.Contains(t, names, name)
+}
