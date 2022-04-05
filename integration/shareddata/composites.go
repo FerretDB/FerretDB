14d85db1bdc55aa6be858a2b3ff78e24f3a1ea10
Alexey Palazhchenko <alexey.palazhchenko@ferretdb.io>
2022-04-05 11:28:26 +0300

    Integration tests improvements (#441)
    Add test data for documents and arrays.
    Fix NaNs comparision.
    
    Closes #411.

---
+++ integration/shareddata/composites.go
@@ -0,0 +1,30 @@
+// Copyright 2021 FerretDB Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package shareddata
+
+import "go.mongodb.org/mongo-driver/bson"
+
+// Composites contain composite values for tests.
+//
+// This shared data set is not frozen yet, but please add to it only if it is really shared.
+var Composites = &Values[string]{
+	data: map[string]any{
+		"document":       bson.D{{"foo", int32(42)}},
+		"document-empty": bson.D{},
+
+		"array":       bson.A{int32(42)},
+		"array-empty": bson.A{},
+	},
+}
