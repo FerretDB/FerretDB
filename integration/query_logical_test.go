2d1cf437819dd688a93d6f58f14624b0b3307c06
w84thesun <dmitry.eremenko@ferretdb.io>
2022-04-12 18:14:04 +0300

    WIP

--- integration/query_logical_test.go
+++ integration/query_logical_test.go
@@ -20,6 +20,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 
@@ -165,7 +166,7 @@ func TestQueryLogicalNor(t *testing.T) {
 	for name, tc := range map[string]struct {
 		q           bson.D
 		expectedIDs []any
-		err         error
+		err         mongo.CommandError
 	}{
 		"Nor": {
 			q: bson.D{{
@@ -214,12 +215,100 @@ func TestQueryLogicalNor(t *testing.T) {
 
 			var actual []bson.D
 			cursor, err := collection.Find(ctx, tc.q, options.Find().SetSort(bson.D{{"_id", 1}}))
-			if tc.err != nil {
+			if tc.err.Code != 0 {
 				require.Nil(t, tc.expectedIDs)
-				assertEqualError(t, tc.err.(mongo.CommandError), err)
+				assertEqualError(t, tc.err, err)
 				return
 			}
 			require.NoError(t, err)
+			err = cursor.All(ctx, &actual)
+			require.NoError(t, err)
+			assert.Equal(t, tc.expectedIDs, collectIDs(t, actual))
+		})
+	}
+}
+func TestQueryLogicalNot(t *testing.T) {
+	t.Parallel()
+	providers := []shareddata.Provider{shareddata.Scalars, shareddata.Composites}
+	ctx, collection := setup(t, providers...)
+
+	for name, tc := range map[string]struct {
+		filter      bson.D
+		expectedIDs []any
+		err         mongo.CommandError
+	}{
+		"IDNull": {
+			filter: bson.D{{"_id", bson.D{{"$not", nil}}}},
+			err: mongo.CommandError{
+				Code:    2,
+				Name:    "BadValue",
+				Message: "$not needs a regex or a document",
+			},
+		},
+		"ValueNull": {
+			filter: bson.D{{"value", bson.D{{"$not", bson.D{{"$eq", nil}}}}}},
+			err: mongo.CommandError{
+				Code:    2,
+				Name:    "BadValue",
+				Message: "$not needs a regex or a document",
+			},
+		},
+		"NoSuchFieldNull": {
+			filter: bson.D{{"no-such-field", bson.D{{"$not", 0}}}},
+			expectedIDs: []any{
+				"array", "array-empty", "array-three",
+				"binary", "binary-empty",
+				"bool-false", "bool-true",
+				"datetime", "datetime-epoch", "datetime-year-max", "datetime-year-min",
+				"document", "document-empty",
+				"double", "double-max", "double-nan", "double-negative-infinity", "double-negative-zero",
+				"double-positive-infinity", "double-smallest", "double-whole", "double-zero",
+				"int32", "int32-max", "int32-min", "int32-zero",
+				"int64", "int64-max", "int64-min", "int64-zero",
+				"null",
+				"objectid", "objectid-empty",
+				"regex", "regex-empty",
+				"string", "string-double", "string-empty", "string-whole",
+				"timestamp", "timestamp-i",
+			},
+		},
+
+		"ValueNumber": {
+			filter:      bson.D{{"value", bson.D{{"$not", 42}}}},
+			expectedIDs: []any{"array", "array-three", "double-whole", "int32", "int64"},
+		},
+
+		"ValueRegex": {
+			filter: bson.D{{"value", bson.D{{"$not", primitive.Regex{Pattern: "^fo"}}}}},
+			expectedIDs: []any{
+				"array", "array-empty",
+				"binary", "binary-empty",
+				"bool-false", "bool-true",
+				"datetime", "datetime-epoch", "datetime-year-max", "datetime-year-min",
+				"document", "document-empty",
+				"double", "double-max", "double-nan", "double-negative-infinity", "double-negative-zero",
+				"double-positive-infinity", "double-smallest", "double-whole", "double-zero",
+				"int32", "int32-max", "int32-min", "int32-zero",
+				"int64", "int64-max", "int64-min", "int64-zero",
+				"null",
+				"objectid", "objectid-empty",
+				"regex", "regex-empty",
+				"string-double", "string-empty", "string-whole",
+				"timestamp", "timestamp-i",
+			},
+		},
+	} {
+		name, tc := name, tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			var actual []bson.D
+			cursor, err := collection.Find(ctx, tc.filter, options.Find().SetSort(bson.D{{"_id", 1}}))
+			if tc.err.Code != 0 {
+				require.Nil(t, tc.expectedIDs)
+				assertEqualError(t, tc.err, err)
+				return
+			}
 			err = cursor.All(ctx, &actual)
 			require.NoError(t, err)
 			assert.Equal(t, tc.expectedIDs, collectIDs(t, actual))
