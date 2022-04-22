e99bafb04ba39a4fa222e39f11194e0b09dd92a4
Alexey Palazhchenko <alexey.palazhchenko@ferretdb.io>
2022-04-22 09:45:16 +0300

    Pass context to PostgreSQL pool

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -12,4 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package handlers hosts wire protocol commands handlers.
 package handlers
--- internal/handlers/pg/handler.go
+++ internal/handlers/pg/handler.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package pg provides PostgreSQL handler.
 package pg
 
 import (
