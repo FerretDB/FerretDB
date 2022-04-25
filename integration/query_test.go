26289104ff05b6d17be3191fcc6fdd80af2dc50d
Georgy Ginzburg <ginzburggeorgy@gmail.com>
2022-04-25 19:59:19 +0300

    better names in test data

--- integration/query_test.go
+++ integration/query_test.go
@@ -13,8 +13,8 @@ func TestUnknownFilterOperator(t *testing.T) {
 	t.Parallel()
 	ctx, collection := setup(t, shareddata.Scalars)
 
-	filter := bson.D{{"value", bson.D{{"$aboba", 42}}}}
-	errExpected := mongo.CommandError{Code: 2, Name: "BadValue", Message: "unknown operator: $aboba"}
+	filter := bson.D{{"value", bson.D{{"$someUnknownOperator", 42}}}}
+	errExpected := mongo.CommandError{Code: 2, Name: "BadValue", Message: "unknown operator: $someUnknownOperator"}
 	_, err := collection.Find(ctx, filter)
 	AssertEqualError(t, errExpected, err)
 }
