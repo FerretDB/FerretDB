f4892bd4a6e224f392f75cd0ec78184b1a32a8e5
Ekaterina Blech <ekaterina.blech@ferretdb.io>
2022-04-25 14:54:39 +0300

    common handlers interface + dummy handler

--- internal/handlers/common/handlers.go
+++ internal/handlers/common/handlers.go
@@ -12,8 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package handlers hosts wire protocol commands handlers.
-package handlers
+// Handler interface  commands handlers.
+package common
 
 import (
 	"context"
