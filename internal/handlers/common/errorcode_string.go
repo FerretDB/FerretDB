b9e1fbf34a7f779ad74df54982be22f66cc7d06a
fenogentov <ShuraF@yandex.ru>
2022-04-26 21:09:08 +0300

    fix string errors

--- internal/handlers/common/errorcode_string.go
+++ internal/handlers/common/errorcode_string.go
@@ -8,6 +8,7 @@ func _() {
 	// An "invalid array index" compiler error signifies that the constant values have changed.
 	// Re-run the stringer command to generate them again.
 	var x [1]struct{}
+	_ = x[ErrWeird-0]
 	_ = x[errInternalError-1]
 	_ = x[ErrBadValue-2]
 	_ = x[ErrFailedToParse-9]
@@ -22,7 +23,7 @@ func _() {
 }
 
 const (
-	_ErrorCode_name_0 = "InternalErrorBadValue"
+	_ErrorCode_name_0 = "WeirdErrorInternalErrorBadValue"
 	_ErrorCode_name_1 = "FailedToParse"
 	_ErrorCode_name_2 = "NamespaceNotFound"
 	_ErrorCode_name_3 = "NamespaceExists"
@@ -34,14 +35,13 @@ const (
 )
 
 var (
-	_ErrorCode_index_0 = [...]uint8{0, 13, 21}
+	_ErrorCode_index_0 = [...]uint8{0, 10, 23, 31}
 	_ErrorCode_index_7 = [...]uint8{0, 13, 26}
 )
 
 func (i ErrorCode) String() string {
 	switch {
-	case 1 <= i && i <= 2:
-		i -= 1
+	case 0 <= i && i <= 2:
 		return _ErrorCode_name_0[_ErrorCode_index_0[i]:_ErrorCode_index_0[i+1]]
 	case i == 9:
 		return _ErrorCode_name_1
