632b783976c4a4adfac11960c1339c81d763e188
Matvii Horskyi <matvey.gorsky.it@gmail.com>
2022-04-26 20:16:02 +0300

    WIP

--- internal/handlers/common/parse.go
+++ internal/handlers/common/parse.go
@@ -1,3 +1,17 @@
+// Copyright 2021 FerretDB Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package common
 
 import (
@@ -8,6 +22,7 @@ import (
 	"github.com/FerretDB/FerretDB/internal/util/lazyerrors"
 )
 
+// ParseOSRelease parses the etc/os-release file.
 func ParseOSRelease(reader io.Reader) (map[string]string, error) {
 	scanner := bufio.NewScanner(reader)
 
