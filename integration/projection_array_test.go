33cb9a200415e2e529c7456542c58ad55ff0ea57
Georgy Ginzburg <ginzburggeorgy@gmail.com>
2022-05-01 05:07:58 +0300

    tests

--- integration/projection_array_test.go
+++ integration/projection_array_test.go
@@ -1,11 +1,12 @@
 package integration
 
 import (
-	"github.com/FerretDB/FerretDB/internal/types"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"math"
 	"testing"
 )
 
@@ -15,116 +16,136 @@ func TestProjectionQuerySlice(t *testing.T) {
 	_, err := collection.InsertOne(ctx,
 		bson.D{{"_id", "array"}, {"value", bson.A{1, 2, 3, 4}}})
 	require.NoError(t, err)
+
 	type testCase struct {
-		projection bson.D
-		expected   *types.Array
-		err        *mongo.CommandError
+		projection    bson.D
+		expectedArray bson.A
+		err           *mongo.CommandError
 	}
 
-	t.Run("SingleArg", func(t *testing.T) {
-		t.Parallel()
-		for name, tc := range map[string]testCase{
-			"StringType": {
-				projection: bson.D{{"value", bson.D{{"$slice", "string"}}}},
-				expected:   nil,
-				err:        &mongo.CommandError{},
+	for name, tc := range map[string]testCase{
+		"SingleArgString": {
+			projection:    bson.D{{"value", bson.D{{"$slice", "string"}}}},
+			expectedArray: nil,
+			err: &mongo.CommandError{
+				Code: 28667,
+				Name: "Location28667",
+				Message: `Invalid $slice syntax. The given syntax { $slice: string } ` +
+					`did not match the find() syntax because :: Location31273: $slice only supports numbers ` +
+					`and [skip, limit] arrays :: The given syntax did not match the expression $slice syntax. ` +
+					`:: caused by :: Expression $slice takes at least 2 arguments, and at most 3, ` +
+					`but 1 were passed in.`,
 			},
-			"Positive<Len": {
-				projection: bson.D{{"value", bson.D{{"$slice", 2}}}},
-				expected:   nil,
-				err:        nil,
+		},
+		"SkipIsString": {
+			projection:    bson.D{{"value", bson.D{{"$slice", bson.A{"string", 5}}}}},
+			expectedArray: nil,
+			err: &mongo.CommandError{
+				Code:    28724,
+				Name:    "Location28724",
+				Message: "First argument to $slice must be an array, but is of type: string",
 			},
-			"Positive>=Len": {
-				projection: bson.D{{"value", bson.D{{"$slice", 10}}}},
-				expected:   nil,
-				err:        &mongo.CommandError{},
+		},
+		"LimitIsString": {
+			projection:    bson.D{{"value", bson.D{{"$slice", bson.A{int32(2), "string"}}}}},
+			expectedArray: nil,
+			err: &mongo.CommandError{
+				Code:    28724,
+				Name:    "Location28724",
+				Message: "First argument to $slice must be an array, but is of type: int",
 			},
-			"NegativeAbs>Len": {
-				projection: bson.D{{"value", bson.D{{"$slice", -10}}}},
-				expected:   nil,
-				err:        &mongo.CommandError{},
+		},
+		"ArgEmptyArr": {
+			projection:    bson.D{{"value", bson.D{{"$slice", bson.A{}}}}},
+			expectedArray: nil,
+			err: &mongo.CommandError{
+				Code: 28667,
+				Name: "Location28667",
+				Message: "Invalid $slice syntax. The given syntax { $slice: [] } " +
+					"did not match the find() syntax because :: Location31273: " +
+					"$slice only supports numbers and [skip, limit] arrays :: " +
+					"The given syntax did not match the expression " +
+					"$slice syntax. :: caused by :: " +
+					"Expression $slice takes at least 2 arguments, and at most 3, but 0 were passed in.",
 			},
-			"NegativeAbs==Len": {
-				projection: bson.D{{"value", bson.D{{"$slice", -4}}}},
-				expected:   nil,
-				err:        &mongo.CommandError{},
+		},
+		"PositiveSingleArg": {
+			projection:    bson.D{{"value", bson.D{{"$slice", 2}}}},
+			expectedArray: bson.A{int32(1), int32(2)},
+		},
+		"NegativeSingleArg": {
+			projection:    bson.D{{"value", bson.D{{"$slice", -2}}}},
+			expectedArray: bson.A{int32(3), int32(4)},
+		},
+		"PositiveSkip": {
+			projection:    bson.D{{"value", bson.D{{"$slice", bson.A{1, 2}}}}},
+			expectedArray: bson.A{int32(2), int32(3)},
+		},
+		"NegativeSkip": {
+			projection:    bson.D{{"value", bson.D{{"$slice", bson.A{-3, 2}}}}},
+			expectedArray: bson.A{int32(2), int32(3)},
+		},
+		"NegativeLimit": {
+			projection: bson.D{{"value", bson.D{{"$slice", bson.A{3, -2}}}}},
+			err: &mongo.CommandError{
+				Code:    28724,
+				Name:    "Location28724",
+				Message: "First argument to $slice must be an array, but is of type: int",
 			},
-			"NegativeAbs<Len": {
-				projection: bson.D{{"value", bson.D{{"$slice", -3}}}},
-				expected:   nil,
-				err:        &mongo.CommandError{},
+		},
+		"ArgNaN": {
+			projection:    bson.D{{"value", bson.D{{"$slice", math.NaN()}}}},
+			expectedArray: bson.A{},
+		},
+		"ArgInf": {
+			projection:    bson.D{{"value", bson.D{{"$slice", math.Inf(1)}}}},
+			expectedArray: bson.A{int32(1), int32(2), int32(3), int32(4)},
+		},
+		"SingleArgNull": {
+			projection: bson.D{{"value", bson.D{{"$slice", nil}}}},
+			err: &mongo.CommandError{
+				Code: 28667,
+				Name: "Location28667",
+				Message: "Invalid $slice syntax. The given syntax { $slice: null } " +
+					"did not match the find() syntax because :: Location31273: $slice only supports " +
+					"numbers and [skip, limit] arrays :: " +
+					"The given syntax did not match the expression $slice syntax. " +
+					":: caused by :: Expression $slice takes at least 2 arguments, " +
+					"and at most 3, but 1 were passed in.",
 			},
-		} {
-			name, tc := name, tc
-			t.Run(name, func(t *testing.T) {
-				t.Parallel()
+		},
+		"NullInPair": {
+			projection: bson.D{{"value", bson.D{{"$slice", bson.A{2, nil}}}}},
+			err: &mongo.CommandError{
+				Code: 28667,
+				Name: "Location28667",
+				Message: "Invalid $slice syntax. The given syntax { $slice: null } " +
+					"did not match the find() syntax because :: Location31273: $slice only supports " +
+					"numbers and [skip, limit] arrays :: " +
+					"The given syntax did not match the expression $slice syntax. " +
+					":: caused by :: Expression $slice takes at least 2 arguments, " +
+					"and at most 3, but 1 were passed in.",
+			},
+		},
+	} {
+		name, tc := name, tc
+		t.Run(name, func(t *testing.T) {
+			cursor, err := collection.Find(
+				ctx, bson.D{},
+				options.Find().SetProjection(tc.projection),
+			)
 
-				cursor, err := collection.Find(ctx, bson.D{}) //options.Find().SetProjection(tc.projection),
+			if tc.err != nil {
+				require.Nil(t, tc.expectedArray)
+				AssertEqualError(t, *tc.err, err)
+				return
+			}
+			require.NoError(t, err)
 
-				if tc.err != nil {
-					require.Nil(t, tc.expected)
-					AssertEqualError(t, *tc.err, err)
-					return
-				}
-
-				var actual []bson.D
-				err = cursor.All(ctx, &actual)
-				require.NoError(t, err)
-				assert.Equal(t, tc.expected, actual)
-			})
-		}
-	})
-	//t.Run("MultipleArgs", func(t *testing.T) {
-	//	t.Skip()
-	//	t.Parallel()
-	//	for name, tc := range map[string]testCase{
-	//		// $slice: [ <number to skip>, <number to return> ]
-	//		"InvalidNumberOfArgs": {
-	//			projection: bson.D{{"value", bson.D{{"$slice", bson.A{1, 2, 3}}}}},
-	//			expected:   nil,
-	//			err:        mongo.CommandError{},
-	//		},
-	//		"ToSkipInvalidType": {
-	//			projection: bson.D{{"value", bson.D{{"$slice", bson.A{"string", 2}}}}},
-	//			expected:   nil,
-	//			err:        mongo.CommandError{},
-	//		},
-	//		"ToReturnInvalidValue": { // can't be negative
-	//			projection: bson.D{{"value", bson.D{{"$slice", bson.A{1, -2}}}}},
-	//			expected:   nil,
-	//			err:        mongo.CommandError{},
-	//		},
-	//		"ToReturnInvalidType": {
-	//			projection: bson.D{{"value", bson.D{{"$slice", bson.A{1, "string"}}}}},
-	//			expected:   nil,
-	//			err:        mongo.CommandError{},
-	//		},
-	//		"ToSkip>=0_ToReturn>Len": {
-	//			projection: bson.D{{"value", bson.D{{"$slice", bson.A{2, 5}}}}},
-	//			expected:   nil,
-	//			err:        mongo.CommandError{},
-	//		},
-	//		"ToSkip>=0_ToReturn<=Len": {
-	//			projection: bson.D{{"value", bson.D{{"$slice", bson.A{2, 3}}}}},
-	//			expected:   nil,
-	//			err:        mongo.CommandError{},
-	//		},
-	//		"ToSkip<0_ToReturn<=Len": {
-	//			projection: bson.D{{"value", bson.D{{"$slice", bson.A{-2, 4}}}}},
-	//			expected:   nil,
-	//			err:        mongo.CommandError{},
-	//		},
-	//		"ToSkip<0_ToReturn>Len": {
-	//			projection: bson.D{{"value", bson.D{{"$slice", bson.A{-3, 10}}}}},
-	//			expected:   nil,
-	//			err:        mongo.CommandError{},
-	//		},
-	//	} {
-	//		//name, tc := name, tc
-	//		t.Run(name, func(t *testing.T) {
-	//			t.Parallel()
-	//		})
-	//	}
-	//})
-
+			var actual []bson.D
+			err = cursor.All(ctx, &actual)
+			require.NoError(t, err)
+			assert.Equal(t, tc.expectedArray, actual[0].Map()["value"])
+		})
+	}
 }
