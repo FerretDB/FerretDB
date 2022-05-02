b5431c066735fa6917af69a0c133f9fe97502597
Ekaterina Blech <ekaterina.blech@ferretdb.io>
2022-05-02 11:57:54 +0300

    deprecated to new must.NotFail

--- internal/types/path_test.go
+++ internal/types/path_test.go
@@ -31,132 +31,137 @@ func TestRemoveByPath(t *testing.T) {
 		name string
 		path []string
 		res  any
-		err  string
 	}
-	for _, tc := range []testCase{{ //nolint:paralleltest // false positive
-		name: "not found no error, ismaster field removed",
-		path: []string{"ismaster", "0"},
-		res: MustNewDocument(
-			"ismaster", true,
-			"client", MustNewArray(
-				MustNewDocument(
-					"document", "abc",
-					"score", float64(42.13),
-					"age", int32(1000),
-				),
-				MustNewDocument(
-					"document", "def",
-					"score", float64(42.13),
-					"age", int32(1000),
-				),
-				MustNewDocument(
-					"document", "jkl",
-					"score", int32(24),
-					"age", int32(1002),
-				),
-			),
-			"value", must.NotFail(NewArray("none")),
-		),
-	}, {
-		name: "removed entire client field",
-		path: []string{"client"},
-		res: MustNewDocument(
-			"ismaster", true,
-			"value", must.NotFail(NewArray("none")),
-		),
-	}, {
-		name: "only 1d array element of client field is removed",
-		path: []string{"client", "1"},
-		res: MustNewDocument(
-			"ismaster", true,
-			"client", MustNewArray(
-				MustNewDocument(
-					"document", "abc",
-					"score", float64(42.13),
-					"age", int32(1000),
-				),
-				MustNewDocument(
-					"document", "jkl",
-					"score", int32(24),
-					"age", int32(1002),
-				),
-			),
-			"value", must.NotFail(NewArray("none")),
-		),
-	}, {
-		name: "not found, element must be on place, no error",
-		path: []string{"client", "3"},
-		res: MustNewDocument(
-			"ismaster", true,
-			"client", MustNewArray(
-				MustNewDocument(
-					"document", "abc",
-					"score", float64(42.13),
-					"age", int32(1000),
-				),
-				MustNewDocument(
-					"document", "def",
-					"score", float64(42.13),
-					"age", int32(1000),
-				),
-				MustNewDocument(
-					"document", "jkl",
-					"score", int32(24),
-					"age", int32(1002),
-				),
-			),
-			"value", must.NotFail(NewArray("none")),
-		),
-	}, {
-		name: "not found, no error doc is same",
-		path: []string{"compression", "invalid"},
-		res: MustNewDocument(
-			"ismaster", true,
-			"client", MustNewArray(
-				MustNewDocument(
-					"document", "abc",
-					"score", float64(42.13),
-					"age", int32(1000),
-				),
-				MustNewDocument(
-					"document", "def",
-					"score", float64(42.13),
-					"age", int32(1000),
-				),
-				MustNewDocument(
-					"document", "jkl",
-					"score", int32(24),
-					"age", int32(1002),
-				),
-			),
-			"value", must.NotFail(NewArray("none")),
-		),
-	}} {
+	for _, tc := range []testCase{ //nolint:paralleltest // false positive
+		{
+			name: "not found no error, ismaster field removed",
+			path: []string{"ismaster", "0"},
+			res: must.NotFail(NewDocument(
+				"ismaster", true,
+				"client", must.NotFail(NewArray(
+					must.NotFail(NewDocument(
+						"document", "abc",
+						"score", float64(42.13),
+						"age", int32(1000),
+					)),
+					must.NotFail(NewDocument(
+						"document", "def",
+						"score", float64(42.13),
+						"age", int32(1000),
+					)),
+					must.NotFail(NewDocument(
+						"document", "jkl",
+						"score", int32(24),
+						"age", int32(1002),
+					)),
+				)),
+				"value", must.NotFail(NewArray("none")),
+			)),
+		},
+		{
+			name: "removed entire client field",
+			path: []string{"client"},
+			res: must.NotFail(NewDocument(
+				"ismaster", true,
+				"value", must.NotFail(NewArray("none")),
+			)),
+		},
+		{
+			name: "only 1d array element of client field is removed",
+			path: []string{"client", "1"},
+			res: must.NotFail(NewDocument(
+				"ismaster", true,
+				"client", must.NotFail(NewArray(
+					must.NotFail(NewDocument(
+						"document", "abc",
+						"score", float64(42.13),
+						"age", int32(1000),
+					)),
+					must.NotFail(NewDocument(
+						"document", "jkl",
+						"score", int32(24),
+						"age", int32(1002),
+					)),
+				)),
+				"value", must.NotFail(NewArray("none")),
+			)),
+		},
+		{
+			name: "not found, element must be on place, no error",
+			path: []string{"client", "3"},
+			res: must.NotFail(NewDocument(
+				"ismaster", true,
+				"client", must.NotFail(NewArray(
+					must.NotFail(NewDocument(
+						"document", "abc",
+						"score", float64(42.13),
+						"age", int32(1000),
+					)),
+					must.NotFail(NewDocument(
+						"document", "def",
+						"score", float64(42.13),
+						"age", int32(1000),
+					)),
+					must.NotFail(NewDocument(
+						"document", "jkl",
+						"score", int32(24),
+						"age", int32(1002),
+					)),
+				)),
+				"value", must.NotFail(NewArray("none")),
+			)),
+		},
+		{
+			name: "not found, no error doc is same",
+			path: []string{"compression", "invalid"},
+			res: must.NotFail(NewDocument(
+				"ismaster", true,
+				"client", must.NotFail(NewArray(
+					must.NotFail(NewDocument(
+						"document", "abc",
+						"score", float64(42.13),
+						"age", int32(1000),
+					)),
+					must.NotFail(NewDocument(
+						"document", "def",
+						"score", float64(42.13),
+						"age", int32(1000),
+					)),
+					must.NotFail(NewDocument(
+						"document", "jkl",
+						"score", int32(24),
+						"age", int32(1002),
+					)),
+				)),
+				"value", must.NotFail(NewArray("none")),
+			)),
+		},
+	} {
 		tc := tc
 		t.Run(fmt.Sprint(tc.path), func(t *testing.T) {
 			t.Parallel()
 
-			doc := MustNewDocument(
+			doc := must.NotFail(NewDocument(
 				"ismaster", true,
-				"client", MustNewArray(
-					MustNewDocument(
+				"client", must.NotFail(NewArray(
+					must.NotFail(NewDocument(
 						"document", "abc",
 						"score", float64(42.13),
 						"age", int32(1000),
-					),
-					MustNewDocument(
+					)),
+					must.NotFail(NewDocument(
 						"document", "def",
 						"score", float64(42.13),
 						"age", int32(1000),
-					),
-					MustNewDocument(
+					)),
+					must.NotFail(NewDocument(
 						"document", "jkl",
 						"score", int32(24),
 						"age", int32(1002),
-					),
-				),
+					)),
+				)),
 				"value", must.NotFail(NewArray("none")),
-			)
+			))
 
 			doc.RemoveByPath(tc.path...)
 			assert.Equal(t, tc.res, doc, tc.name)
@@ -167,27 +172,27 @@ func TestRemoveByPath(t *testing.T) {
 func TestGetByPath(t *testing.T) {
 	t.Parallel()
 
-	doc := MustNewDocument(
+	doc := must.NotFail(NewDocument(
 		"ismaster", true,
-		"client", MustNewDocument(
-			"driver", MustNewDocument(
+		"client", must.NotFail(NewDocument(
+			"driver", must.NotFail(NewDocument(
 				"name", "nodejs",
 				"version", "4.0.0-beta.6",
-			),
-			"os", MustNewDocument(
+			)),
+			"os", must.NotFail(NewDocument(
 				"type", "Darwin",
 				"name", "darwin",
 				"architecture", "x64",
 				"version", "20.6.0",
-			),
+			)),
 			"platform", "Node.js v14.17.3, LE (unified)|Node.js v14.17.3, LE (unified)",
-			"application", MustNewDocument(
+			"application", must.NotFail(NewDocument(
 				"name", "mongosh 1.0.1",
-			),
-		),
+			)),
+		)),
 		"compression", must.NotFail(NewArray("none")),
 		"loadBalanced", false,
-	)
+	))
 
 	type testCase struct {
 		path []string
@@ -203,10 +208,10 @@ func TestGetByPath(t *testing.T) {
 		res:  must.NotFail(NewArray("none")),
 	}, {
 		path: []string{"client", "driver"},
-		res: MustNewDocument(
+		res: must.NotFail(NewDocument(
 			"name", "nodejs",
 			"version", "4.0.0-beta.6",
-		),
+		)),
 	}, {
 		path: []string{"client", "0"},
 		err:  `types.getByPath: types.Document.Get: key not found: "0"`,
