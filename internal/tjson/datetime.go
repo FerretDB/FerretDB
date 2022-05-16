8607c21840cddf64557d4155cbad35a8bf5b9265
Ekaterina Blech <ekaterina.blech@ferretdb.io>
2022-05-16 16:45:23 +0300

    date time

---
+++ internal/tjson/datetime.go
@@ -0,0 +1,84 @@
+// Copyright 2021 FerretDB Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tjson
+
+import (
+	"bytes"
+	"encoding/json"
+	"time"
+
+	"github.com/FerretDB/FerretDB/internal/util/lazyerrors"
+)
+
+// dateTimeType represents BSON UTC datetime type.
+type dateTimeType time.Time
+
+// tjsontype implements tjsontype interface.
+func (dt *dateTimeType) tjsontype() {}
+
+// String returns formatted time for debugging.
+func (dt *dateTimeType) String() string {
+	return time.Time(*dt).Format(time.RFC3339Nano)
+}
+
+var dateTimeSchema = map[string]any{
+	"type":   "string",
+	"format": "date-time",
+}
+
+// string in RFC 3339.
+type dateTimeJSON string
+
+// Unmarshal build-in to tigris.
+func (dt *dateTimeType) Unmarshal(_ map[string]any) ([]byte, error) {
+	res, err := json.Marshal(dateTimeJSON(
+		time.Time(*dt).Format(time.RFC3339Nano),
+	))
+	if err != nil {
+		return nil, lazyerrors.Error(err)
+	}
+	return res, nil
+}
+
+// Marshal tigris to build-in.
+func (dt *dateTimeType) Marshal(data []byte, _ map[string]any) error {
+	if bytes.Equal(data, []byte("null")) {
+		panic("null data")
+	}
+
+	r := bytes.NewReader(data)
+	dec := json.NewDecoder(r)
+	dec.DisallowUnknownFields()
+
+	var o dateTimeJSON
+	if err := dec.Decode(&o); err != nil {
+		return lazyerrors.Error(err)
+	}
+	if err := checkConsumed(dec, r); err != nil {
+		return lazyerrors.Error(err)
+	}
+	t, err := time.Parse(time.RFC3339Nano, string(o))
+	if err != nil {
+		return lazyerrors.Error(err)
+	}
+
+	*dt = dateTimeType(t)
+	return nil
+}
+
+// check interfaces
+var (
+	_ tjsontype = (*dateTimeType)(nil)
+)
---
+++ internal/tjson/datetime_test.go
@@ -0,0 +1,60 @@
+// Copyright 2021 FerretDB Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tjson
+
+import (
+	"testing"
+	"time"
+)
+
+var dateTimeTestCases = []testCase{{
+	name: "2021",
+	v:    time.Date(2021, 11, 1, 10, 18, 42, 123000000, time.UTC),
+	j:    `"2021-11-01T10:18:42.123Z"`,
+	s:    dateTimeSchema,
+}, {
+	name: "unix_zero",
+	v:    time.Unix(0, 0).UTC(),
+	j:    `"1970-01-01T00:00:00Z"`,
+	s:    dateTimeSchema,
+}, {
+	name: "0",
+	v:    time.Date(0, 1, 1, 0, 0, 0, 0, time.UTC),
+	j:    `"0000-01-01T00:00:00Z"`,
+	s:    dateTimeSchema,
+}, {
+	name: "9999",
+	v:    time.Date(9999, 12, 31, 23, 59, 59, 999000000, time.UTC),
+	j:    `"9999-12-31T23:59:59.999Z"`,
+	s:    dateTimeSchema,
+}, {
+	name: "EOF",
+	j:    `"{"`,
+	jErr: `parsing time "{" as "2006-01-02T15:04:05.999999999Z07:00": cannot parse "{" as "2006"`,
+	s:    dateTimeSchema,
+}}
+
+func TestDateTime(t *testing.T) {
+	t.Parallel()
+	testJSON(t, dateTimeTestCases, func() tjsontype { return new(dateTimeType) })
+}
+
+func FuzzDateTime(f *testing.F) {
+	fuzzJSON(f, dateTimeTestCases, func() tjsontype { return new(dateTimeType) })
+}
+
+func BenchmarkDateTime(b *testing.B) {
+	benchmark(b, dateTimeTestCases, func() tjsontype { return new(dateTimeType) })
+}
