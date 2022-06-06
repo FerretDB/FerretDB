9486c10c1e566c2e3fcf9c9b23d556a2bf473266
Alexey Palazhchenko <alexey.palazhchenko@ferretdb.io>
2022-06-06 17:54:37 +0300

    Add package description

--- internal/util/logging/logging.go
+++ internal/util/logging/logging.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package logging provides logging helpers.
 package logging
 
 import (
