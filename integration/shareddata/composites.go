a740046a9299ff71e58f74953e61a770636499b1
Elena Grahovac <elena.grahovac@ferretdb.io>
2022-06-10 16:42:59 +0200

    Added issue number

--- integration/shareddata/composites.go
+++ integration/shareddata/composites.go
@@ -38,7 +38,7 @@ var Composites = &Values[string]{
 		"array-embedded":      bson.A{bson.A{int32(42), "foo"}, nil},
 		"array-empty":         bson.A{},
 
-		// TODO: This case demonstrates some bugs.
+		// TODO: This case demonstrates a bug, see https://github.com/FerretDB/FerretDB/issues/732
 		// "array-empty-nested": bson.A{bson.A{}},
 
 		"array-null": bson.A{nil},
