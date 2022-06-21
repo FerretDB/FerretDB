b02a02f94a833c110dd510a7475024dfcd8c3025
noisersup <patryk@kwiatek.xyz>
2022-06-21 17:30:11 +0200

    Remove overlooked comment

--- internal/handlers/common/msg_getfreemonitoringstatus.go
+++ internal/handlers/common/msg_getfreemonitoringstatus.go
@@ -29,7 +29,7 @@ func MsgGetFreeMonitoringStatus(ctx context.Context, msg *wire.OpMsg) (*wire.OpM
 	err := reply.SetSections(wire.OpMsgSection{
 		Documents: []*types.Document{must.NotFail(types.NewDocument(
 			"state", "disabled",
-			"message", "monitoring is not enabled", // Should I include that?
+			"message", "monitoring is not enabled",
 			"ok", float64(1),
 		))},
 	})
