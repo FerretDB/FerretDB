db37e83d5a4bdc43876398b93ba119fc6e7ecb4d
Ekaterina Blech <ekaterina.blech@ferretdb.io>
2022-06-21 15:04:23 +0300

    wip

---
+++ ferretdb/handler_tigris.go
@@ -0,0 +1,37 @@
+// Copyright 2021 FerretDB Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:build tigris
+// +build tigris
+
+package ferretdb
+
+import (
+	"flag"
+
+	"github.com/FerretDB/FerretDB/internal/handlers"
+	"github.com/FerretDB/FerretDB/internal/handlers/tigris"
+)
+
+// `tigris` handler flags.
+var (
+	tigrisURLF = flag.String("tigris-url", "127.0.0.1:8081", "Tigris URL")
+)
+
+// init registers `tigris` handler for Tigris that is enabled only when compiled with `tigris` build tag.
+func init() {
+	registeredHandlers["tigris"] = func(*newHandlerOpts) (handlers.Interface, error) {
+		return tigris.New()
+	}
+}
---
+++ ferretdb/runner.go
@@ -0,0 +1,188 @@
+// Copyright 2021 FerretDB Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ferretdb
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/common/expfmt"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"golang.org/x/exp/maps"
+	"golang.org/x/exp/slices"
+
+	"github.com/FerretDB/FerretDB/internal/clientconn"
+	"github.com/FerretDB/FerretDB/internal/handlers"
+	"github.com/FerretDB/FerretDB/internal/util/debug"
+	"github.com/FerretDB/FerretDB/internal/util/logging"
+	"github.com/FerretDB/FerretDB/internal/util/must"
+	"github.com/FerretDB/FerretDB/internal/util/version"
+)
+
+// configuration type Config struct { GoodName string }
+
+// Config ConnectionString contains a string connecting to the backend.
+// "postgres://user@postgres:5432/ferretdb" - then it's postgres
+type Config struct {
+	ConetionString string
+}
+
+// registeredHandlers maps handler names to constructors.
+// The values for `registeredHandlers` must be set through the `init()` functions of the corresponding handlers
+// so that we can control which handlers will be included in the build with build tags.
+var registeredHandlers = map[string]newHandler{}
+
+// newHandler represents a function that constructs a new handler.
+type newHandler func(opts *newHandlerOpts) (handlers.Interface, error)
+
+// newHandlerOpts represents common configuration for constructing handlers.
+//
+// Handler-specific configuration is passed via command-line flags directly.
+type newHandlerOpts struct {
+	ctx    context.Context
+	logger *zap.Logger
+}
+
+// initFlags improves flags settings after all global flags are initialized
+// and all handler constructors are registered.
+func initFlags(config *Config) {
+	_, ok := registeredHandlers["pg"]
+	if !ok {
+		panic("no pg handler registered")
+	}
+
+	handlers := maps.Keys(registeredHandlers)
+	slices.Sort(handlers)
+
+	f := flag.Lookup("handler")
+	f.Usage = "backend handler: " + strings.Join(handlers, ", ")
+	f.DefValue = "pg"
+	must.NoError(f.Value.Set(f.DefValue))
+
+	levels := []string{
+		zapcore.DebugLevel.String(),
+		zapcore.InfoLevel.String(),
+		zapcore.WarnLevel.String(),
+		zapcore.ErrorLevel.String(),
+	}
+
+	f = flag.Lookup("log-level")
+	f.Usage = "log level: " + strings.Join(levels, ", ")
+	f.DefValue = zapcore.DebugLevel.String()
+	must.NoError(f.Value.Set(f.DefValue))
+}
+
+// Run function that runs embedded proxy until ctx is canceled.
+func run(ctx context.Context, config Config) error {
+	initFlags()
+	flag.Parse()
+
+	level, err := zapcore.ParseLevel(*logLevelF)
+	if err != nil {
+		log.Fatal(err)
+	}
+	logging.Setup(level)
+	logger := zap.L()
+
+	info := version.Get()
+
+	if *versionF {
+		fmt.Fprintln(os.Stdout, "version:", info.Version)
+		fmt.Fprintln(os.Stdout, "commit:", info.Commit)
+		fmt.Fprintln(os.Stdout, "branch:", info.Branch)
+		fmt.Fprintln(os.Stdout, "dirty:", info.Dirty)
+		return nil
+	}
+
+	startFields := []zap.Field{
+		zap.String("version", info.Version),
+		zap.String("commit", info.Commit),
+		zap.String("branch", info.Branch),
+		zap.Bool("dirty", info.Dirty),
+	}
+	for _, k := range info.BuildEnvironment.Keys() {
+		v := must.NotFail(info.BuildEnvironment.Get(k))
+		startFields = append(startFields, zap.Any(k, v))
+	}
+	logger.Info("Starting FerretDB "+info.Version+"...", startFields...)
+
+	var found bool
+	for _, m := range clientconn.AllModes {
+		if *modeF == string(m) {
+			found = true
+			break
+		}
+	}
+	if !found {
+		logger.Sugar().Fatalf("Unknown mode %q.", *modeF)
+	}
+
+	// TODO - add ifs - not very beautiful. What are the options?
+	ctx, stop := notifyAppTermination(context.Background())
+	go func() {
+		<-ctx.Done()
+		logger.Info("Stopping...")
+		stop()
+	}()
+
+	go debug.RunHandler(ctx, *debugAddrF, logger.Named("debug"))
+
+	newHandler := registeredHandlers[*handlerF]
+	if newHandler == nil {
+		logger.Sugar().Fatalf("Unknown backend handler %q.", *handlerF)
+	}
+	h, err := newHandler(&newHandlerOpts{
+		ctx:    ctx,
+		logger: logger,
+	})
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+	defer h.Close()
+
+	l := clientconn.NewListener(&clientconn.NewListenerOpts{
+		ListenAddr:      *listenAddrF,
+		ProxyAddr:       *proxyAddrF,
+		Mode:            clientconn.Mode(*modeF),
+		Handler:         h,
+		Logger:          logger,
+		TestConnTimeout: *testConnTimeoutF,
+	})
+
+	prometheus.DefaultRegisterer.MustRegister(l)
+
+	err = l.Run(ctx)
+	if err == nil || err == context.Canceled {
+		logger.Info("Listener stopped")
+	} else {
+		logger.Error("Listener stopped", zap.Error(err))
+	}
+
+	mfs, err := prometheus.DefaultGatherer.Gather()
+	if err != nil {
+		panic(err)
+	}
+	for _, mf := range mfs {
+		if _, err := expfmt.MetricFamilyToText(os.Stderr, mf); err != nil {
+			panic(err)
+		}
+	}
+}
