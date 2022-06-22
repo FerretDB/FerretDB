42006ee372fce21f9fd3dca05e1bf74a7decfc47
Alexey Palazhchenko <alexey.palazhchenko@ferretdb.io>
2022-06-22 14:53:36 +0000

    One more issue

--- internal/handlers/tigris/msg_whatsmyuri.go
+++ internal/handlers/tigris/msg_whatsmyuri.go
@@ -23,6 +23,6 @@ import (
 
 // MsgWhatsMyURI implements HandlerInterface.
 func (h *Handler) MsgWhatsMyURI(ctx context.Context, msg *wire.OpMsg) (*wire.OpMsg, error) {
-	// TODO
+	// TODO https://github.com/FerretDB/FerretDB/issues/692
 	return nil, notImplemented(must.NotFail(msg.Document()).Command())
 }
