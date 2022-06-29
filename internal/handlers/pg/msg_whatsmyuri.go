8134ada6b37eeca71df3c1a200cfe8d4ae6c313c
Elena Grahovac <elena.grahovac@ferretdb.io>
2022-06-29 15:29:22 +0200

    Common implementation for WhatsMyURI

--- internal/handlers/pg/msg_whatsmyuri.go
+++ internal/handlers/pg/msg_whatsmyuri.go
@@ -17,29 +17,12 @@ package pg
 import (
 	"context"
 
-	"github.com/FerretDB/FerretDB/internal/clientconn/info"
+	"github.com/FerretDB/FerretDB/internal/handlers/common"
 
-	"github.com/FerretDB/FerretDB/internal/types"
-	"github.com/FerretDB/FerretDB/internal/util/lazyerrors"
-	"github.com/FerretDB/FerretDB/internal/util/must"
 	"github.com/FerretDB/FerretDB/internal/wire"
 )
 
 // MsgWhatsMyURI implements HandlerInterface.
 func (h *Handler) MsgWhatsMyURI(ctx context.Context, msg *wire.OpMsg) (*wire.OpMsg, error) {
-	var reply wire.OpMsg
-
-	connInfo := info.GetConnInfo(ctx)
-
-	err := reply.SetSections(wire.OpMsgSection{
-		Documents: []*types.Document{must.NotFail(types.NewDocument(
-			"you", connInfo.PeerAddr.String(),
-			"ok", float64(1),
-		))},
-	})
-	if err != nil {
-		return nil, lazyerrors.Error(err)
-	}
-
-	return &reply, nil
+	return common.MsgWhatsMyURI(ctx, msg)
 }
