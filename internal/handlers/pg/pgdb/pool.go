849b49c0cb37efe98c0f39857334e2cfbd53384d
Ekaterina Blech <ekaterina.blech@ferretdb.io>
2022-07-05 18:26:23 +0300

    wip

--- internal/handlers/pg/pgdb/pool.go
+++ internal/handlers/pg/pgdb/pool.go
@@ -352,9 +352,9 @@ func (pgPool *Pool) DropDatabase(ctx context.Context, db string) error {
 // CreateCollection creates a new FerretDB collection in existing schema.
 //
 // It returns:
-// * ErrInvalidTableName if the table name doesn't conform restrictions.
-// * ErrAlreadyExist if table already exist.
-// * ErrTableNotExist is schema does not exist.
+//  - ErrInvalidTableName - if the table name doesn't conform restrictions.
+//  - ErrAlreadyExist if table already exist.
+//  - ErrTableNotExist is schema does not exist.
 func (pgPool *Pool) CreateCollection(ctx context.Context, db, collection string) error {
 	if !regex.MatchString(collection) {
 		return ErrInvalidTableName
