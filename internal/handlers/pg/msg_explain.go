7515b03e2f4ac105add01c3f5d42287eac6131c5
Ekaterina Blech <ekaterina.blech@ferretdb.io>
2022-07-22 10:50:44 +0300

    wip

--- internal/handlers/pg/msg_explain.go
+++ internal/handlers/pg/msg_explain.go
@@ -17,6 +17,7 @@ package pg
 import (
 	"context"
 	"fmt"
+	"net"
 	"os"
 
 	"github.com/jackc/pgx/v4"
@@ -37,44 +38,11 @@ func (h *Handler) MsgExplain(ctx context.Context, msg *wire.OpMsg) (*wire.OpMsg,
 	if err != nil {
 		return nil, lazyerrors.Error(err)
 	}
-	common.Ignored(document, h.l, "verbosity")
-	var sp pgdb.SQLParam
-	if sp.DB, err = common.GetRequiredParam[string](document, "$db"); err != nil {
-		return nil, err
-	}
-	commandParam, err := document.Get(document.Command())
+	sp, err := h.parseExplainUserInput(ctx, document)
 	if err != nil {
-		return nil, err
-	}
-	var command *types.Document
-	var ok bool
-	if command, ok = commandParam.(*types.Document); !ok {
-		return nil, common.NewErrorMsg(
-			common.ErrBadValue,
-			fmt.Sprintf("has invalid type %s", common.AliasFromType(commandParam)),
-		)
+		return nil, lazyerrors.Error(err)
 	}
 
-	switch command.Command() {
-	case "count", "findAndModify", "find":
-		// ok
-	default:
-		return nil, common.NewErrorMsg(
-			common.ErrNotImplemented,
-			fmt.Sprintf("explain for %s s not supported", command.Command()),
-		)
-	}
-	collectionParam, err := command.Get(command.Command())
-	if err != nil {
-		return nil, err
-	}
-	if sp.Collection, ok = collectionParam.(string); !ok {
-		return nil, common.NewErrorMsg(
-			common.ErrBadValue,
-			fmt.Sprintf("collection name has invalid type %s", common.AliasFromType(collectionParam)),
-		)
-	}
-	sp.Explain = true
 	resDocs := make([]*types.Document, 0, 16)
 	err = h.pgPool.InTransaction(ctx, func(tx pgx.Tx) error {
 		fetchedChan, err := h.pgPool.QueryDocuments(ctx, tx, sp)
@@ -99,42 +67,80 @@ func (h *Handler) MsgExplain(ctx context.Context, msg *wire.OpMsg) (*wire.OpMsg,
 		return nil, err
 	}
 
+	return h.buildExplainResult(ctx, document, resDocs)
+}
+
+// parseExplainUserInput validates user input and returns pgdb.SQLParam.
+func (h *Handler) parseExplainUserInput(ctx context.Context, document *types.Document) (pgdb.SQLParam, error) {
+	common.Ignored(document, h.l, "verbosity")
+	var err error
+	var sp pgdb.SQLParam
+	if sp.DB, err = common.GetRequiredParam[string](document, "$db"); err != nil {
+		return sp, err
+	}
+	commandParam, err := document.Get(document.Command())
+	if err != nil {
+		return sp, err
+	}
+	var command *types.Document
+	var ok bool
+	if command, ok = commandParam.(*types.Document); !ok {
+		return sp, common.NewErrorMsg(
+			common.ErrBadValue,
+			fmt.Sprintf("has invalid type %s", common.AliasFromType(commandParam)),
+		)
+	}
+	switch command.Command() {
+	case "count", "findAndModify", "find":
+		// ok
+	default:
+		return sp, common.NewErrorMsg(
+			common.ErrNotImplemented,
+			fmt.Sprintf("explain for %s s not supported", command.Command()),
+		)
+	}
+	collectionParam, err := command.Get(command.Command())
+	if err != nil {
+		return sp, err
+	}
+	if sp.Collection, ok = collectionParam.(string); !ok {
+		return sp, common.NewErrorMsg(
+			common.ErrBadValue,
+			fmt.Sprintf("collection name has invalid type %s", common.AliasFromType(collectionParam)),
+		)
+	}
+	sp.Explain = true
+	return sp, nil
+}
+
+// buildExplainResult build explain response.
+func (h *Handler) buildExplainResult(ctx context.Context, document *types.Document, resDocs []*types.Document) (*wire.OpMsg, error) {
 	hostname, err := os.Hostname()
 	if err != nil {
 		return nil, lazyerrors.Error(err)
 	}
+
+	var port int
 	connInfo := conninfo.GetConnInfo(ctx)
-	var peerAddr string
 	if connInfo.PeerAddr != nil {
-		peerAddr = connInfo.PeerAddr.String()
+		port = connInfo.PeerAddr.(*net.TCPAddr).Port
 	}
-	// TODO get port from peerAddr
+
 	serverInfo := must.NotFail(types.NewDocument(
 		"host", hostname,
-		"port", peerAddr,
+		"port", port,
 		"version", version.MongoDBVersion,
 		"gitVersion", version.Get().Commit,
 		"ferretdbVersion", version.Get().Version,
 	))
 
-	firstBatch := types.MakeArray(len(resDocs))
-	for _, doc := range resDocs {
-		must.NoError(doc.Set("explainVersion", 1))
-		must.NoError(doc.Set("command", document))
-		must.NoError(doc.Set("serverInfo", serverInfo))
-		if err = firstBatch.Append(doc); err != nil {
-			return nil, err
-		}
-	}
-
 	var reply wire.OpMsg
 	err = reply.SetSections(wire.OpMsgSection{
 		Documents: []*types.Document{must.NotFail(types.NewDocument(
-			"cursor", must.NotFail(types.NewDocument(
-				"firstBatch", firstBatch,
-				"id", int64(0), // TODO
-				"ns", sp.DB+"."+sp.Collection,
-			)),
+			"queryPlanner", types.MakeArray(len(resDocs)),
+			"explainVersion", 1,
+			"command", document,
+			"serverInfo", serverInfo,
 			"ok", float64(1),
 		))},
 	})
