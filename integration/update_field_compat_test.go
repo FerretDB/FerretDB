40bc00db390108a2550e8a926dcb2025eeda785e
Alexey Palazhchenko <alexey.palazhchenko@ferretdb.io>
2022-08-01 18:08:21 +0300

    WIP

--- integration/update_field_compat_test.go
+++ integration/update_field_compat_test.go
@@ -26,9 +26,11 @@ func TestUpdateFieldCompatInc(t *testing.T) {
 	testCases := map[string]updateCompatTestCase{
 		"Double": {
 			update: bson.D{{"$inc", bson.D{{"v", 42.13}}}},
+			skip:   "https://github.com/FerretDB/FerretDB/issues/972",
 		},
 		"DoubleNegative": {
 			update: bson.D{{"$inc", bson.D{{"v", -42.13}}}},
+			skip:   "https://github.com/FerretDB/FerretDB/issues/972",
 		},
 	}
 
