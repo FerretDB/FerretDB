49e7332aefc2cecbb4a2153e3d98ce9bf4f9ed9b
w84thesun <dmitry.eremenko@ferretdb.io>
2022-08-03 17:34:00 +0300

    WIP

--- integration/update_field_compat_test.go
+++ integration/update_field_compat_test.go
@@ -38,11 +38,11 @@ func TestUpdateFieldCompatInc(t *testing.T) {
 		},
 		"DotNotationFieldExist": {
 			update: bson.D{{"$inc", bson.D{{"v.foo", int32(1)}}}},
-			skip:   "TODO",
+			skip:   "https://github.com/FerretDB/FerretDB/issues/972",
 		},
 		"DotNotationFieldNotExist": {
 			update: bson.D{{"$inc", bson.D{{"foo.bar", int32(1)}}}},
-			skip:   "TODO",
+			skip:   "https://github.com/FerretDB/FerretDB/issues/972",
 		},
 	}
 
