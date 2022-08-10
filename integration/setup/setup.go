99f05bc645c9ccf1dfb9afdbca711b328ce73780
Alexey Palazhchenko <alexey.palazhchenko@ferretdb.io>
2022-08-10 15:39:14 +0300

    WIP

--- integration/setup/setup.go
+++ integration/setup/setup.go
@@ -28,8 +28,6 @@ import (
 )
 
 // SetupOpts represents setup options.
-//
-// TODO add option to use read-only user: https://github.com/FerretDB/FerretDB/issues/914.
 type SetupOpts struct {
 	// Database to use. If empty, temporary test-specific database is created.
 	DatabaseName string
