9fff1d2d81bf02955dce05c3e31ac066c3ab0cee
noisersup <patryk@kwiatek.xyz>
2022-08-11 15:19:49 +0200

    wip

--- internal/tjson/datetime_test.go
+++ internal/tjson/datetime_test.go
@@ -23,7 +23,7 @@ import (
 
 var dateTimeTestCases = []testCase{{
 	name:   "2021",
-	v:      pointer.To(dateTimeType(time.Date(2021, 11, 1, 10, 18, 42, 123000000, time.UTC).Local())),
+	v:      pointer.To(dateTimeType(time.Date(2021, 11, 1, 10, 18, 42, 123000000, time.UTC))),
 	schema: dateTimeSchema,
 	j:      `"2021-11-01T10:18:42.123Z"`,
 }, {
@@ -33,12 +33,12 @@ var dateTimeTestCases = []testCase{{
 	j:      `"1970-01-01T00:00:00Z"`,
 }, {
 	name:   "0",
-	v:      pointer.To(dateTimeType(time.Date(0, 1, 1, 0, 0, 0, 0, time.UTC).Local())),
+	v:      pointer.To(dateTimeType(time.Date(0, 1, 1, 0, 0, 0, 0, time.UTC))),
 	schema: dateTimeSchema,
 	j:      `"0000-01-01T00:00:00Z"`,
 }, {
 	name:   "9999",
-	v:      pointer.To(dateTimeType(time.Date(9999, 12, 31, 23, 59, 59, 999000000, time.UTC).Local())),
+	v:      pointer.To(dateTimeType(time.Date(9999, 12, 31, 23, 59, 59, 999000000, time.UTC))),
 	schema: dateTimeSchema,
 	j:      `"9999-12-31T23:59:59.999Z"`,
 }, {
