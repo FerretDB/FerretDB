4e01df3776a43ab93141f952d7e4241846f9e3dd
noisersup <patryk@kwiatek.xyz>
2022-08-22 17:23:21 +0200

    wip

--- internal/clientconn/conn.go
+++ internal/clientconn/conn.go
@@ -324,17 +324,30 @@ func (c *conn) route(ctx context.Context, reqHeader *wire.MsgHeader, reqBody wir
 			protoErr, recoverable := common.ProtocolError(err)
 			closeConn = !recoverable
 
-			doc, err := resBody.(*wire.OpMsg).Document()
-			if err != nil {
-				panic(err)
+			msg, ok := resBody.(*wire.OpMsg)
+			if !ok {
+				//TODO: message about type
+				panic(msg)
 			}
-
-			if _, err := doc.Get("writeErrors"); err != nil {
+			if msg == nil {
 				var res wire.OpMsg
 				must.NoError(res.SetSections(wire.OpMsgSection{
 					Documents: []*types.Document{protoErr.Document()},
 				}))
 				resBody = &res
+			} else {
+				doc, err := resBody.(*wire.OpMsg).Document()
+				if err != nil {
+					panic(err)
+				}
+
+				if _, err := doc.Get("writeErrors"); err != nil {
+					var res wire.OpMsg
+					must.NoError(res.SetSections(wire.OpMsgSection{
+						Documents: []*types.Document{protoErr.Document()},
+					}))
+					resBody = &res
+				}
 			}
 
 			result = pointer.ToString(protoErr.Code().String())
