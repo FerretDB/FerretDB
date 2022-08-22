a9f33d5311009dd67aa358493bdcc9dee09fe705
Elena Grahovac <elena@grahovac.me>
2022-08-22 16:43:41 +0200

    respect strict cases in tests

--- integration/create_test.go
+++ integration/create_test.go
@@ -44,7 +44,7 @@ func TestCreateTigris(t *testing.T) {
 			expectedErr: &mongo.CommandError{
 				Code:    2,
 				Name:    "BadValue",
-				Message: "collection name is not same as schema name 'TestCreateTigris' ''",
+				Message: "invalid schema, the follwing keys are not supported: [bsonType]",
 			},
 		},
 		"GoodValidator": {
