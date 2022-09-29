7faf3d706d896a0060a2ce567df0983328c5ff45
Elena Grahovac <elena@grahovac.me>
2022-09-29 21:47:50 +0200

    task fmt

--- integration/query_logical_compat_test.go
+++ integration/query_logical_compat_test.go
@@ -17,9 +17,8 @@ package integration
 import (
 	"testing"
 
-	"go.mongodb.org/mongo-driver/bson/primitive"
-
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func TestQueryLogicalCompatAnd(t *testing.T) {
