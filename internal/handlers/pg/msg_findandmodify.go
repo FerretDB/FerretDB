7911c7fcd869c7cf92cce7de56af0042da015fc9
Dmitry <dmitry.eremenko@ferretdb.io>
2022-10-10 08:10:54 +0100

    Add simple query pushdown (#1207)
    This PR closes #896.

--- internal/handlers/pg/msg_findandmodify.go
+++ internal/handlers/pg/msg_findandmodify.go
@@ -68,6 +68,7 @@ func (h *Handler) MsgFindAndModify(ctx context.Context, msg *wire.OpMsg) (*wire.
 		DB:         params.DB,
 		Collection: params.Collection,
 		Comment:    params.Comment,
+		Filter:     params.Query,
 	}
 
 	// This is not very optimal as we need to fetch everything from the database to have a proper sort.
