377eebed7ddd42e192a2357cb0ec1420a9fae568
Alexey Palazhchenko <alexey.palazhchenko@ferretdb.io>
2022-10-10 10:06:50 +0200

    Fix compilation

--- internal/handlers/pg/pgdb/query.go
+++ internal/handlers/pg/pgdb/query.go
@@ -209,7 +209,7 @@ func prepareWhereClause(sqlFilters *types.Document) (string, []any) {
 			case types.ObjectID:
 				filters = append(filters, fmt.Sprintf(`((_jsonb->'_id')::jsonb = %s)`, p.Next()))
 
-				args = append(args, string(must.NotFail(fjson.Marshal(v))))
+				args = append(args, string(must.NotFail(pjson.Marshal(v))))
 			}
 		default:
 			continue
