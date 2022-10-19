90adb7abb0ac6c5006441251dc9957ceb467acbf
noisersup <patryk@kwiatek.xyz>
2022-10-19 15:21:04 +0200

    wip

--- internal/types/document_validation.go
+++ internal/types/document_validation.go
@@ -38,7 +38,8 @@ func (e *ValidationError) Error() string {
 // ValidateData checks if the document represents a valid "data document".
 // If the document is not valid it returns *ValidationError.
 func (d *Document) ValidateData() error {
-	idPresent := false
+	var idPresent bool
+
 	// The following block should be used to checks that keys are valid.
 	// All further key related validation rules should be added here.
 	for _, key := range d.keys {
