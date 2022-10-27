24243fc42d9cd594ee0ffce48e80615cc127782a
noisersup <patryk@kwiatek.xyz>
2022-10-27 17:50:48 +0200

    wip

--- internal/types/document_validation_test.go
+++ internal/types/document_validation_test.go
@@ -19,9 +19,8 @@ import (
 	"math"
 	"testing"
 
-	"github.com/stretchr/testify/require"
-
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/FerretDB/FerretDB/internal/util/must"
 )
