c1cafe3aea1c0b846432559d9a170cc5284ba19e
Alexander Tobi Fashakin <alexander.fashakin@ferretdb.io>
2022-11-07 15:03:21 +0100

    update title slug for documentation

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
       <main>
