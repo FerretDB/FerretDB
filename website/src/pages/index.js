da29fc0706d346b27ef900947f331a11101d26a2
Alexander Tobi Fashakin <alexander.fashakin@ferretdb.io>
2022-11-15 12:39:16 +0100

    Add documentation for dot notation in arrays, objects, and embedded documents (#1382)
    Closes #1310.

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -30,8 +30,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="Explore the installation guide, tutorials, and references on how to use FerretDB.">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
