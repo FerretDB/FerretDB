21254ef0cdda6b6670a670de948c2fe4da008253
w84thesun <dmitry.eremenko@ferretdb.io>
2022-11-23 18:06:09 +0000

    WIP

--- internal/wire/op_query.go
+++ internal/wire/op_query.go
@@ -42,6 +42,7 @@ type OpQuery struct {
 func (query *OpQuery) msgbody() {}
 
 // readFrom composes an OpQuery from a buffered reader.
+// It may return ValidationError if the document read from bufr is invalid.
 func (query *OpQuery) readFrom(bufr *bufio.Reader) error {
 	if err := binary.Read(bufr, binary.LittleEndian, &query.Flags); err != nil {
 		return lazyerrors.Errorf("wire.OpQuery.ReadFrom (binary.Read): %w", err)
