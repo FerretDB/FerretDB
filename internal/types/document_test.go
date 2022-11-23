321e2dcf62b5956e7cf7498a6ff904fb30ffd977
w84thesun <dmitry.eremenko@ferretdb.io>
2022-11-23 11:27:59 +0000

    WIP

--- internal/types/document_test.go
+++ internal/types/document_test.go
@@ -75,7 +75,7 @@ func TestDocument(t *testing.T) {
 		assert.Equal(t, int32(42), must.NotFail(b.Get("foo")))
 	})
 
-	t.Run("SetID", func(t *testing.T) {
+	t.Run("MoveIDToTheFirstIndex", func(t *testing.T) {
 		t.Parallel()
 
 		doc := must.NotFail(NewDocument(
@@ -88,9 +88,7 @@ func TestDocument(t *testing.T) {
 			"foo", "bar",
 			"_id", int32(42),
 		))
-		assert.Equal(t, []string{"_id", "foo"}, doc.Keys())
-
-		doc.Set("_id", "bar")
+		doc.MoveIDToTheFirstIndex()
 		assert.Equal(t, []string{"_id", "foo"}, doc.Keys())
 	})
 
