4594b6530a50f1c86ce8dfb415a796783f987a89
w84thesun <dmitry.eremenko@ferretdb.io>
2022-11-25 12:20:39 +0000

    WIP

--- internal/handlers/tigris/msg_explain.go
+++ internal/handlers/tigris/msg_explain.go
@@ -16,12 +16,87 @@ package tigris
 
 import (
 	"context"
+	"net"
+	"os"
 
+	"github.com/FerretDB/FerretDB/internal/clientconn/conninfo"
 	"github.com/FerretDB/FerretDB/internal/handlers/common"
+	"github.com/FerretDB/FerretDB/internal/types"
+	"github.com/FerretDB/FerretDB/internal/util/lazyerrors"
+	"github.com/FerretDB/FerretDB/internal/util/must"
+	"github.com/FerretDB/FerretDB/internal/util/version"
 	"github.com/FerretDB/FerretDB/internal/wire"
 )
 
 // MsgExplain implements HandlerInterface.
 func (h *Handler) MsgExplain(ctx context.Context, msg *wire.OpMsg) (*wire.OpMsg, error) {
-	return nil, common.NewCommandErrorMsg(common.ErrNotImplemented, "`explain` command is not implemented yet")
+	document, err := msg.Document()
+	if err != nil {
+		return nil, lazyerrors.Error(err)
+	}
+
+	db, err := common.GetRequiredParam[string](document, "$db")
+	if err != nil {
+		return nil, lazyerrors.Error(err)
+	}
+
+	command, err := common.GetRequiredParam[*types.Document](document, document.Command())
+	if err != nil {
+		return nil, lazyerrors.Error(err)
+	}
+
+	explain, err := common.GetRequiredParam[*types.Document](document, "explain")
+	if err != nil {
+		return nil, lazyerrors.Error(err)
+	}
+
+	filter, err := common.GetOptionalParam[*types.Document](explain, "filter", nil)
+	if err != nil {
+		return nil, lazyerrors.Error(err)
+	}
+
+	queryPlanner := must.NotFail(types.NewDocument(
+		"Filter", string(h.db.BuildFilter(filter)),
+	))
+
+	hostname, err := os.Hostname()
+	if err != nil {
+		return nil, lazyerrors.Error(err)
+	}
+
+	var port int32
+	connInfo := conninfo.GetConnInfo(ctx)
+	if connInfo.PeerAddr != nil {
+		if tcpAddr, ok := connInfo.PeerAddr.(*net.TCPAddr); ok {
+			port = int32(tcpAddr.Port)
+		}
+	}
+
+	serverInfo := must.NotFail(types.NewDocument(
+		"host", hostname,
+		"port", port,
+		"version", version.MongoDBVersion,
+		"gitVersion", version.Get().Commit,
+		"ferretdbVersion", version.Get().Version,
+	))
+
+	cmd := command.DeepCopy()
+	cmd.Set("$db", db)
+
+	var reply wire.OpMsg
+
+	err = reply.SetSections(wire.OpMsgSection{
+		Documents: []*types.Document{must.NotFail(types.NewDocument(
+			"queryPlanner", queryPlanner,
+			"explainVersion", "1",
+			"command", cmd,
+			"serverInfo", serverInfo,
+			"ok", float64(1),
+		))},
+	})
+	if err != nil {
+		return nil, lazyerrors.Error(err)
+	}
+
+	return &reply, nil
 }
