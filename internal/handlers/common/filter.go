3cdd0bee253c2d36366be23e5d7d5379f3a86e55
chilagrow <chi.fujii@ferretdb.io>
2022-11-30 09:47:04 +0900

    Move query tests to compat by comparing the error code instead of the error message (#1579)

--- internal/handlers/common/filter.go
+++ internal/handlers/common/filter.go
@@ -593,7 +593,7 @@ func filterFieldRegex(fieldValue any, regex types.Regex) (bool, error) {
 		if !slices.Contains([]rune{'i', 'm', 's', 'x'}, option) {
 			return false, NewCommandErrorMsgWithArgument(
 				ErrBadRegexOption,
-				fmt.Sprintf("invalid flag in regex options: %c", option),
+				fmt.Sprintf(" invalid flag in regex options: %c", option),
 				"$options",
 			)
 		}
