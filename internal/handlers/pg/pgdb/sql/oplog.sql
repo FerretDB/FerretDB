3c5b1d23248cb034a4bfbe8fe3814fbc6f423cfb
Alexey Palazhchenko <alexey.palazhchenko@ferretdb.io>
2022-12-01 11:57:22 +0400

    Add OpLog prototype

---
+++ internal/handlers/pg/pgdb/sql/oplog.sql
@@ -0,0 +1,19 @@
+CREATE OR REPLACE FUNCTION oplog_insert_trigger_func() RETURNS trigger AS $$
+
+DECLARE
+    oplog_record oplog%ROWTYPE;
+
+BEGIN
+    SELECT nextval('oplog_id_seq') INTO oplog_record.id;
+    oplog_record.table_name := TG_TABLE_NAME;
+    oplog_record.operation := 'INSERT';
+    oplog_record.row_data := row_to_json(NEW);
+    INSERT INTO oplog VALUES (oplog_record);
+    RETURN NEW;
+END;
+
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER oplog_insert_trigger
+    AFTER INSERT ON oplog
+    FOR EACH ROW EXECUTE PROCEDURE oplog_insert_trigger_func();
