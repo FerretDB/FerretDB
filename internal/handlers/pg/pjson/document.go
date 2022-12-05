bc7c5fc353b6ca916f87799fc7255c7e13065a07
Elena Grahovac <elena@grahovac.me>
2022-12-05 14:53:59 +0100

    wip

--- internal/handlers/pg/pjson/document.go
+++ internal/handlers/pg/pjson/document.go
@@ -17,6 +17,8 @@ package pjson
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
+	"time"
 
 	"github.com/FerretDB/FerretDB/internal/types"
 	"github.com/FerretDB/FerretDB/internal/util/lazyerrors"
@@ -89,7 +91,46 @@ func (doc *documentType) MarshalJSON() ([]byte, error) {
 
 	var buf bytes.Buffer
 
-	// TODO
+	buf.WriteString(`{"$s":`)
+	buf.Write(must.NotFail(marshalSchema(&td)))
+
+	keys := td.Keys()
+
+	for _, key := range keys {
+		buf.WriteByte(',')
+
+		var b []byte
+		var err error
+
+		if b, err = json.Marshal(key); err != nil {
+			return nil, lazyerrors.Error(err)
+		}
+
+		buf.Write(b)
+		buf.WriteByte(':')
+
+		value, err := td.Get(key)
+		if err != nil {
+			return nil, lazyerrors.Error(err)
+		}
+
+		b, err = Marshal(value)
+		if err != nil {
+			return nil, lazyerrors.Error(err)
+		}
+
+		buf.Write(b)
+	}
+
+	buf.WriteByte('}')
+
+	return buf.Bytes(), nil
+}
+
+// marshalSchema marshals document's schema.
+func marshalSchema(td *types.Document) (json.RawMessage, error) {
+	var buf bytes.Buffer
+
 	buf.WriteString(`{"$k":`)
 
 	keys := td.Keys()
@@ -114,9 +155,63 @@ func (doc *documentType) MarshalJSON() ([]byte, error) {
 		buf.Write(b)
 		buf.WriteByte(':')
 
-		value, err := td.Get(key)
-		if err != nil {
-			return nil, lazyerrors.Error(err)
+		value := must.NotFail(td.Get(key))
+
+		switch val := value.(type) {
+		case *types.Document:
+			buf.WriteString(`{"t": "object", "$s":`)
+
+			b, err := marshalSchema(val)
+			if err != nil {
+				return nil, lazyerrors.Error(err)
+			}
+
+			buf.Write(b)
+
+			buf.WriteByte('}')
+
+		case *types.Array:
+			buf.WriteString(`{"t": "array", "$s":`)
+
+			// todo recursive schema for each element
+
+			buf.WriteByte('}')
+
+		case float64:
+			buf.WriteString(`{"t": "double"}`)
+
+		case string:
+			buf.WriteString(`{"t": "string"}`)
+
+		case types.Binary:
+			buf.WriteString(`{"t": "binData", "s": 0}`) // todo
+
+		case types.ObjectID:
+			buf.WriteString(`{"t": "objectId"}`)
+
+		case bool:
+			buf.WriteString(`{"t": "bool"}`)
+
+		case time.Time:
+			buf.WriteString(`{"t": "date"}`)
+
+		case types.NullType:
+			buf.WriteString(`{"t": "null"}`)
+
+		case types.Regex:
+			buf.WriteString(`{"t": "regex", "o": ""}`) // todo
+
+		case int32:
+			buf.WriteString(`{"t": "int"}`)
+
+		case types.Timestamp:
+			buf.WriteString(`{"t": "timestamp"}`)
+
+		case int64:
+			buf.WriteString(`{"t": "long"}`)
+
+		default:
+			panic(fmt.Sprintf("pjson.marshalSchema: unknown type %[1]T (value %[1]q)", val))
 		}
 
 		b, err := Marshal(value)
