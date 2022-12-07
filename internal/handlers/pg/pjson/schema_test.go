fa1e0833add3043803e778d182111e0147133ca3
Elena Grahovac <elena@grahovac.me>
2022-12-07 18:24:16 +0100

    more examples

--- internal/handlers/pg/pjson/schema_test.go
+++ internal/handlers/pg/pjson/schema_test.go
@@ -25,99 +25,129 @@ import (
 )
 
 func TestSchemaMarshalUnmarshal(t *testing.T) {
-	expected := schema{
-		Keys: []string{"_id", "arr", "data", "distance", "name"},
-		Properties: map[string]*elem{
-			"_id": objectIDSchema,
-			"arr": {
-				Type: schemaTypeArray,
-				Items: []*elem{
-					boolSchema,
-					dateSchema,
-					regexSchema("i"),
-					{
+	t.Parallel()
+
+	for name, tc := range map[string]struct {
+		schema schema
+		json   string
+	}{
+		"AllTypes": {
+			schema: schema{
+				Keys: []string{"_id", "arr", "data", "distance", "name"},
+				Properties: map[string]*elem{
+					"_id": objectIDSchema,
+					"arr": {
+						Type: schemaTypeArray,
+						Items: []*elem{
+							boolSchema,
+							dateSchema,
+							regexSchema("i"),
+							{
+								Type: schemaTypeObject,
+								Schema: &schema{
+									Keys: []string{"bar", "baz", "arr"},
+									Properties: map[string]*elem{
+										"bar": nullSchema,
+										"baz": longSchema,
+										"arr": {
+											Type: schemaTypeArray,
+											Items: []*elem{
+												intSchema,
+												timestampSchema,
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+					"data":     binDataSchema(byte(types.BinaryFunction)),
+					"distance": doubleSchema,
+					"name":     stringSchema,
+				},
+			},
+			json: `{
+				"$k": ["_id", "arr", "data", "distance", "name"],
+				"p": {
+					"_id": {"t": "objectId"},
+					"arr": {"t": "array", "i": [
+						{"t": "bool"},
+						{"t": "date"},
+						{"t": "regex", "o": "i"},
+						{"t": "object", "$s": {
+							"$k": ["bar", "baz", "arr"],
+							"p": {
+								"arr": {"t": "array", "i": [
+									{"t": "int"}, 
+									{"t": "timestamp"}
+								]},
+								"bar": {"t": "null"},
+								"baz": {"t": "long"}
+							}
+						}}
+					]},
+					"data": {"t": "binData", "s": 1},
+					"distance": {"t": "double"},
+					"name": {"t": "string"}
+				}
+			}`,
+		},
+		"Embedded": {
+			schema: schema{
+				Keys: []string{"obj"},
+				Properties: map[string]*elem{
+					"obj": {
 						Type: schemaTypeObject,
 						Schema: &schema{
-							Keys: []string{"bar", "baz", "arr"},
+							Keys: []string{"arr", "empty-arr"},
 							Properties: map[string]*elem{
-								"bar": nullSchema,
-								"baz": longSchema,
 								"arr": {
 									Type: schemaTypeArray,
 									Items: []*elem{
-										intSchema,
-										timestampSchema,
+										{
+											Type: schemaTypeObject,
+										},
 									},
+								},
+								"empty-arr": {
+									Type: schemaTypeArray,
 								},
 							},
 						},
 					},
 				},
 			},
-			"data":     binDataSchema(byte(types.BinaryFunction)),
-			"distance": doubleSchema,
-			"name":     stringSchema,
+			json: `{
+				"$k": ["obj"],
+				"p": {
+					"obj": {"t": "object", "$s": {
+						"$k": ["arr", "empty-arr"],
+						"p": {	
+							"arr": {"t": "array", "i": [{"t": "object"}]},
+							"empty-arr": {"t": "array"}				
+						}
+					}}
+				}
+			}`,
 		},
+	} {
+		tc := tc
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			actualB, err := tc.schema.Marshal()
+			require.NoError(t, err)
+			actualB = testutil.IndentJSON(t, actualB)
+
+			expectedB := testutil.IndentJSON(t, []byte(tc.json))
+			require.Equal(t, string(expectedB), string(actualB))
+
+			var actual schema
+			err = actual.Unmarshal(expectedB)
+			require.NoError(t, err)
+
+			assert.Equal(t, tc.schema, actual)
+		})
 	}
-
-	actualB, err := expected.Marshal()
-	require.NoError(t, err)
-	actualB = testutil.IndentJSON(t, actualB)
-
-	expectedB := testutil.IndentJSON(t, []byte(`{
-		"$k": ["_id", "arr", "data", "distance", "name"],
-		"p": {
-			"_id": {"t": "objectId"},
-			"arr": {"t": "array", "i": [
-				{"t": "bool"},
-				{"t": "date"},
-				{"t": "regex", "o": "i"},
-				{"t": "object", "$s": {
-					"$k": ["bar", "baz", "arr"],
-					"p": {
-						"arr": {"t": "array", "i": [
-							{"t": "int"}, 
-							{"t": "timestamp"}
-						]},
-						"bar": {"t": "null"},
-						"baz": {"t": "long"}
-					}
-				}}
-			]},
-			"data": {"t": "binData", "s": 1},
-			"distance": {"t": "double"},
-			"name": {"t": "string"}
-		}
-	}`))
-
-	require.Equal(t, string(expectedB), string(actualB))
-
-	var actual schema
-	err = actual.Unmarshal(expectedB)
-	require.NoError(t, err)
-
-	assert.Equal(t, expected, actual)
-}
-
-func TestSchemaUnmarshal(t *testing.T) {
-	var actual schema
-	err := actual.Unmarshal([]byte(`{
-		"$k": ["_id", "data", "distance", "name"], 
-		"_id": {"t": "objectId"},
-		"data": {"t": "binData", "s": 1}, 
-		"distance": {"t": "double"}, 
-		"name": {"t": "string"}
-	}`))
-	require.NoError(t, err)
-
-	expected := &schema{
-		Keys: []string{"_id", "data", "distance", "name"},
-		Properties: map[string]*elem{
-			"_id":      objectIDSchema,
-			"data":     binDataSchema(byte(types.BinaryFunction)),
-			"distance": doubleSchema,
-			"name":     stringSchema,
-		},
-	}
-	assert.Equal(t, expected, &actual)
 }
