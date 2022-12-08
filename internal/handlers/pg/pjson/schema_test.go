a1052890c57d6de71658c6d7edb54b2ad07fec3b
Elena Grahovac <elena@grahovac.me>
2022-12-08 12:18:31 +0100

    wip

--- internal/handlers/pg/pjson/schema_test.go
+++ internal/handlers/pg/pjson/schema_test.go
@@ -33,7 +33,6 @@ func TestSchemaMarshalUnmarshal(t *testing.T) {
 	}{
 		"AllTypes": {
 			schema: schema{
-				Keys: []string{"_id", "arr", "data", "distance", "name"},
 				Properties: map[string]*elem{
 					"_id": objectIDSchema,
 					"arr": {
@@ -45,7 +44,6 @@ func TestSchemaMarshalUnmarshal(t *testing.T) {
 							{
 								Type: elemTypeObject,
 								Schema: &schema{
-									Keys: []string{"bar", "baz", "arr"},
 									Properties: map[string]*elem{
 										"bar": nullSchema,
 										"baz": longSchema,
@@ -57,6 +55,7 @@ func TestSchemaMarshalUnmarshal(t *testing.T) {
 											},
 										},
 									},
+									Keys: []string{"bar", "baz", "arr"},
 								},
 							},
 						},
@@ -65,9 +64,9 @@ func TestSchemaMarshalUnmarshal(t *testing.T) {
 					"distance": doubleSchema,
 					"name":     stringSchema,
 				},
+				Keys: []string{"_id", "arr", "data", "distance", "name"},
 			},
 			json: `{
-				"$k": ["_id", "arr", "data", "distance", "name"],
 				"p": {
 					"_id": {"t": "objectId"},
 					"arr": {"t": "array", "i": [
@@ -75,7 +74,6 @@ func TestSchemaMarshalUnmarshal(t *testing.T) {
 						{"t": "date"},
 						{"t": "regex", "o": "i"},
 						{"t": "object", "$s": {
-							"$k": ["bar", "baz", "arr"],
 							"p": {
 								"arr": {"t": "array", "i": [
 									{"t": "int"}, 
@@ -83,23 +81,23 @@ func TestSchemaMarshalUnmarshal(t *testing.T) {
 								]},
 								"bar": {"t": "null"},
 								"baz": {"t": "long"}
-							}
+							},
+							"$k": ["bar", "baz", "arr"]
 						}}
 					]},
 					"data": {"t": "binData", "s": 1},
 					"distance": {"t": "double"},
 					"name": {"t": "string"}
-				}
+				},
+				"$k": ["_id", "arr", "data", "distance", "name"]
 			}`,
 		},
 		"Embedded": {
 			schema: schema{
-				Keys: []string{"obj"},
 				Properties: map[string]*elem{
 					"obj": {
 						Type: elemTypeObject,
 						Schema: &schema{
-							Keys: []string{"arr", "empty-arr"},
 							Properties: map[string]*elem{
 								"arr": {
 									Type: elemTypeArray,
@@ -107,27 +105,45 @@ func TestSchemaMarshalUnmarshal(t *testing.T) {
 										{
 											Type: elemTypeObject,
 										},
+										{
+											Type: elemTypeObject,
+											Schema: &schema{
+												Properties: map[string]*elem{
+													"foo": {
+														Type:  elemTypeArray,
+														Items: []*elem{},
+													},
+												},
+												Keys: []string{"foo"},
+											},
+										},
 									},
 								},
 								"empty-arr": {
 									Type: elemTypeArray,
 								},
 							},
+							Keys: []string{"arr", "empty-arr"},
 						},
 					},
 				},
+				Keys: []string{"obj"},
 			},
 			json: `{
-				"$k": ["obj"],
 				"p": {
 					"obj": {"t": "object", "$s": {
-						"$k": ["arr", "empty-arr"],
 						"p": {	
-							"arr": {"t": "array", "i": [{"t": "object"}]},
+							"arr": {"t": "array", "i": [
+								{"t": "object"}, {"t": "object", "$s": {
+									"p": {"foo": {"t": "array"}}, "$k": ["foo"]
+								}}
+							]},
 							"empty-arr": {"t": "array"}				
-						}
+						},
+						"$k": ["arr", "empty-arr"]
 					}}
-				}
+				},
+				"$k": ["obj"]
 			}`,
 		},
 	} {
