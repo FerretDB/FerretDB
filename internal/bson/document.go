ad79bf4009c1d364cf85b61a999a60ee7c029503
Alexey Palazhchenko <alexey.palazhchenko@ferretdb.io>
2022-12-10 10:26:22 +0400

    Add TODOs

--- internal/bson/document.go
+++ internal/bson/document.go
@@ -180,7 +180,7 @@ func (doc *Document) ReadFrom(r *bufio.Reader) error {
 
 		switch tag(t) {
 		case tagDocument:
-			// TODO check maximum nesting
+			// TODO check maximum nesting https://github.com/FerretDB/FerretDB/issues/1639
 
 			var v Document
 			if err := v.ReadFrom(bufr); err != nil {
@@ -195,7 +195,7 @@ func (doc *Document) ReadFrom(r *bufio.Reader) error {
 			fields = append(fields, field{key: key, value: value})
 
 		case tagArray:
-			// TODO check maximum nesting
+			// TODO check maximum nesting https://github.com/FerretDB/FerretDB/issues/1639
 
 			var v arrayType
 			if err := v.ReadFrom(bufr); err != nil {
