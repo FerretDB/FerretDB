ce030820e6c44e7ddd7f33ccebafec27db92f5ba
Dmitry <dmitry.eremenko@ferretdb.io>
2022-12-13 14:14:26 +0000

    WIP

--- internal/clientconn/listener.go
+++ internal/clientconn/listener.go
@@ -209,8 +209,6 @@ func setupTLSListener(addr, certFile, keyFile string) (net.Listener, error) {
 		return nil, lazyerrors.Error(err)
 	}
 
-	fmt.Println("TLS listener address " + listener.Addr().String())
-
 	return listener, nil
 }
 
