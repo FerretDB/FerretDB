a47703319f3043ca3f10c1d50f5fffc95698ee26
Alexey Palazhchenko <alexey.palazhchenko@ferretdb.io>
2022-12-20 14:54:38 +0400

    WIP

---
+++ build/legacy-mongo-shell/mongorc.js
@@ -0,0 +1,29 @@
+// Please do not commit changes in this file.
+
+(function() {
+    "use strict";
+
+    const col = db.test;
+
+    col.drop();
+
+    const init = [
+        { _id: "double", v: 42.13 },
+        { _id: "double-whole", v: 42.0 },
+        { _id: "double-zero", v: 0.0 },
+    ];
+
+    col.insertMany(init);
+
+    const query = { v: { $gt: 42.0 } };
+
+    const expected = [
+        { _id: "double", v: 42.13 },
+    ];
+
+    const actual = col.find(query).toArray();
+
+    assert.eq(expected, actual);
+
+    print("mongorc.js test passed!")
+})();
