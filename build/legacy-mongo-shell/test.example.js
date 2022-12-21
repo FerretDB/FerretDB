81900f97e5206a293551cee8883bb21f9631d92b
Alexey Palazhchenko <alexey.palazhchenko@ferretdb.io>
2022-12-21 11:33:20 +0400

    Format

--- build/legacy-mongo-shell/test.example.js
+++ build/legacy-mongo-shell/test.example.js
@@ -1,28 +1,28 @@
 // Move to test.js and run with `task mongo-test`.
 
-(function() {
-    "use strict";
+(function () {
+  'use strict';
 
-    const col = db.test;
+  const col = db.test;
 
-    col.drop();
+  col.drop();
 
-    const init = [
-        { _id: "double", v: 42.13 },
-        { _id: "double-whole", v: 42.0 },
-        { _id: "double-zero", v: 0.0 },
-    ];
+  const init = [
+    { _id: 'double', v: 42.13 },
+    { _id: 'double-whole', v: 42.0 },
+    { _id: 'double-zero', v: 0.0 },
+  ];
 
-    col.insertMany(init);
+  col.insertMany(init);
 
-    const query = { v: { $gt: 42.0 } };
+  const query = { v: { $gt: 42.0 } };
 
-    const expected = [
-        { _id: "double", v: 42.13 },
-    ];
+  const expected = [
+    { _id: 'double', v: 42.13 },
+  ];
 
-    const actual = col.find(query).toArray();
-    assert.eq(expected, actual);
+  const actual = col.find(query).toArray();
+  assert.eq(expected, actual);
 
-    print("test.js passed!")
+  print('test.js passed!');
 })();
