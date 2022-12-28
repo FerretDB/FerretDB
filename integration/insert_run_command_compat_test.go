ac76aa31aaa16326469a4793c24bc0646bab286f
Elena Grahovac <elena@grahovac.me>
2022-12-28 14:21:04 +0100

    tests

--- integration/insert_run_command_compat_test.go
+++ integration/insert_run_command_compat_test.go
@@ -19,7 +19,6 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-
 	"go.mongodb.org/mongo-driver/bson"
 
 	"github.com/FerretDB/FerretDB/integration/setup"
