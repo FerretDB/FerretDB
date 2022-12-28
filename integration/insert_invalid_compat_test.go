df245223d706921256f5c75e1df44a78f7fc8009
Elena Grahovac <elena@grahovac.me>
2022-12-28 10:14:06 +0100

    test invalid ordered param

---
+++ integration/insert_invalid_compat_test.go
@@ -0,0 +1,88 @@
+// Copyright 2021 FerretDB Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package integration
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.mongodb.org/mongo-driver/bson"
+
+	"github.com/FerretDB/FerretDB/integration/setup"
+)
+
+type insertInvalidCompatTestCase struct {
+	documents []any // required, slice of bson.D to be insert
+	ordered   any   // required, ordered parameter
+}
+
+// testInsertInvalidCompat tests insert compatibility test cases with invalid parameters.
+// It uses runCommand instead of insertOne or insertMany to set more invalid parameters.
+func testInsertInvalidCompat(t *testing.T, testCases map[string]insertInvalidCompatTestCase) {
+	t.Helper()
+
+	for name, tc := range testCases {
+		name, tc := name, tc
+		t.Run(name, func(t *testing.T) {
+			t.Helper()
+
+			t.Parallel()
+
+			ctx, targetCollections, compatCollections := setup.SetupCompat(t)
+
+			for i := range targetCollections {
+				targetCollection := targetCollections[i]
+				compatCollection := compatCollections[i]
+				t.Run(targetCollection.Name(), func(t *testing.T) {
+					t.Helper()
+
+					// RunCommand must be used to give ability set various invalid parameters.
+					var targetRes, compatRes bson.D
+					targetErr := targetCollection.Database().RunCommand(ctx, bson.D{
+						{"insert", targetCollection.Name()},
+						{"documents", tc.documents},
+						{"ordered", tc.ordered},
+					}).Decode(&targetRes)
+					compatErr := compatCollection.Database().RunCommand(ctx, bson.D{
+						{"insert", compatCollection.Name()},
+						{"documents", tc.documents},
+						{"ordered", tc.ordered},
+					}).Decode(&compatRes)
+
+					require.NotNil(t, targetErr)
+					require.Equal(t, compatErr, targetErr)
+
+					assert.Equal(t, compatRes, targetRes)
+				})
+			}
+		})
+	}
+}
+
+func TestInsertInvalidCompat(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]insertInvalidCompatTestCase{
+		"InsertOrderedInvalid": {
+			documents: []any{
+				bson.D{{"_id", "foo"}},
+			},
+			ordered: "foo",
+		},
+	}
+
+	testInsertInvalidCompat(t, testCases)
+}
