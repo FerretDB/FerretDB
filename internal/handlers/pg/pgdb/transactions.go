c1420dffa5732a7cb5f6dd04f69b88465aca3e45
Alexey Palazhchenko <alexey.palazhchenko@ferretdb.io>
2022-12-29 16:55:52 +0400

    Add issue

--- internal/handlers/pg/pgdb/transactions.go
+++ internal/handlers/pg/pgdb/transactions.go
@@ -85,8 +85,8 @@ func (pgPool *Pool) InTransaction(ctx context.Context, f func(pgx.Tx) error) (er
 // If f returns (possibly wrapped) *transactionConflictError, the transaction is retried multiple times with delays.
 // If the transaction still fails after that, the last error is returned.
 func (pgPool *Pool) InTransactionRetry(ctx context.Context, f func(pgx.Tx) error) error {
-	// TODO use exponential backoff instead
-	// https://aws.amazon.com/ru/blogs/architecture/exponential-backoff-and-jitter/
+	// TODO use exponential backoff with jitter instead
+	// https://github.com/FerretDB/FerretDB/issues/1720
 	const (
 		retriesMax    = 20
 		retryDelayMin = 100 * time.Millisecond
