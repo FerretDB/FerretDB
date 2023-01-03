7cc8930ffb6c485e99a2c2922bc0e6ade796864b
Elena Grahovac <elena@grahovac.me>
2023-01-03 15:56:25 +0100

    Distinct compat tests

---
+++ integration/distinct_compat_test.go
@@ -0,0 +1,124 @@
+// Copyright 2021 FerretDB Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package integration
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.mongodb.org/mongo-driver/bson"
+
+	"github.com/FerretDB/FerretDB/integration/setup"
+	"github.com/FerretDB/FerretDB/integration/shareddata"
+)
+
+// distinctCompatTestCase describes count compatibility test case.
+type distinctCompatTestCase struct {
+	field      string                   // required
+	filter     bson.D                   // required
+	resultType compatTestCaseResultType // defaults to nonEmptyResult
+}
+
+func testDistinctCompat(t *testing.T, testCases map[string]distinctCompatTestCase) {
+	t.Helper()
+
+	// Use shared setup because distinct queries can't modify data.
+	// TODO Use read-only user. https://github.com/FerretDB/FerretDB/issues/1025
+	s := setup.SetupCompatWithOpts(t, &setup.SetupCompatOpts{
+		Providers:                shareddata.AllProviders(),
+		AddNonExistentCollection: true,
+	})
+	ctx, targetCollections, compatCollections := s.Ctx, s.TargetCollections, s.CompatCollections
+
+	for name, tc := range testCases {
+		name, tc := name, tc
+		t.Run(name, func(t *testing.T) {
+			t.Helper()
+
+			t.Parallel()
+
+			filter := tc.filter
+			require.NotNil(t, filter, "filter should be set")
+
+			var nonEmptyResults bool
+			for i := range targetCollections {
+				targetCollection := targetCollections[i]
+				compatCollection := compatCollections[i]
+				t.Run(targetCollection.Name(), func(t *testing.T) {
+					t.Helper()
+
+					targetRes, targetErr := targetCollection.Distinct(ctx, tc.field, tc.filter)
+					compatRes, compatErr := compatCollection.Distinct(ctx, tc.field, tc.filter)
+
+					if targetErr != nil {
+						t.Logf("Target error: %v", targetErr)
+						targetErr = UnsetRaw(t, targetErr)
+						compatErr = UnsetRaw(t, compatErr)
+						assert.Equal(t, compatErr, targetErr)
+						return
+					}
+					require.NoError(t, compatErr, "compat error; target returned no error")
+
+					t.Logf("Compat (expected) result: %v", compatRes)
+					t.Logf("Target (actual)   result: %v", targetRes)
+					assert.Equal(t, compatRes, targetRes)
+
+					if targetRes != nil || compatRes != nil {
+						nonEmptyResults = true
+					}
+				})
+			}
+
+			switch tc.resultType {
+			case nonEmptyResult:
+				assert.True(t, nonEmptyResults, "expected non-empty results")
+			case emptyResult:
+				assert.False(t, nonEmptyResults, "expected empty results")
+			default:
+				t.Fatalf("unknown result type %v", tc.resultType)
+			}
+		})
+	}
+}
+
+func TestDistinctCompat(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]distinctCompatTestCase{
+		"EmptyField": {
+			field:  "",
+			filter: bson.D{},
+		},
+		"IDAny": {
+			field:  "_id",
+			filter: bson.D{},
+		},
+		"IDString": {
+			field:  "_id",
+			filter: bson.D{{"_id", "string"}},
+		},
+		"IDNotExists": {
+			field:  "_id",
+			filter: bson.D{{"_id", "count-id-not-exists"}},
+		},
+		"FieldTypeArrays": {
+			field:  "v",
+			filter: bson.D{{"v", bson.D{{"$type", "array"}}}},
+		},
+	}
+
+	testDistinctCompat(t, testCases)
+}
