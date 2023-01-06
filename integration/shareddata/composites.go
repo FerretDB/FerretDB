882fbe713075bdaf333f7b348bc7e173c37cbab1
noisersup <patryk@kwiatek.xyz>
2023-01-06 04:16:18 +0100

    wip

--- integration/shareddata/composites.go
+++ integration/shareddata/composites.go
@@ -30,7 +30,7 @@ var Composites = &Values[string]{
 	handlers: []string{"pg"},
 	data: map[string]any{
 		"document":                   bson.D{{"foo", int32(42)}},
-		"document-composite":         bson.D{{"foo", int32(42)}, {"42", "foo"}, {"array", bson.A{int32(42), "foo", nil}}}, // TODO: use it with dot notation
+		"document-composite":         bson.D{{"foo", int32(42)}, {"42", "foo"}, {"array", bson.A{int32(42), "foo", nil}}},
 		"document-composite-reverse": bson.D{{"array", bson.A{int32(42), "foo", nil}}, {"42", "foo"}, {"foo", int32(42)}},
 		"document-null":              bson.D{{"foo", nil}},
 		"document-empty":             bson.D{},
