caa8cf5b784a0bafae3bff49734a8dd90d472af9
Dmitry <dmitry.eremenko@ferretdb.io>
2023-01-16 11:47:33 +0000

    WIP

--- internal/clientconn/conn.go
+++ internal/clientconn/conn.go
@@ -514,8 +514,6 @@ func (c *conn) route(ctx context.Context, reqHeader *wire.MsgHeader, reqBody wir
 }
 
 func (c *conn) handleOpMsg(ctx context.Context, msg *wire.OpMsg, cmd string) (*wire.OpMsg, error) {
-	c.l.Debug("FFFFFFFFFF ", cmd)
-
 	if cmd, ok := common.Commands[cmd]; ok {
 		if cmd.Handler != nil {
 			return cmd.Handler(c.h, ctx, msg)
