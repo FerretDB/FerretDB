f2f8bc985479961d6655684fb9f6825b9b11be59
Alexey Palazhchenko <alexey.palazhchenko@ferretdb.io>
2023-01-23 21:49:34 +0400

    Ensure that we don't import extra dependencies

--- internal/handlers/registry/registry_test.go
+++ internal/handlers/registry/registry_test.go
@@ -14,8 +14,62 @@
 
 package registry
 
-import "testing"
+import (
+	"encoding/json"
+	"os/exec"
+	"testing"
 
-func TestDummy(t *testing.T) {
-	// we need at least one test per package to correctly calculate coverage
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"golang.org/x/exp/maps"
+)
+
+// extraDeps returns extra FerretDB dependencies (packages) when the given tag is enabled.
+func extraDeps(t *testing.T, tag string) []string {
+	t.Helper()
+
+	type goListResult struct {
+		Deps []string `json:"Deps"`
+	}
+
+	b, err := exec.Command("go", "list", "-json", "../../../cmd/ferretdb").Output()
+	require.NoError(t, err)
+	var withoutTag goListResult
+	require.NoError(t, json.Unmarshal(b, &withoutTag))
+
+	b, err = exec.Command("go", "list", "-json", "-tags", tag, "../../../cmd/ferretdb").Output()
+	require.NoError(t, err)
+	var withTag goListResult
+	require.NoError(t, json.Unmarshal(b, &withTag))
+
+	diff := make(map[string]struct{}, len(withTag.Deps))
+	for _, dep := range withTag.Deps {
+		diff[dep] = struct{}{}
+	}
+
+	for _, dep := range withoutTag.Deps {
+		delete(diff, dep)
+	}
+
+	return maps.Keys(diff)
+}
+
+// TestDeps ensures that extra dependencies are not used when the given Go build tag / backend is not enabled.
+func TestDeps(t *testing.T) {
+	t.Parallel()
+
+	t.Run("Tigris", func(t *testing.T) {
+		t.Parallel()
+
+		diff := extraDeps(t, "ferretdb_tigris")
+		assert.Contains(t, diff, "github.com/tigrisdata/tigris-client-go/driver")
+		assert.Contains(t, diff, "google.golang.org/grpc")
+	})
+
+	t.Run("Hana", func(t *testing.T) {
+		t.Parallel()
+
+		diff := extraDeps(t, "ferretdb_hana")
+		assert.Contains(t, diff, "github.com/SAP/go-hdb/driver")
+	})
 }
