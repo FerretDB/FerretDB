e17fa6d5a8571c31c7842b26ffd80ce9e84f6c79
Alexey Palazhchenko <alexey.palazhchenko@ferretdb.io>
2023-01-29 22:14:52 +0100

    Fix `InTransaction` helper's edge case
    It hangs if `f` function panics.

---
+++ internal/handlers/pg/pgdb/transactions_test.go
@@ -0,0 +1,41 @@
+// Copyright 2021 FerretDB Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package pgdb
+
+import (
+	"testing"
+
+	"github.com/jackc/pgx/v4"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/FerretDB/FerretDB/internal/util/testutil"
+)
+
+func TestInTransaction(t *testing.T) {
+	t.Parallel()
+
+	ctx := testutil.Ctx(t)
+	pool := getPool(ctx, t)
+
+	assert.Panics(t, func() {
+		pool.InTransaction(ctx, func(pgx.Tx) error {
+			// We can't test `runtime.Goexit()`, but `panic(nil)` hangs the buggy code as well;
+			// see comments inside `InTransaction`.
+			//
+			//nolint:vet // we need it for testing
+			panic(nil)
+		})
+	})
+}
