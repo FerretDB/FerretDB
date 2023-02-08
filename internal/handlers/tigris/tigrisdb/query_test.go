cd765a73005b12be16ad123c37da55fab9b050f5
Dmitry <dmitry.eremenko@ferretdb.io>
2023-02-08 10:30:14 +0000

    WIP

--- internal/handlers/tigris/tigrisdb/query_test.go
+++ internal/handlers/tigris/tigrisdb/query_test.go
@@ -67,13 +67,10 @@ func TestQueryDocuments(t *testing.T) {
 			var doc *types.Document
 
 			n, doc, err = iter.Next()
-			if err != nil {
-				if errors.Is(err, iterator.ErrIteratorDone) {
-					break
-				}
-
-				require.NoError(t, err)
+			if errors.Is(err, iterator.ErrIteratorDone) {
+				break
 			}
+
 			require.NoError(t, err)
 			require.Equal(t, i, n)
 
