5c7178b0971da461f01c082b2a7a058d415c211d
Alexey Palazhchenko <alexey.palazhchenko@ferretdb.io>
2023-02-22 16:39:21 +0400

    That should work

--- build/legacy-mongo-shell/test.example.js
+++ build/legacy-mongo-shell/test.example.js
@@ -1,4 +1,4 @@
-// Move to test.js and run with `task test-script`.
+// Move to test.js and run with `task testjs`.
 
 (function() {
   'use strict';
--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -1,1 +1,17 @@
-// Please do not commit changes in this file.
+// Please do not merge changes in this file.
+
+(function() {
+  'use strict';
+
+  const coll = db.jstests_elemmatch_value;
+  coll.drop();
+
+  assert.commandWorked(coll.insert([
+    {a: 5},
+    {a: [5]}, // matches all the specified query criteria
+    {a: [3, 7]},
+  ]))
+
+  assert.eq(coll.find({a: {$elemMatch: {$lt: 6, $gt: 4}}}, {_id: 0}).toArray(), [{a: [5]}]);
+  print('test.js passed!');
+})();
