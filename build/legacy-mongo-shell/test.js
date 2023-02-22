26f182fdba3af467caf1edac2aa768d8471f211d
Alexey Palazhchenko <alexey.palazhchenko@ferretdb.io>
2023-02-22 16:58:43 +0400

    Update rules

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -3,15 +3,5 @@
 (function() {
   'use strict';
 
-  const coll = db.jstests_elemmatch_value;
-  coll.drop();
-
-  assert.commandWorked(coll.insert([
-    {a: 5},
-    {a: [5]}, // matches all the specified query criteria
-    {a: [3, 7]},
-  ]))
-
-  assert.eq(coll.find({a: {$elemMatch: {$lt: 6, $gt: 4}}}, {_id: 0}).toArray(), [{a: [5]}]);
   print('test.js passed!');
 })();
