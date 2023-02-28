cdcdb2d42f16b2d5d3a1c18d3386c398722ecaf5
b1ron <goldingbyron@gmail.com>
2023-02-28 23:28:23 +0100

    make test easier to read

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -7,8 +7,6 @@
   t.drop();
 
   t.insert({_id: 1, a: [0, 2, 5, 5, 1, 0]});
-
-  // pull all instances of 0 and 5.
   t.updateOne({_id: 1}, {$pullAll: {a: [0, 5]}});
 
   const res = t.findOne().a;
@@ -17,12 +15,10 @@
 
   t.drop();
 
-  // pull 3
   t.insert({a: [1, 2, 3]});
   t.update({}, {$pullAll: {a: [3]}});
   assert.eq([1, 2], t.findOne().a);
 
-  // pulling a non-existent element leaves the array unmodified.
   t.update({}, {$pullAll: {a: [3]}});
   assert.eq([1, 2], t.findOne().a);
 
@@ -43,13 +39,11 @@
 
   t.drop();
 
-  // test we can pull an embedded array.
   const ten = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   t.insert({a: {b: ten}});
   t.update({}, {$pullAll: {'a.b': ten}});
   assert.eq([], t.findOne().a.b);
 
-  // test we can pull half of the array.
   t.drop();
   t.insert({a: {b: ten}});
   t.update({}, {$pullAll: {'a.b': ten.slice(5)}});
@@ -65,7 +59,6 @@
   t.update({m: 1}, {$pullAll: {'x.y': [1]}});
   assert(('z' in t.findOne()) == false);
 
-  // upserts a new document if no match.
   t.drop();
   const filter = {_id: 1};
   t.update(filter, {$pullAll: {'a.b': [1]}}, {upsert: true});
