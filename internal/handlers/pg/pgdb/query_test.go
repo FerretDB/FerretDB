767db3d6144d3cf3b9638f9d4f4cbe7eb6962df7
noisersup <patryk@kwiatek.xyz>
2023-02-28 13:07:40 +0100

    wip

--- internal/handlers/pg/pgdb/query_test.go
+++ internal/handlers/pg/pgdb/query_test.go
@@ -289,7 +289,7 @@ func TestPrepareWhereClause(t *testing.T) {
 	objectID := types.ObjectID{0x62, 0x56, 0xc5, 0xba, 0x0b, 0xad, 0xc0, 0xff, 0xee, 0xff, 0xff, 0xff}
 
 	// WHERE clauses occurring frequently in tests
-	whereEq := " WHERE ((_jsonb->$1)::jsonb = $2)"
+	whereEq := " WHERE (_jsonb->$1)::jsonb = $2"
 	whereEqOrContain := whereEq + " OR (_jsonb->$1)::jsonb @> $2"
 
 	for name, tc := range map[string]struct {
