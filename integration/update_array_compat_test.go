3c0e03a75ed4a5a18350cd06b978beecadc13bce
b1ron <goldingbyron@gmail.com>
2023-02-28 15:24:04 +0100

    add a compat test case

--- integration/update_array_compat_test.go
+++ integration/update_array_compat_test.go
@@ -199,6 +199,9 @@ func TestUpdateArrayCompatPullAll(t *testing.T) {
 		"Int32": {
 			update: bson.D{{"$pullAll", bson.D{{"v", bson.A{int32(42)}}}}},
 		},
+		"Int32-Six-Elements": {
+			update: bson.D{{"$pullAll", bson.D{{"v", bson.A{int32(0), int32(5)}}}}},
+		},
 		"Int64": {
 			update: bson.D{{"$pullAll", bson.D{{"v", bson.A{int64(42)}}}}},
 		},
