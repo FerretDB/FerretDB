c7dab6e70bb67d79821d436b156a371dc846ee29
b1ron <goldingbyron@gmail.com>
2023-03-02 14:04:56 +0100

    add test script

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -3,5 +3,15 @@
 (function() {
   'use strict';
 
+  const t = db.pull_all;
+
+  t.drop();
+
+  t.insert({'_id': 1, 'scores': [0, 2, 5, 5, 1, 0]});
+  t.update({'_id': 1}, {$pullAll: {'scores': [0, 5]}});
+
+  const expected = {'_id': 1, 'scores': [2, 1]};
+  assert.eq(expected.scores, t.findOne().scores);
+
   print('test.js passed!');
 })();
