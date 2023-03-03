77b2114bf1c1700716eab873c1af281b5a3a6f63
Chi Fujii <chi.fujii@ferretdb.io>
2023-03-03 17:58:32 +0900

    update error param

--- internal/handlers/common/aggregations/sort.go
+++ internal/handlers/common/aggregations/sort.go
@@ -34,7 +34,7 @@ func newSort(stage *types.Document) (Stage, error) {
 		return nil, commonerrors.NewCommandErrorMsgWithArgument(
 			commonerrors.ErrSortBadExpression,
 			"the $sort key specification must be an object",
-			"aggregate",
+			"$sort",
 		)
 	}
 
@@ -42,7 +42,7 @@ func newSort(stage *types.Document) (Stage, error) {
 		return nil, commonerrors.NewCommandErrorMsgWithArgument(
 			commonerrors.ErrSortMissingKey,
 			"$sort stage must have at least one sort key",
-			"aggregate",
+			"$sort",
 		)
 	}
 
