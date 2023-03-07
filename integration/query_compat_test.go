680ea601c74c7a28579f20d88ff84c649a9b3515
Elena Grahovac <elena@grahovac.me>
2023-03-07 19:20:16 +0100

    wip

--- integration/query_compat_test.go
+++ integration/query_compat_test.go
@@ -259,6 +259,25 @@ func testQueryCompatSort() map[string]queryCompatTestCase {
 			filter: bson.D{},
 			sort:   bson.D{{"v", -1}, {"_id", 1}},
 		},
+
+		"DotNotation": {
+			filter: bson.D{},
+			sort:   bson.D{{"v.foo", 1}, {"_id", 1}},
+		},
+		"DotNotationIndex": {
+			filter: bson.D{},
+			sort:   bson.D{{"v.0", 1}, {"_id", 1}},
+		},
+		"DotNotationNonExistent": {
+			filter:     bson.D{},
+			sort:       bson.D{{"invalid.foo", 1}, {"_id", 1}},
+			resultType: emptyResult,
+		},
+		"DotNotationMissingField": {
+			filter:     bson.D{},
+			sort:       bson.D{{"v..foo", 1}, {"_id", 1}},
+			resultType: emptyResult,
+		},
 	}
 
 	return testCases
