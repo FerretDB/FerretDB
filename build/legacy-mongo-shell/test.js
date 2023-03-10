2547606cb4df36f96461014c254205c5695a19b9
Alexey Palazhchenko <alexey.palazhchenko@ferretdb.io>
2023-03-10 12:22:01 +0400

    Add a test script to show we sort keys for `upsert`

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -3,5 +3,14 @@
 (function() {
   'use strict';
 
+  const coll = db.test;
+
+  coll.drop();
+
+  const orig = {'_id': 1, 'c': 1, 'b': 2, 'a': 3};
+
+  coll.update({_id: orig._id}, orig, {upsert: true});
+  assert.eq(orig, coll.findOne());
+
   print('test.js passed!');
 })();
