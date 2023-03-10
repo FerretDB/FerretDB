05d0505038a8666237c4c9861195d4e7cc0f22d9
Alexey Palazhchenko <alexey.palazhchenko@ferretdb.io>
2023-03-10 13:14:23 +0400

    Add test script to show panic on a bad update path

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -3,5 +3,14 @@
 (function() {
   'use strict';
 
+  const coll = db.test;
+
+  coll.drop();
+
+  coll.insert({_id: 1});
+
+  const expectedWriteError = coll.update({'_id': 1}, {$set: {'0..': 'val01'}});
+  assert.eq(expectedWriteError.hasWriteError(), true);
+
   print('test.js passed!');
 })();
