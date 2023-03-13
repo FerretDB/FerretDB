f9da57ff9e0442256c776531c32ff2e2134ce1b6
Dmitry <dmitry.eremenko@ferretdb.io>
2023-03-13 22:35:59 +0000

    WIP

--- integration/update_array_compat_test.go
+++ integration/update_array_compat_test.go
@@ -272,7 +272,7 @@ func TestUpdateArrayCompatAddToSetEach(t *testing.T) {
 			update: bson.D{{"$addToSet", bson.D{{"non-existent-field", bson.D{{"$each", bson.A{int32(42)}}}}}}},
 		},
 		"DotNotation": {
-			update: bson.D{{"$addToSet", bson.D{{"v.0.foo", bson.D{{"$each", bson.A{int32(42)}}}}}}},
+			update: bson.D{{"$addToSet", bson.D{{"v.0.foo", bson.D{{"$each", bson.A{bson.D{{"bar", "zoo"}}}}}}}}},
 		},
 		"DotNotationNonArray": {
 			update: bson.D{{"$addToSet", bson.D{{"v.0.foo.0.bar", bson.D{{"$each", bson.A{int32(42)}}}}}}},
