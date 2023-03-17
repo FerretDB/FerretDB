0fc4775c0d7d63ce9a4dbc08e31cf4ae3a020b89
noisersup <patryk@kwiatek.xyz>
2023-03-17 19:06:30 +0100

    Merge

--- build/docker/all-in-one/entrypoint.sh
+++ build/docker/all-in-one/entrypoint.sh
@@ -2,6 +2,4 @@
 
 set -ex
 
-# Don't use `exec` so ctrl+c / SIGINT stops bash and not runsvdir (which ignores SIGINT).
-# That's a hack, but good enough for all-in-one container.
-/usr/bin/runsvdir /etc/service
+exec /usr/bin/runsvdir /etc/service
