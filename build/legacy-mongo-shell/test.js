d8ecaa39173a4ac8e866d83e811c6ae4e0d4b931
b1ron <goldingbyron@gmail.com>
2023-03-17 14:34:30 +0100

    add test script

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -3,5 +3,26 @@
 (function() {
   'use strict';
 
+  const t = db.allow_null;
+
+  t.drop();
+  t.insert({'a': 1});
+  t.insert({'a': 2});
+  t.insert({'a': 3});
+  t.insert({'a': 4});
+
+  const collName = 'allow_null';
+
+  let res = assert.commandWorked(db.runCommand({distinct: collName, key: 'a'}));
+  assert.eq([1, 2, 3, 4], res.values.sort());
+
+  // eslint-disable-next-line max-len
+  res = assert.commandWorked(db.runCommand({distinct: collName, key: 'a', query: {}}));
+  assert.eq([1, 2, 3, 4], res.values.sort());
+
+  // eslint-disable-next-line max-len
+  res = assert.commandWorked(db.runCommand({distinct: collName, key: 'a', query: null}));
+  assert.eq([1, 2, 3, 4], res.values.sort());
+
   print('test.js passed!');
 })();
