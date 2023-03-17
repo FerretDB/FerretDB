8ebe61c2f736a11deb6e81eb2d260e3ada1432e4
b1ron <goldingbyron@gmail.com>
2023-03-17 12:33:35 +0100

    add test script

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -3,5 +3,16 @@
 (function() {
   'use strict';
 
+  const t = db.type_miss_match;
+
+  t.drop();
+  t.insert({'a': 1});
+  t.insert({'a': 1});
+  t.insert({'a': 2});
+  t.insert({'a': 3});
+
+  // eslint-disable-next-line max-len
+  assert.commandFailedWithCode(t.runCommand('distinct', {'key': {'a': 1}}), ErrorCodes.TypeMismatch);
+
   print('test.js passed!');
 })();
