27e791cd38cb2b810beb12e483ee37bdd6f9f444
b1ron <goldingbyron@gmail.com>
2023-03-22 21:05:35 +0100

    add test script

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -3,5 +3,13 @@
 (function() {
   'use strict';
 
+  const t = db.foo;
+  t.drop();
+
+  t.save({a: [{}, {b: 1}]});
+  assert.eq(1, t.find({'a.b': 1}).itcount()); // to show the expected behaviour.
+
+  assert.eq(1, t.find({a: {$elemMatch: {b: 1}}}).itcount());
+
   print('test.js passed!');
 })();
