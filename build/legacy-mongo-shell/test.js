79619545847a26f75a9eb1e607e2535bce4ad043
b1ron <goldingbyron@gmail.com>
2023-03-24 17:08:51 +0100

    add test script

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -3,5 +3,12 @@
 (function() {
   'use strict';
 
+  const t = db.foo;
+  t.drop();
+
+  assert.commandWorked(t.stats({scale: 1}));
+  // eslint-disable-next-line max-len
+  assert.commandWorked(t.stats()); // causes a network error and fails because it uses BSON undefined type 0x06.
+
   print('test.js passed!');
 })();
