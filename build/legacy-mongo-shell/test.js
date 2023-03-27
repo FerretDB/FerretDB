a06bd4bf1832bceed7189cff07d788cdb11e2155
b1ron <goldingbyron@gmail.com>
2023-03-27 22:14:19 +0200

    add test script

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -3,5 +3,24 @@
 (function() {
   'use strict';
 
+  const t = db.foo;
+  t.drop();
+
+  t.save({_id: 1, a: 1, b: 1});
+  t.save({_id: 2, a: 1, b: 1});
+
+  const expected = [
+    {_id: 1},
+    {_id: 2},
+  ];
+
+  const proj = {_id: 1};
+
+  assert.eq(expected, t.find({}, proj).toArray());
+
+  // test that sorting doesn't break projection
+  assert.eq(expected, t.find({}, proj).sort({_id: 1}).toArray());
+  assert.eq(expected.reverse(), t.find({}, proj).sort({_id: -1}).toArray());
+
   print('test.js passed!');
 })();
