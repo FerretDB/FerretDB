b0d0c61f5970a891590a022daa389bb66a88ef9a
Elena Grahovac <elena@grahovac.me>
2023-03-28 21:12:16 +0200

    wip

--- internal/handlers/pg/pgdb/indexes_test.go
+++ internal/handlers/pg/pgdb/indexes_test.go
@@ -58,7 +58,7 @@ func TestCreateIndexIfNotExists(t *testing.T) {
 	require.NoError(t, err)
 
 	expectedIndexdef := fmt.Sprintf(
-		"CREATE UNIQUE INDEX %s ON %s.%s USING btree (((_jsonb -> 'foo'::text)), ((_jsonb -> 'bar'::text)) DESC)",
+		"CREATE INDEX %s ON %s.%s USING btree (((_jsonb -> 'foo'::text)), ((_jsonb -> 'bar'::text)) DESC)",
 		pgIndexName, databaseName, tableName,
 	)
 	assert.Equal(t, expectedIndexdef, indexdef)
