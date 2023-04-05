ed005374a60f5f51a9bf2091f09399d3e24ae869
b1ron <goldingbyron@gmail.com>
2023-04-05 12:42:19 +0200

    add test

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -3,5 +3,19 @@
 (function() {
   'use strict';
 
+  // Make 4 test databases.
+  db.getSiblingDB('foo').coll.insert({});
+  db.getSiblingDB('bar').coll.insert({});
+  db.getSiblingDB('buz').coll.insert({});
+  db.getSiblingDB('baz').coll.insert({});
+
+  const listDatabasesOut = db.adminCommand({listDatabases: 1});
+  const dbList = listDatabasesOut.databases;
+  let sizeSum = 0;
+  for (let i = 0; i < dbList.length; i++) {
+    sizeSum += dbList[i].sizeOnDisk;
+  }
+  assert.eq(sizeSum, listDatabasesOut.totalSize);
+
   print('test.js passed!');
 })();
