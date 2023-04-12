536cac152f32ecaea42d429071de461a72fdb712
Chi Fujii <chi.fujii@ferretdb.io>
2023-04-12 15:41:37 +0900

    add debug error test

---
+++ integration/debug_test.go
@@ -0,0 +1,36 @@
+// Copyright 2021 FerretDB Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package integration
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"go.mongodb.org/mongo-driver/bson"
+
+	"github.com/FerretDB/FerretDB/integration/setup"
+	"github.com/FerretDB/FerretDB/integration/shareddata"
+)
+
+func TestDebug(t *testing.T) {
+	ctx, collection := setup.Setup(t, shareddata.Int32s)
+
+	command := bson.D{
+		{"debugError", "fatal test failed"},
+	}
+
+	res := collection.Database().RunCommand(ctx, command)
+	require.EqualError(t, res.Err(), "(InternalError) fatal test failed")
+}
