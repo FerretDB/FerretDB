5eea6ae719b27dd4dd2c5c999a091a884ed03161
Chi Fujii <chi.fujii@ferretdb.io>
2023-04-18 10:41:48 +0900

    demonstrate bug

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -3,5 +3,27 @@
 (function() {
   'use strict';
 
+  const coll = db.test;
+
+  coll.drop();
+
+  const init = [
+    {_id: 'exists'},
+  ];
+
+  coll.insertMany(init);
+
+  var actualErr = null
+  try {
+    coll.findOneAndUpdate({_id:"not_exists"}, {$set: {_id:"exists"}}, {upsert:true})
+  }
+  catch(err) {
+    actualErr = err
+  }
+
+  const expectedErrCode = 66
+
+  assert.eq(expectedErrCode, actualErr.code);
+
   print('test.js passed!');
 })();
