d7f6bd9ee189d0163fd85c927e8d38ba1178e3e1
raeidish <raeidish@gmail.com>
2023-04-19 12:58:09 +0200

    fix lint errors on checkdocks

--- tools/checkdocs/checkdocs.go
+++ tools/checkdocs/checkdocs.go
@@ -1,3 +1,17 @@
+// Copyright 2021 FerretDB Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
@@ -11,64 +25,58 @@ import (
 	"regexp"
 )
 
+// FileSlug describes the file name and expected slug of blog posts.
 type FileSlug struct {
 	slug     string
 	fileName string
 }
 
 func main() {
-    dir := filepath.Join("website","blog")
+	dir := filepath.Join("website", "blog")
 	fs, err := os.ReadDir(dir)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	slugs := GetBlogSlugs(fs)
-    pass := true
+	pass := true
 	for _, slug := range slugs {
-
 		fo, err := os.Open(filepath.Join(dir, slug.fileName))
-		defer fo.Close()
-        
 		if err != nil {
 			log.Fatalf("Couldn't open file: %s", slug.fileName)
 			continue
 		}
 
-        serr := VerifySlug(slug, fo)
+		defer fo.Close()
 
-        if serr != nil {
-            log.Print(serr)
-            pass = false
-        }
-        
+		serr := VerifySlug(slug, fo)
+
+		if serr != nil {
+			log.Print(serr)
+			pass = false
+		}
 	}
-    
-    if !pass {
-        log.Fatal("One or more blog posts are not correctly formated")
-    }
 
+	if !pass {
+		log.Fatal("One or more blog posts are not correctly formated")
+	}
 }
 
+// GetBlogSlugs returns slice containing FileSlug for each DirEntry.
 func GetBlogSlugs(fs []fs.DirEntry) []FileSlug {
-	// start with 4 digits then a 0[1-9] or 1 [0 1 or 2] then - and either 0 [1-9] or [1 or 2][0-9] or 3[0 or 1] - slug(any) and end with .md
-	r := regexp.MustCompile(`^\d{4}\-(?:0[1-9]|1[012])\-(?:0[1-9]|[12][0-9]|3[01])-(.*).md$`)
-
+	// start with 4 digits then a 0[1-9] or 1 [0 1 or 2]
+	// then - and either 0 [1-9] or [1 or 2][0-9] or 3[0 or 1] - slug(any) and end with .md.
+	fnRegex := regexp.MustCompile(`^\d{4}\-(?:0[1-9]|1[012])\-(?:0[1-9]|[12][0-9]|3[01])-(.*).md$`)
+	mdRegex := regexp.MustCompile(`.md$`)
 	var fileSlugs []FileSlug
 	for _, f := range fs {
-
 		fn := f.Name()
-		m, err := regexp.MatchString(`.md$`, fn)
 
-		if !m {
+		if !mdRegex.MatchString(fn) {
 			continue
 		}
 
-		if err != nil {
-			log.Fatalf("regexp error, %s", err)
-		}
-
-		sm := r.FindStringSubmatch(fn)
+		sm := fnRegex.FindStringSubmatch(fn)
 		if len(sm) > 2 {
 			log.Fatalf("File %s is not correctly formated (yyyy-mm-dd-'slug'.md)", fn)
 			continue
@@ -80,6 +88,7 @@ func GetBlogSlugs(fs []fs.DirEntry) []FileSlug {
 	return fileSlugs
 }
 
+// VerifySlug returns error if file doesn't contain expected slug.
 func VerifySlug(fS FileSlug, f io.Reader) error {
 	r := regexp.MustCompile("^slug: (.*)")
 
@@ -94,8 +103,8 @@ func VerifySlug(fS FileSlug, f io.Reader) error {
 	}
 
 	if !pass {
-        return fmt.Errorf("Slug is not correctly formated in file %s",fS.fileName)
+		return fmt.Errorf("Slug is not correctly formated in file %s", fS.fileName)
 	}
 
-    return nil
+	return nil
 }
