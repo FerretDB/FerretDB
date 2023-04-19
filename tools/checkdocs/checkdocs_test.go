f8b756f4a5d3178d7a38b44428d8415a3e669fab
raeidish <raeidish@gmail.com>
2023-04-19 12:58:24 +0200

    fix lint errors on checkdocks tests

--- tools/checkdocs/checkdocs_test.go
+++ tools/checkdocs/checkdocs_test.go
@@ -1,3 +1,17 @@
+// Copyright 2021 FerretDB Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
@@ -14,19 +28,20 @@ func TestGetBlogSlugs(t *testing.T) {
 slug: using-cla-assistant-with-ferretdb
 title: "Using CLA Assistant with FerretDB"
 author: Alexey Palazhchenko
-description: Like many other open-source projects, FerretDB requires all contributors to sign [our Contributor License Agreement (CLA)](https://gist.github.com/ferretdb-bot/554e6a30bfcc1d954f3853b4aad95281) to protect them from liability.
+description: Like many other open-source projects FerretDB 
 image: /img/blog/cla3.jpg
 date: 2022-05-16
 ---
 Finally, we need a web server that would handle HTTPS for us.
-For that, we will use [Caddy](https://caddyserver.com):
-Caddy will listen on both HTTP and HTTPS ports, and retrieve the TLS certificate from Let’s Encrypt that will be stored in “./data/caddy” on the host.
+For that, we will use
+Caddy will listen on both HTTP and
 `),
 		},
 	}
 	dirs, _ := m.ReadDir(".")
 	slugs := GetBlogSlugs(dirs)
-	assert.Equal(t, slugs[0], FileSlug{fileName: "2022-05-16-using-cla-assistant-with-ferretdb.md", slug: "using-cla-assistant-with-ferretdb"}, "should be equal")
+	tSlug := FileSlug{fileName: "2022-05-16-using-cla-assistant-with-ferretdb.md", slug: "using-cla-assistant-with-ferretdb"}
+	assert.Equal(t, slugs[0], tSlug, "should be equal")
 }
 
 func TestVerifySlugs(t *testing.T) {
@@ -36,18 +51,19 @@ func TestVerifySlugs(t *testing.T) {
 slug: using-cla-assistant-with-ferretdb
 title: "Using CLA Assistant with FerretDB"
 author: Alexey Palazhchenko
-description: Like many other open-source projects, FerretDB requires all contributors to sign [our Contributor License Agreement (CLA)](https://gist.github.com/ferretdb-bot/554e6a30bfcc1d954f3853b4aad95281) to protect them from liability.
+description: Like many other open-source projects
 image: /img/blog/cla3.jpg
 date: 2022-05-16
 ---
 Finally, we need a web server that would handle HTTPS for us.
 For that, we will use [Caddy](https://caddyserver.com):
-Caddy will listen on both HTTP and HTTPS ports, and retrieve the TLS certificate from Let’s Encrypt that will be stored in “./data/caddy” on the host.
+Caddy will listen on both HTTP and HTTPS ports, 
 `),
 		},
 	}
 	dirs, _ := m.ReadDir(".")
 	slugs := GetBlogSlugs(dirs)
 	f, _ := m.Open("2022-05-16-using-cla-assistant-with-ferretdb.md")
-	VerifySlug(slugs[0], f)
+	err := VerifySlug(slugs[0], f)
+	assert.Nil(t, err)
 }
