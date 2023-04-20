127fb8814b45e896ca5ae93ea6b79923d2be59f8
Chi Fujii <chi.fujii@ferretdb.io>
2023-04-20 18:02:32 +0900

    add todo

--- internal/handlers/common/aggregations/stages/unwind.go
+++ internal/handlers/common/aggregations/stages/unwind.go
@@ -105,6 +105,7 @@ func newUnwind(stage *types.Document) (Stage, error) {
 
 // Process implements Stage interface.
 func (u *unwind) Process(ctx context.Context, iter types.DocumentsIterator, closer *iterator.MultiCloser) (types.DocumentsIterator, error) { //nolint:lll // for readability
+	// TODO https://github.com/FerretDB/FerretDB/issues/2490
 	docs, err := iterator.ConsumeValues(iterator.Interface[struct{}, *types.Document](iter))
 	if err != nil {
 		return nil, lazyerrors.Error(err)
