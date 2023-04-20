315bd022af7f29e961d0e82ef799f211ff76d56c
b1ron <goldingbyron@gmail.com>
2023-04-20 15:02:27 +0200

    add script

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -3,5 +3,38 @@
 (function() {
   'use strict';
 
+  const t = db.proj;
+  const coll = 'proj';
+
+  t.drop();
+
+  const verycomplexDoc = {
+    _id: 1,
+    a: {
+      b: {
+        c: {
+          array: [
+            {
+              token: "aAOBX7fkiRB+XGH1oQ9fln7sM62ox06qzUKpaan7Bys="
+            }
+          ]
+        }
+      }
+    }
+  };
+
+  assert.commandWorked(t.insert(verycomplexDoc));
+
+  const filter = {'a.b.c.array.token': 'aAOBX7fkiRB+XGH1oQ9fln7sM62ox06qzUKpaan7Bys='};
+  const proj = {'a.b.c.array.token': 1};
+
+  assert.eq(verycomplexDoc._id, t.findOne(filter, proj)._id);
+
+  const res = t.runCommand({'find': coll, 'filter': filter, 'projection': proj});
+
+  // command must fail as we don't support $ projection
+  // we also seem to project the _id field
+  assert.commandFailed(res);
+
   print('test.js passed!');
 })();
