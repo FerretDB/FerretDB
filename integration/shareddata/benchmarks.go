b079d3b6f1c341f4f9064514c9087ad5cb63b4bb
Alexey Palazhchenko <alexey.palazhchenko@ferretdb.io>
2023-05-01 12:47:39 +0400

    Rename file

--- integration/shareddata/benchmarks.go
+++ integration/shareddata/benchmarks.go
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package shareddata provides data for tests and benchmarks.
 package shareddata
 
 import (
--- integration/shareddata/shareddata.go
+++ integration/shareddata/shareddata.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package shareddata provides data for tests and benchmarks.
 package shareddata
 
 import (
