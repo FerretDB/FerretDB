5aca87b024db52955ad9493ad259538d0b296376
Patryk Kwiatek <patryk@kwiatek.xyz>
2023-05-03 14:02:18 +0200

    Pushdown simplest sorting for `aggregate` command  (#2530)
    Closes #2438.
    
    Co-authored-by: Elena Grahovac <elena@grahovac.me>

--- integration/aggregate_documents_compat_test.go
+++ integration/aggregate_documents_compat_test.go
@@ -884,6 +884,7 @@ func TestAggregateCompatLimit(t *testing.T) {
 				bson.D{{"$match", bson.D{{"v", "foo"}}}},
 				bson.D{{"$limit", 1}},
 			},
+			resultPushdown: true, // $sort and $match are first two stages
 		},
 		"BeforeMatch": {
 			pipeline: bson.A{
@@ -1405,6 +1406,7 @@ func TestAggregateCompatSkip(t *testing.T) {
 				bson.D{{"$match", bson.D{{"v", "foo"}}}},
 				bson.D{{"$skip", int32(1)}},
 			},
+			resultPushdown: true, // $match after $sort can be pushed down
 		},
 		"BeforeMatch": {
 			pipeline: bson.A{
