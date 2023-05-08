3c7c7fd7edc955b9c433cc60670b5c69d0bed4e0
Alexey Palazhchenko <alexey.palazhchenko@ferretdb.io>
2023-05-08 20:05:35 +0400

    Add test script for rename collection issue

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -3,5 +3,17 @@
 (function() {
   'use strict';
 
+  const nameLong = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+  const nameShort = "test";
+
+  db.getCollection(nameLong).drop();
+  db.getCollection(nameShort).drop();
+
+  db.getCollection(nameLong).insertOne({});
+
+  assert.commandWorked(db.getCollection(nameLong).renameCollection(nameShort));
+
+  assert.commandWorked(db.getCollection(nameShort).renameCollection(nameLong));
+
   print('test.js passed!');
 })();
