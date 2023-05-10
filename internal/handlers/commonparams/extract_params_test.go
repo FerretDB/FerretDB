25c1f3abe1fe6d0f6cee0a7b9c02a8023f3b2eda
Dmitry <dmitry.eremenko@ferretdb.io>
2023-05-10 11:45:37 +0100

    WIP

--- internal/handlers/commonparams/extract_params_test.go
+++ internal/handlers/commonparams/extract_params_test.go
@@ -26,93 +26,73 @@ import (
 )
 
 func TestParse(t *testing.T) {
-	type findParams struct { //nolint:vet // this is a test struct to avoid import cycle
-		DB          string          `name:"$db"`
-		Collection  string          `name:"collection"`
-		Filter      *types.Document `name:"filter,opt" error:"badValue"`
-		Sort        *types.Document `name:"sort,opt"`
-		Projection  *types.Document `name:"projection,opt"`
-		Skip        int64           `name:"skip"`
-		Limit       int64           `name:"limit"`
-		BatchSize   int64           `name:"batchSize"`
-		SingleBatch bool            `name:"singleBatch"`
-		Comment     string          `name:"comment"`
-		MaxTimeMS   int64           `name:"maxTimeMS"`
+	type allTagsThatPass struct {
+		DB           string          `name:"$db"`
+		Collection   string          `name:"collection"`
+		Filter       *types.Document `name:"filter,opt"`
+		AllowDiskUse any             `name:"allowDiskUse,ignored"`
+	}
 
-		ReturnKey           bool `name:"returnKey,non-default"`
-		ShowRecordId        bool `name:"showRecordId,non-default"`
-		Tailable            bool `name:"tailable,non-default"`
-		OplogReplay         bool `name:"oplogReplay,non-default"`
-		NoCursorTimeout     bool `name:"noCursorTimeout,non-default"`
-		AwaitData           bool `name:"awaitData,non-default"`
-		AllowPartialResults bool `name:"allowPartialResults,non-default"`
+	type noTag struct {
+		Find string
+	}
 
-		Collation any `name:"collation,unimplemented"`
-		Let       any `name:"let,unimplemented"`
-
-		AllowDiskUse any `name:"allowDiskUse,ignored"`
-		ReadConcern  any `name:"readConcern,ignored"`
-		Max          any `name:"max,ignored"`
-		Min          any `name:"min,ignored"`
+	type unimplementedTag struct {
+		Find string `name:"find,unimplemented"`
 	}
 
 	tests := map[string]struct {
 		doc        *types.Document
 		command    string
+		params     any
 		wantParams any
 		wantErr    error
 	}{
-		"simple": {
+		"AllTagTypesThatPass": {
 			command: "find",
 			doc: must.NotFail(types.NewDocument(
 				"$db", "test",
 				"find", "test",
 				"filter", must.NotFail(types.NewDocument("a", "b")),
-				"sort", must.NotFail(types.NewDocument("a", "b")),
-				"projection", must.NotFail(types.NewDocument("a", "b")),
-				"skip", int64(123),
-				"limit", int64(123),
-				"batchSize", int64(484),
-				"singleBatch", false,
-				"comment", "123",
-				"maxTimeMS", int64(123),
+				"allowDiskUse", "123",
 			)),
-			wantParams: findParams{
-				DB:          "test",
-				Collection:  "test",
-				Filter:      must.NotFail(types.NewDocument("a", "b")),
-				Sort:        must.NotFail(types.NewDocument("a", "b")),
-				Projection:  must.NotFail(types.NewDocument("a", "b")),
-				Skip:        123,
-				Limit:       123,
-				BatchSize:   484,
-				SingleBatch: false,
-				Comment:     "123",
-				MaxTimeMS:   123,
+			params: &allTagsThatPass{},
+			wantParams: &allTagsThatPass{
+				DB:         "test",
+				Collection: "test",
+				Filter:     must.NotFail(types.NewDocument("a", "b")),
 			},
 			wantErr: nil,
 		},
-		"empty tag": {
-			command: "count",
-			doc:     must.NotFail(types.NewDocument("find", "test")),
-			wantParams: &struct {
-				Find string
-			}{},
-			wantErr: errors.New("[extract_params.go:78 commonparams.ExtractParams] unexpected field 'find' encountered"),
+		"UnimplementedTag": {
+			command: "command",
+			doc: must.NotFail(types.NewDocument(
+				"find", "test",
+			)),
+			params: &unimplementedTag{},
+			wantParams: &unimplementedTag{
+				Find: "test",
+			},
+			wantErr: errors.New("NotImplemented (238): find: support for field \"find\" with value test is not implemented yet"),
+		},
+		"EmptyTag": {
+			command:    "count",
+			doc:        must.NotFail(types.NewDocument("find", "test")),
+			params:     &noTag{},
+			wantParams: &noTag{},
+			wantErr:    errors.New("[extract_params.go:80 commonparams.ExtractParams] unexpected field 'find' encountered"),
 		},
 	}
 	for name, tt := range tests {
 		t.Run(name, func(t *testing.T) {
-			params := findParams{}
-
-			err := ExtractParams(tt.doc, tt.command, &params, zap.NewNop())
+			err := ExtractParams(tt.doc, tt.command, tt.params, zap.NewNop())
 			if tt.wantErr != nil {
 				require.Equal(t, tt.wantErr.Error(), err.Error())
 				return
 			}
 			require.NoError(t, err)
 
-			require.Equal(t, tt.wantParams, params)
+			require.Equal(t, tt.wantParams, tt.params)
 		})
 	}
 }
