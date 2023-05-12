9249bc47758599e66b9ac7d75e64600d2c671fd4
wqhhust <wuqihua_it@163.com>
2023-05-12 14:38:59 +0800

    Cleanup old Docker images  (#2533)
    Closes #353.

---
+++ tools/cleantool/cleantool.go
@@ -0,0 +1,92 @@
+// Copyright 2021 FerretDB Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/google/go-github/v41/github"
+	"golang.org/x/oauth2"
+)
+
+// if the most recent update time of the package was 90 days ago, then mark it as
+// stale.
+func isPackageToBeClean(p *github.PackageVersion) bool {
+	daysBack := 90
+	toBeClean := false
+
+	if time.Now().After(p.UpdatedAt.Add(time.Duration(daysBack) * 24 * time.Hour)) {
+		log.Printf("Stale version id: %v , recent update was at %s", p.GetID(), p.UpdatedAt)
+
+		toBeClean = true
+	} else {
+		log.Printf("Skip version id: %v , recent update was at %s", p.GetID(), p.UpdatedAt)
+	}
+
+	return toBeClean
+}
+
+func main() {
+	ctx := context.Background()
+	tokenName := "ROBOT_TOKEN"
+	token := os.Getenv(tokenName)
+
+	if token == "" {
+		log.Fatalf("env variable %v is not found, please set it before run it", tokenName)
+	}
+
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+	packageType := "container"
+	packageName := "ferretdb-dev"
+	orgName := "FerretDB"
+	pageSize := 100
+	pageIndex := 1
+
+	var versions []string
+
+	for {
+		listOption := github.ListOptions{Page: pageIndex, PerPage: pageSize}
+		packageListOption := github.PackageListOptions{ListOptions: listOption}
+		packages, _, err := client.Organizations.PackageGetAllVersions(ctx, orgName, packageType, packageName, &packageListOption)
+		if err != nil {
+			log.Printf("Failed to get versions for page %d", pageIndex)
+		}
+
+		for _, v := range packages {
+			if isPackageToBeClean(v) {
+				versions = append(versions, fmt.Sprintf("%v", v.GetID()))
+			}
+		}
+
+		if len(packages) < pageSize {
+			log.Printf("Come to the last page (page %d)", pageIndex)
+			break
+		}
+
+		pageIndex++
+	}
+
+	staleVersions := strings.Join(versions, ", ")
+	log.Printf("Found %d stale versions: %v", len(versions), staleVersions)
+}
