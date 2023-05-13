359df6962b851f96282baaa8c1219f7d58a720ba
Dmitry <dmitry.eremenko@ferretdb.io>
2023-05-13 11:47:22 +0100

    WIP

--- internal/handlers/common/explain.go
+++ internal/handlers/common/explain.go
@@ -17,15 +17,19 @@ package common
 import (
 	"go.uber.org/zap"
 
-	"github.com/FerretDB/FerretDB/internal/handlers/commonparams"
+	"github.com/FerretDB/FerretDB/internal/handlers/commonerrors"
 	"github.com/FerretDB/FerretDB/internal/types"
+	"github.com/FerretDB/FerretDB/internal/util/iterator"
+	"github.com/FerretDB/FerretDB/internal/util/lazyerrors"
+	"github.com/FerretDB/FerretDB/internal/util/must"
 )
 
-// ExplainParams represents parameters for the explain command.
+// ExplainParams represents the parameters for the explain command.
 type ExplainParams struct {
-	DB         string          `ferretdb:"$db"`
-	Collection string          `ferretdb:"collection"`
-	Explain    *types.Document `ferretdb:"explain"`
+	DB         string `ferretdb:"$db"`
+	Collection string `ferretdb:"collection"`
+
+	Explain *types.Document `ferretdb:"explain"`
 
 	Filter *types.Document `ferretdb:"filter,opt"`
 	Sort   *types.Document `ferretdb:"sort,opt"`
@@ -39,36 +43,82 @@ type ExplainParams struct {
 
 // GetExplainParams returns the parameters for the explain command.
 func GetExplainParams(document *types.Document, l *zap.Logger) (*ExplainParams, error) {
-	var params ExplainParams
+	var err error
 
-	err := commonparams.ExtractParams(document, "explain", &params, l)
-	if err != nil {
-		return nil, err
+	var db, collection string
+
+	if db, err = GetRequiredParam[string](document, "$db"); err != nil {
+		return nil, lazyerrors.Error(err)
 	}
 
-	//if cmd.Command() == "aggregate" {
-	//	var pipeline *types.Array
-	//
-	//	pipeline, err = GetRequiredParam[*types.Array](explain, "pipeline")
-	//	if err != nil {
-	//		return nil, commonerrors.NewCommandErrorMsgWithArgument(
-	//			commonerrors.ErrMissingField,
-	//			"BSON field 'aggregate.pipeline' is missing but a required field",
-	//			document.Command(),
-	//		)
-	//	}
-	//
-	//	stagesDocs = must.NotFail(iterator.ConsumeValues(pipeline.Iterator()))
-	//	for _, d := range stagesDocs {
-	//		if _, ok := d.(*types.Document); !ok {
-	//			return nil, commonerrors.NewCommandErrorMsgWithArgument(
-	//				commonerrors.ErrTypeMismatch,
-	//				"Each element of the 'pipeline' array must be an object",
-	//				document.Command(),
-	//			)
-	//		}
-	//	}
-	//}
+	Ignored(document, l, "verbosity")
 
-	return &params, nil
+	var cmd *types.Document
+
+	cmd, err = GetRequiredParam[*types.Document](document, document.Command())
+	if err != nil {
+		return nil, lazyerrors.Error(err)
+	}
+
+	if collection, err = GetRequiredParam[string](cmd, cmd.Command()); err != nil {
+		return nil, lazyerrors.Error(err)
+	}
+
+	var explain, filter, sort *types.Document
+
+	cmd, err = GetRequiredParam[*types.Document](document, document.Command())
+	if err != nil {
+		return nil, lazyerrors.Error(err)
+	}
+
+	explain, err = GetRequiredParam[*types.Document](document, "explain")
+	if err != nil {
+		return nil, lazyerrors.Error(err)
+	}
+
+	filter, err = GetOptionalParam(explain, "filter", filter)
+	if err != nil {
+		return nil, lazyerrors.Error(err)
+	}
+
+	sort, err = GetOptionalParam(explain, "sort", filter)
+	if err != nil {
+		return nil, lazyerrors.Error(err)
+	}
+
+	var stagesDocs []any
+
+	if cmd.Command() == "aggregate" {
+		var pipeline *types.Array
+
+		pipeline, err = GetRequiredParam[*types.Array](explain, "pipeline")
+		if err != nil {
+			return nil, commonerrors.NewCommandErrorMsgWithArgument(
+				commonerrors.ErrMissingField,
+				"BSON field 'aggregate.pipeline' is missing but a required field",
+				document.Command(),
+			)
+		}
+
+		stagesDocs = must.NotFail(iterator.ConsumeValues(pipeline.Iterator()))
+		for _, d := range stagesDocs {
+			if _, ok := d.(*types.Document); !ok {
+				return nil, commonerrors.NewCommandErrorMsgWithArgument(
+					commonerrors.ErrTypeMismatch,
+					"Each element of the 'pipeline' array must be an object",
+					document.Command(),
+				)
+			}
+		}
+	}
+
+	return &ExplainParams{
+		DB:         db,
+		Collection: collection,
+		Filter:     filter,
+		Sort:       sort,
+		StagesDocs: stagesDocs,
+		Aggregate:  cmd.Command() == "aggregate",
+		Command:    cmd,
+	}, nil
 }
