c4310217d8cc2eda38d08052462201d8316a31ba
noisersup <patryk@kwiatek.xyz>
2023-05-22 19:41:01 +0200

    wip

--- internal/handlers/common/aggregations/stages/projection/projection.go
+++ internal/handlers/common/aggregations/stages/projection/projection.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package projection provides projection for aggregations.
 package projection
 
 import (
