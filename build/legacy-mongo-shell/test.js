ea5dc27960d4b3e8116418da34ba50246ad800f7
Elena Grahovac <elena@grahovac.me>
2023-05-30 18:06:17 +0200

    Demo upsert with _id being set

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -3,5 +3,20 @@
 (function() {
   'use strict';
 
+  const coll = db.test;
+
+  coll.drop();
+
+  coll.findAndModify({query: {"non-existent": {$exists: false}}, upsert: true, update: {$set: {_id: "hello"}}});
+
+  const query = {};
+
+  const expected = [
+    {_id: 'hello'},
+  ];
+
+  const actual = coll.find(query).toArray();
+  assert.eq(expected, actual);
+
   print('test.js passed!');
 })();
