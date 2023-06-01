5ce4a803e5356c53d95a6847d65d0c645675a68b
b1ron <goldingbyron@gmail.com>
2023-06-01 18:26:29 +0200

    add test

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -3,5 +3,23 @@
 (function() {
   'use strict';
 
+  const t = db.pull;
+  t.drop();
+
+  let o = {_id: 1, a: []};
+  for (let i = 0; i < 5; i++) {
+    o.a.push({x: i, y: i});
+  };
+
+  t.insert(o);
+
+  t.update({}, {$pull: {a: {x: 3}}});
+
+  o.a = o.a.filter(function(z) {
+    return z.x != 3;
+  });
+
+  assert.eq(o, t.findOne(), "element 3 should be pulled");
+
   print('test.js passed!');
 })();
