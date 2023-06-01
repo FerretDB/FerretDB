315f125d52086f77936302f00d87d05fa93545c8
b1ron <goldingbyron@gmail.com>
2023-06-01 18:41:33 +0200

    add test

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -3,5 +3,16 @@
 (function() {
   'use strict';
 
+  const t = db.field_path;
+  t.drop();
+
+  t.insert({_id: 1, a: 1});
+  t.insert({_id: 2, a: 2});
+
+  const expected = [{'_id': 1, 'a': 1}, {'_id': 2, 'a': 2}];
+
+  const pipeline = {$project: {_id: 1, a: '$a'}};
+  assert.eq(expected, t.aggregate(pipeline).toArray());
+
   print('test.js passed!');
 })();
