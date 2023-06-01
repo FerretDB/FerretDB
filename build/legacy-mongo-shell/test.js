86e7f476dab7e06bbe958655aedd2c6b1c718591
b1ron <goldingbyron@gmail.com>
2023-06-01 15:28:55 +0200

    add test

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -3,5 +3,24 @@
 (function() {
   'use strict';
 
+  const t = db.batch_size;
+  t.drop();
+
+  // insert 102 documents
+  const defaultBatchSize = 101;
+  for (let i = 0; i < defaultBatchSize + 1; i++) {
+    t.insert({_id: i});
+  }
+
+  const coll = 'batch_size';
+
+  // assert that when no batchSize is specified, we use the default batchSize of 101
+  var res = db.runCommand({find: coll, filter: {}});
+  assert.eq(res.cursor.firstBatch.length, defaultBatchSize);
+
+  // assert that batchSize works accordingly
+  var res = db.runCommand({find: coll, filter: {}, batchSize: 1});
+  assert.eq(res.cursor.firstBatch.length, 1);
+
   print('test.js passed!');
 })();
