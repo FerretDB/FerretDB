d3d525a108e903a177a981bb3cca92b3b399769a
b1ron <goldingbyron@gmail.com>
2023-06-02 12:08:18 +0200

    add a test for $project

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -11,12 +11,23 @@
   pipe = [pipe];
 
   // FailedToParse occurs when cursor is not present.
-  const cmd = {pipeline: pipe, cursor: {batchSize: 0}};
-  const res = t.runCommand('aggregate', cmd);
+  let cmd = {pipeline: pipe, cursor: {batchSize: 0}};
+  let res = t.runCommand('aggregate', cmd);
 
-  const code = 2; // BadValue for $mod in a $match pipeline stage.
+  let code = 2; // BadValue for $mod in a $match pipeline stage.
 
   const assertThrowsMsg = 'expected the following error code: ' + tojson(code);
+  assert.commandFailedWithCode(res, code, assertThrowsMsg);
+
+  pipe = {$project: {a: {$mod: [0 /* invalid */, 0]}}};
+  pipe = [pipe];
+
+  cmd = {pipeline: pipe, cursor: {batchSize: 0}};
+  res = t.runCommand('aggregate', cmd);
+
+  // eslint-disable-next-line max-len
+  code = 16610; // Failed to optimize pipeline :: caused by :: can't $mod by zero"
+
   assert.commandFailedWithCode(res, code, assertThrowsMsg);
 
   print('test.js passed!');
