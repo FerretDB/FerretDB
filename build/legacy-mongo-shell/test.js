ca09c75bf193059e827fd0df1670835916025165
b1ron <goldingbyron@gmail.com>
2023-06-05 12:03:33 +0200

    use an array of numbers

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -8,17 +8,17 @@
 
   t.drop();
 
-  const doc = {_id: 1, a: [{x: 1}, {x: 2}, {x: 3}]};
+  const doc = {_id: 1, a: [1, 2, 3, 4]};
 
   assert.commandWorked(t.insert(doc));
 
-  const filter = {'a.x': 1}; // positional operator needs a filter
+  const filter = {'a': 1}; // positional operator needs a filter
   const proj = {'a.$': 1}; // add $ operator
 
   // eslint-disable-next-line max-len
   const res = t.runCommand({'find': coll, 'filter': filter, 'projection': proj});
 
-  let expected = {'_id': 1, 'a': [{'x': 1}]};
+  let expected = {'_id': 1, 'a': [1]};
   expected = [expected];
   assert.eq(res.cursor.firstBatch, expected);
 
