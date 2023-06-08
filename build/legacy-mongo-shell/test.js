d252e660b5c4c63670511843bcfb6bc9a71f4860
b1ron <goldingbyron@gmail.com>
2023-06-08 12:37:13 +0200

    add test

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -3,5 +3,19 @@
 (function() {
   'use strict';
 
+  const t = db.compound_index_max_fields;
+  t.drop();
+
+  const spec = {};
+  for (let i = 0; i < 32; i++) {
+    spec['f' + i] = (i % 2 == 0) ? 1 : -1;
+  }
+
+  assert.commandWorked(t.createIndex(spec));
+
+  // create an index that has one too many fields.
+  spec['f32'] = 1;
+  assert.commandFailedWithCode(t.createIndex(spec), 13103); // https://github.com/stbrody/mongo/blob/master/docs/errors.md#srcmongobsonorderingh
+
   print('test.js passed!');
 })();
