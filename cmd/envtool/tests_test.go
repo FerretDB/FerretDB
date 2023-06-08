c5cd962837e254a693548951104ef5e4f4e975d7
Alexey Palazhchenko <alexey.palazhchenko@ferretdb.io>
2023-06-08 16:42:15 +0400

    Shard integration tests

--- cmd/envtool/tests_test.go
+++ cmd/envtool/tests_test.go
@@ -15,168 +15,49 @@
 package main
 
 import (
-	"bytes"
-	"os"
-	"strconv"
-	"strings"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestShardIntegrationTests(t *testing.T) {
+func TestTestsShard(t *testing.T) {
 	t.Parallel()
 
-	t.Run("Successfully Shard Integration tests", func(t *testing.T) {
-		var w bytes.Buffer
-		err := shardIntegrationTests(&w, 0, 10)
-		assert.NoError(t, err)
-		assert.NotNil(t, w.Bytes())
-		s := w.String()
-		assert.Regexp(t, "^\\^.*\\$$", s)
+	tests, err := getAllTestNames(filepath.Join("..", "..", "integration"))
+	require.NoError(t, err)
+	assert.Contains(t, tests, "TestQueryCompatLimit")
+	assert.Contains(t, tests, "TestInit") // shareddata's
+
+	t.Run("ShardTestsInvalidIndex", func(t *testing.T) {
+		t.Parallel()
+
+		_, err := shardTests(3, 3, tests)
+		assert.EqualError(t, err, "cannot shard when index is greater or equal to total (3 >= 3)")
 	})
 
-	t.Run("Fail Shard Integration tests - too big index", func(t *testing.T) {
-		var w bytes.Buffer
-		err := shardIntegrationTests(&w, 12, 10)
-		assert.EqualError(t, err, "Cannot shard when Index is greater or equal to Total (12 >= 10)")
-		assert.Nil(t, w.Bytes())
+	t.Run("ShardTestsInvalidTotal", func(t *testing.T) {
+		t.Parallel()
+
+		_, err := shardTests(3, 1000, tests[:42])
+		assert.EqualError(t, err, "cannot shard when Total is greater than amount of tests (1000 > 42)")
 	})
 
-	t.Run("Failp Shard Integration tests - too big index", func(t *testing.T) {
-		var w bytes.Buffer
-		err := shardIntegrationTests(&w, 12, 100000000000000)
-		assert.ErrorContains(t, err, "Cannot shard when Total is greater than amount of tests")
-		assert.Nil(t, w.Bytes())
+	t.Run("ShardTestsValid", func(t *testing.T) {
+		t.Parallel()
+
+		res, err := shardTests(0, 3, tests)
+		require.NoError(t, err)
+		assert.Equal(t, tests[0], res[0])
+		assert.NotEqual(t, tests[1], res[1])
+		assert.NotEqual(t, tests[2], res[1])
+		assert.Equal(t, tests[3], res[1])
+
+		res, err = shardTests(2, 3, tests)
+		require.NoError(t, err)
+		assert.NotEqual(t, tests[0], res[0])
+		assert.NotEqual(t, tests[1], res[0])
+		assert.Equal(t, tests[2], res[0])
 	})
-}
-
-func TestShardTests(t *testing.T) {
-	t.Parallel()
-
-	t.Run("Shard odd amount of tests into 2 parts", func(t *testing.T) {
-		tests := []string{}
-		for i := 0; i < 10; i++ {
-			tests = append(tests, strconv.Itoa(i))
-		}
-
-		testNames, err := shardTests(0, 2, tests...)
-		assert.NoError(t, err)
-		assert.Len(t, testNames, 5)
-		assert.Equal(t, tests[0:5], testNames)
-
-		testNames, err = shardTests(1, 2, tests...)
-		assert.NoError(t, err)
-		assert.Len(t, testNames, 5)
-		assert.Equal(t, tests[5:], testNames)
-	})
-
-	t.Run("Shard even amount of tests into 2 parts", func(t *testing.T) {
-		tests := []string{}
-		for i := 0; i < 11; i++ {
-			tests = append(tests, strconv.Itoa(i))
-		}
-
-		testNames, err := shardTests(0, 2, tests...)
-		assert.NoError(t, err)
-		assert.Len(t, testNames, 5)
-		assert.Equal(t, tests[0:5], testNames)
-
-		testNames, err = shardTests(1, 2, tests...)
-		assert.NoError(t, err)
-		assert.Len(t, testNames, 6)
-		assert.Equal(t, tests[5:], testNames)
-	})
-
-	t.Run("Shard one test when total is 2", func(t *testing.T) {
-		tests := []string{"1"}
-
-		testNames, err := shardTests(0, 2, tests...)
-		assert.EqualError(t, err, "Cannot shard when Total is greater than amount of tests (2 > 1)")
-		assert.Nil(t, testNames)
-	})
-
-	t.Run("Shard empty tests", func(t *testing.T) {
-		tests := []string{}
-
-		testNames, err := shardTests(0, 2, tests...)
-		assert.EqualError(t, err, "Cannot shard when Total is greater than amount of tests (2 > 0)")
-		assert.Nil(t, testNames)
-	})
-
-	t.Run("Index is greater than Total", func(t *testing.T) {
-		tests := []string{}
-
-		testNames, err := shardTests(2, 0, tests...)
-		assert.EqualError(t, err, "Cannot shard when Index is greater or equal to Total (2 >= 0)")
-		assert.Nil(t, testNames)
-	})
-
-	t.Run("Index is equal to Total", func(t *testing.T) {
-		tests := []string{}
-
-		testNames, err := shardTests(0, 0, tests...)
-		assert.EqualError(t, err, "Cannot shard when Index is greater or equal to Total (0 >= 0)")
-		assert.Nil(t, testNames)
-	})
-}
-
-func TestGetAllTestNames(t *testing.T) {
-	t.Parallel()
-
-	testNames, err := getAllTestNames("integration")
-	assert.NoError(t, err)
-	assert.NotEmpty(t, testNames)
-}
-
-func TestPrepareTestNamesOutput(t *testing.T) {
-	t.Parallel()
-
-	t.Run("Fails when the status is not OK", func(t *testing.T) {
-		output := `TestAggregateCommandCompat
-fail      github.com/FerretDB/FerretDB/integration        0.023s`
-
-		tests, err := prepareTestNamesOutput(output)
-		assert.ErrorContains(t, err, "Could not read test names:")
-		assert.Nil(t, tests)
-	})
-
-	t.Run("Passes when the status is OK", func(t *testing.T) {
-		output := `TestAggregateCommandCompat
-TestAggregateCompatStages
-BenchmarkInsertMany
-ok      github.com/FerretDB/FerretDB/integration        0.023s`
-		expectedOutput := []string{"BenchmarkInsertMany", "TestAggregateCommandCompat", "TestAggregateCompatStages"}
-
-		tests, err := prepareTestNamesOutput(output)
-		assert.NoError(t, err)
-		assert.Equal(t, tests, expectedOutput)
-	})
-}
-
-func TestGetNewWorkingDir(t *testing.T) {
-	t.Parallel()
-
-	// while running the tests the current working location is where the test is
-	oldWorkingDir, err := os.Getwd()
-	assert.NoError(t, err)
-	dir, err := getNewWorkingDir("integration")
-	assert.NoError(t, err)
-	assert.NotEqual(t, oldWorkingDir, dir)
-	assert.Contains(t, dir, "integration")
-	assert.NotContains(t, dir, "envtool")
-	assert.True(t, strings.HasSuffix(dir, "/FerretDB/integration"))
-}
-
-func TestGetRootDir(t *testing.T) {
-	t.Parallel()
-
-	oldWorkingDir, err := os.Getwd()
-	assert.NoError(t, err)
-	rootDir, err := getRootDir()
-	assert.NoError(t, err)
-	assert.NotEqual(t, oldWorkingDir, rootDir)
-	assert.NotContains(t, rootDir, "integration")
-	assert.NotContains(t, rootDir, "envtool")
-	assert.True(t, strings.HasSuffix(rootDir, "/FerretDB"))
 }
