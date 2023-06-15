81cb573ce2400429310090bc3dd382395b2ce950
Dmitry <dmitry.eremenko@ferretdb.io>
2023-06-15 16:01:30 +0100

    WIP

--- internal/handlers/pg/msg_createindexes.go
+++ internal/handlers/pg/msg_createindexes.go
@@ -123,12 +123,6 @@ func (h *Handler) MsgCreateIndexes(ctx context.Context, msg *wire.OpMsg) (*wire.
 			"One of the specified indexes already exists with a different key",
 			document.Command(),
 		)
-	case errors.Is(err, pgdb.ErrUniqueViolation):
-		return nil, commonerrors.NewCommandErrorMsgWithArgument(
-			commonerrors.ErrDuplicateKeyInsert,
-			"Index build failed",
-			document.Command(),
-		)
 	case errors.Is(err, nil):
 	default:
 		return nil, lazyerrors.Error(err)
