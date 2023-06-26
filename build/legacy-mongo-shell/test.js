1981f1ee1fbd235a199d25f8a1e49be4106dd7dd
b1ron <goldingbyron@gmail.com>
2023-06-26 14:55:08 +0200

    add test

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -3,5 +3,46 @@
 (function() {
   'use strict';
 
+  const t = db.unwind;
+  t.drop();
+
+  t.insert({_id: 1, a: [1, 2, 3], b: 1});
+  t.insert({_id: 2, a: [4, 5, 6], b: 2});
+
+  const expected = [
+    {
+      '_id': 1,
+      'a': 1,
+      'b': 1,
+    },
+    {
+      '_id': 1,
+      'a': 2,
+      'b': 1,
+    },
+    {
+      '_id': 1,
+      'a': 3,
+      'b': 1,
+    },
+    {
+      '_id': 2,
+      'a': 4,
+      'b': 2,
+    },
+    {
+      '_id': 2,
+      'a': 5,
+      'b': 2,
+    },
+    {
+      '_id': 2,
+      'a': 6,
+      'b': 2,
+    },
+  ];
+
+  assert.eq(t.aggregate([{$unwind: '$a'}]).toArray(), expected);
+
   print('test.js passed!');
 })();
