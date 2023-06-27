164be8d1963fbe7f3904a4cc3bbfff012d97d94a
Dmitry <dmitry.eremenko@ferretdb.io>
2023-06-27 11:36:45 +0100

    WIP

--- internal/handlers/sqlite/msg_delete.go
+++ internal/handlers/sqlite/msg_delete.go
@@ -95,7 +95,7 @@ func execDelete(ctx context.Context, coll backends.Collection, filter *types.Doc
 
 	defer res.Iter.Close()
 
-	ids := make([]any, 0, 0)
+	ids := make([]any, 0)
 
 	for {
 		var doc *types.Document
