35ab798ca5ad6fcae4c38fe8c599e2e0cbef7880
b1ron <goldingbyron@gmail.com>
2023-06-29 16:23:59 +0200

    add test

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -3,5 +3,21 @@
 (function() {
   'use strict';
 
+  const t = db.limit_reached;
+  t.drop();
+
+  t.insert({a: 1});
+  t.insert({a: 2});
+  t.insert({a: 3});
+  t.insert({a: 4});
+
+  let res = db.runCommand({find: t.getName(), batchSize: 3, limit: 4});
+  assert.eq(res.cursor.firstBatch.length, 3);
+
+  // assert that the cursor has been closed when the limit is reached
+  res = db.runCommand({getMore: res.cursor.id, collection: t.getName(), batchSize: 1});
+  assert.eq(1, res.cursor.nextBatch.length);
+  assert.eq(0, res.cursor.id, 'cursor should be closed');
+
   print('test.js passed!');
 })();
