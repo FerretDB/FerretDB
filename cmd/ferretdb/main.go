b4d906f94995a1869bca6c32f2d4cf43e220f291
Dmitry <dmitry.eremenko@ferretdb.io>
2023-06-29 16:54:01 +0100

    WIP

--- cmd/ferretdb/main.go
+++ cmd/ferretdb/main.go
@@ -104,7 +104,7 @@ var pgFlags struct {
 //
 // See main_sqlite.go.
 var sqliteFlags struct {
-	SQLiteURI string `name:"sqlite-uri" default:"." help:"'file' URI for 'sqlite' handler."`
+	SQLiteURI string `default:"." help:"'file' URI for 'sqlite' handler." name:"sqlite-uri" `
 }
 
 // The tigrisFlags struct represents flags that are used by the "tigris" handler.
