26cd7cda24a43c223186a5cdf262c6ca3cbb9572
noisersup <patryk@kwiatek.xyz>
2023-06-30 15:28:54 +0200

    init

--- internal/handlers/sqlite/msg_renamecollection.go
+++ internal/handlers/sqlite/msg_renamecollection.go
@@ -16,12 +16,67 @@ package sqlite
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"strings"
 
-	"github.com/FerretDB/FerretDB/internal/util/must"
+	"github.com/FerretDB/FerretDB/internal/handlers/common"
+	"github.com/FerretDB/FerretDB/internal/handlers/commonerrors"
+	"github.com/FerretDB/FerretDB/internal/handlers/commonparams"
+	"github.com/FerretDB/FerretDB/internal/util/lazyerrors"
 	"github.com/FerretDB/FerretDB/internal/wire"
 )
 
 // MsgRenameCollection implements HandlerInterface.
 func (h *Handler) MsgRenameCollection(ctx context.Context, msg *wire.OpMsg) (*wire.OpMsg, error) {
-	return nil, notImplemented(must.NotFail(msg.Document()).Command())
+	document, err := msg.Document()
+	if err != nil {
+		return nil, lazyerrors.Error(err)
+	}
+
+	unimplementedFields := []string{}
+	if err = common.Unimplemented(document, unimplementedFields...); err != nil {
+		return nil, err
+	}
+
+	command := document.Command()
+
+	dbName, err := common.GetRequiredParam[string](document, "$db")
+	if err != nil {
+		return nil, err
+	}
+
+	namespaceFrom, err := common.GetRequiredParam[string](document, command)
+	if err != nil {
+		from, _ := document.Get(command)
+
+		return nil, commonerrors.NewCommandErrorMsgWithArgument(
+			commonerrors.ErrBadValue,
+			fmt.Sprintf("collection name has invalid type %s", commonparams.AliasFromType(from)),
+			command,
+		)
+	}
+
+	namespaceTo, err := common.GetRequiredParam[string](document, "to")
+	if err != nil {
+		return nil, commonerrors.NewCommandErrorMsgWithArgument(
+			commonerrors.ErrTypeMismatch,
+			"'to' must be of type String",
+			command,
+		)
+	}
+
+	db := h.b.Database(dbName)
+	defer db.Close()
+}
+
+// splitNamespace returns the database and collection name from a given namespace in format "database.collection".
+func splitNamespace(namespace string) (string, string, error) {
+	parts := strings.Split(namespace, ".")
+
+	if len(parts) != 2 || parts[0] == "" || parts[1] == "" {
+		return "", "", errors.New("invalid namespace")
+	}
+
+	return parts[0], parts[1], nil
 }
