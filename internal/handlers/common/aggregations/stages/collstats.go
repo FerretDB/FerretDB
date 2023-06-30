a5cc636fab2872bfd2099f7c7f0a58753f79c38c
Chi Fujii <chi.fujii@ferretdb.io>
2023-06-30 19:07:22 +0900

    Use cursors with iterator in `aggregate` command (#2929)
    Closes #2877.
    Closes #1733.

--- internal/handlers/common/aggregations/stages/collstats.go
+++ internal/handlers/common/aggregations/stages/collstats.go
@@ -118,7 +118,10 @@ func (c *collStats) Process(ctx context.Context, iter types.DocumentsIterator, c
 		panic("collStatsStage: Process: expected 1 document, got more")
 	}
 
-	return iterator.Values(iterator.ForSlice([]*types.Document{res})), nil
+	iter = iterator.Values(iterator.ForSlice([]*types.Document{res}))
+	closer.Add(iter)
+
+	return iter, nil
 }
 
 // Type implements Stage interface.
