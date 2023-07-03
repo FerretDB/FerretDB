551f4b30cb08097bc9c863c6cdb777a7c0be1740
Dmitry <dmitry.eremenko@ferretdb.io>
2023-07-03 11:48:51 +0100

    WIP

--- internal/handlers/sqlite/msg_ping.go
+++ internal/handlers/sqlite/msg_ping.go
@@ -18,12 +18,24 @@ import (
 	"context"
 
 	"github.com/FerretDB/FerretDB/internal/types"
+	"github.com/FerretDB/FerretDB/internal/util/lazyerrors"
 	"github.com/FerretDB/FerretDB/internal/util/must"
 	"github.com/FerretDB/FerretDB/internal/wire"
 )
 
 // MsgPing implements HandlerInterface.
 func (h *Handler) MsgPing(ctx context.Context, msg *wire.OpMsg) (*wire.OpMsg, error) {
+	res, err := h.b.Database("?").Collection("?").Query(ctx, nil)
+	if err != nil {
+		return nil, lazyerrors.Error(err)
+	}
+	defer res.Iter.Close()
+
+	_, _, err = res.Iter.Next()
+	if err != nil {
+		return nil, lazyerrors.Error(err)
+	}
+
 	var reply wire.OpMsg
 	must.NoError(reply.SetSections(wire.OpMsgSection{
 		Documents: []*types.Document{must.NotFail(types.NewDocument(
