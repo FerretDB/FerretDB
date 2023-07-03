5e7608225dbcc8a5d34174e8a18d17b37871ea9c
Chi Fujii <chi.fujii@ferretdb.io>
2023-07-03 16:20:48 +0900

    add error

--- internal/handlers/commonerrors/error.go
+++ internal/handlers/commonerrors/error.go
@@ -71,6 +71,9 @@ const (
 	// ErrNamespaceExists indicates that the collection already exists.
 	ErrNamespaceExists = ErrorCode(48) // NamespaceExists
 
+	// ErrMaxtimeMSExpired indicates operation was timed out.
+	ErrMaxtimeMSExpired = ErrorCode(50) // ErrMaxtimeMSExpired
+
 	// ErrDollarPrefixedFieldName indicates the field name is prefixed with $.
 	ErrDollarPrefixedFieldName = ErrorCode(52) // DollarPrefixedFieldName
 
