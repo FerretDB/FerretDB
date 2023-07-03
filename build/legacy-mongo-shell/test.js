89cab2a828480f9d5f30cc1ebe9c3c483986b16a
b1ron <goldingbyron@gmail.com>
2023-07-03 15:24:32 +0200

    add test

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -3,5 +3,18 @@
 (function() {
   'use strict';
 
+  const maxBsonObjectSize = db.hello().maxBsonObjectSize;
+  const docOverhead = Object.bsonsize({_id: new ObjectId(), x: ''});
+  const maxStrSize = maxBsonObjectSize - docOverhead;
+  const maxStr = 'a'.repeat(maxStrSize);
+  const coll = db.max_doc_size;
+
+  assert.commandWorked(coll.insert({_id: new ObjectId(), x: maxStr}));
+
+  const largerThanMaxString = maxStr + 'a';
+
+  coll.drop();
+  assert.commandFailedWithCode(coll.insert({_id: new ObjectId(), x: largerThanMaxString}), 2);
+
   print('test.js passed!');
 })();
