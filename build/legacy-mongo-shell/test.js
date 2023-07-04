ed2e6034cde7a3833c422591b363919af0b65ec0
b1ron <goldingbyron@gmail.com>
2023-07-04 16:28:35 +0200

    add test

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -3,5 +3,16 @@
 (function() {
   'use strict';
 
+  const t = db.a;
+  t.drop();
+
+  t.insert({a: []});
+  t.insert({a: [{b: 1}]});
+  t.insert({a: [{c: 1}]});
+  t.insert({a: [{b: 1}, {}]});
+
+  assert.eq(2, t.count({'a.b': null}));
+  assert.eq(2, t.count({a: {$elemMatch: {b: 1}}}));
+
   print('test.js passed!');
 })();
