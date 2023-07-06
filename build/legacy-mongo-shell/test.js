ecbb0965a54c0e576b6010cd149e525c4447de27
b1ron <goldingbyron@gmail.com>
2023-07-06 23:16:08 +0200

    add test

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -3,5 +3,24 @@
 (function() {
   'use strict';
 
+  DBQuery.shellBatchSize = 1;
+
+  const coll = db.block_forever;
+  coll.drop();
+
+  // create a random index
+  coll.createIndex({x: 1});
+
+  coll.insert({});
+  coll.insert({});
+  
+  // create a cursor
+  const cursor = coll.find().batchSize(1);
+  assert.eq(true, cursor.hasNext());
+  cursor.next();
+
+  print('about to block forever');
+  assert.commandWorked(coll.dropIndex({x: 1}));
+
   print('test.js passed!');
 })();
