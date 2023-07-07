0166f7e13ad03a76cb22d43c489a9abff93353fd
noisersup <patryk@kwiatek.xyz>
2023-07-07 13:43:19 +0200

    panic test

--- internal/backends/sqlite/metadata/registry_test.go
+++ internal/backends/sqlite/metadata/registry_test.go
@@ -14,8 +14,68 @@
 
 package metadata
 
-import "testing"
+import (
+	"database/sql"
+	"os"
+	"testing"
 
-func TestDummy(t *testing.T) {
-	// we need at least one test per package to correctly calculate coverage
+	"github.com/FerretDB/FerretDB/internal/util/testutil"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestInTransactionRollbackOnPanic(t *testing.T) {
+	os.Remove("./testdata")
+	require.NoError(t, os.Mkdir("./testdata", 0x655))
+
+	r, err := NewRegistry("file:"+t.TempDir()+"/", testutil.Logger(t))
+	require.NoError(t, err)
+
+	ctx := testutil.Ctx(t)
+
+	db, err := r.DatabaseGetOrCreate(ctx, t.Name())
+	require.NoError(t, err)
+
+	require.Panics(t,
+		func() {
+			err = inTransaction(ctx, db, func(tx *sql.Tx) error {
+				_, err = tx.ExecContext(ctx, "CREATE TABLE test (foo TEXT)")
+				require.NoError(t, err)
+
+				rows, err := tx.QueryContext(ctx, "SELECT name FROM sqlite_schema WHERE type='table'")
+				require.NoError(t, err)
+				defer rows.Close()
+
+				var tables []string
+
+				for rows.Next() {
+					var name string
+					err = rows.Scan(&name)
+					require.NoError(t, err)
+
+					tables = append(tables, name)
+				}
+
+				// Check if table was actually created
+				require.Equal(t, []string{"_ferretdb_collections", "test"}, tables)
+
+				panic("Unexpected panic in transaction!")
+			})
+		})
+
+	rows, err := db.QueryContext(ctx, "SELECT name FROM sqlite_schema WHERE type='table'")
+	require.NoError(t, err)
+	defer rows.Close()
+
+	var tables []string
+
+	for rows.Next() {
+		var name string
+		err = rows.Scan(&name)
+		require.NoError(t, err)
+
+		tables = append(tables, name)
+	}
+
+	assert.Equal(t, []string{"_ferretdb_collections"}, tables)
 }
