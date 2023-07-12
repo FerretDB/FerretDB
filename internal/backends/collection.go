a718823d9b6fd012c77d17529bd4bc55e66c2ab9
Alexey Palazhchenko <alexey.palazhchenko@ferretdb.io>
2023-07-12 09:51:38 +0400

    Fix error checking in backend contracts (#3031)

--- internal/backends/collection.go
+++ internal/backends/collection.go
@@ -73,12 +73,13 @@ type QueryResult struct {
 // The passed context should be used for canceling the initial query.
 // It also can be used to close the returned iterator and free underlying resources,
 // but doing so is not necessary - the handler will do that anyway.
-func (cc *collectionContract) Query(ctx context.Context, params *QueryParams) (res *QueryResult, err error) {
+func (cc *collectionContract) Query(ctx context.Context, params *QueryParams) (*QueryResult, error) {
 	defer observability.FuncCall(ctx)()
-	defer checkError(err)
-	res, err = cc.c.Query(ctx, params)
 
-	return
+	res, err := cc.c.Query(ctx, params)
+	checkError(err)
+
+	return res, err
 }
 
 // InsertParams represents the parameters of Collection.Insert method.
@@ -96,12 +97,13 @@ type InsertResult struct {
 // Insert inserts documents into the collection.
 //
 // Both database and collection may or may not exist; they should be created automatically if needed.
-func (cc *collectionContract) Insert(ctx context.Context, params *InsertParams) (res *InsertResult, err error) {
+func (cc *collectionContract) Insert(ctx context.Context, params *InsertParams) (*InsertResult, error) {
 	defer observability.FuncCall(ctx)()
-	defer checkError(err)
-	res, err = cc.c.Insert(ctx, params)
 
-	return
+	res, err := cc.c.Insert(ctx, params)
+	checkError(err)
+
+	return res, err
 }
 
 // UpdateParams represents the parameters of Collection.Update method.
@@ -115,12 +117,13 @@ type UpdateResult struct {
 }
 
 // Update updates documents in collection.
-func (cc *collectionContract) Update(ctx context.Context, params *UpdateParams) (res *UpdateResult, err error) {
+func (cc *collectionContract) Update(ctx context.Context, params *UpdateParams) (*UpdateResult, error) {
 	defer observability.FuncCall(ctx)()
-	defer checkError(err)
-	res, err = cc.c.Update(ctx, params)
 
-	return
+	res, err := cc.c.Update(ctx, params)
+	checkError(err)
+
+	return res, err
 }
 
 // DeleteParams represents the parameters of Collection.Delete method.
@@ -136,12 +139,13 @@ type DeleteResult struct {
 // Delete deletes documents in collection.
 //
 // If requested database or collection does not exist it returns 0 deleted documents.
-func (cc *collectionContract) Delete(ctx context.Context, params *DeleteParams) (res *DeleteResult, err error) {
+func (cc *collectionContract) Delete(ctx context.Context, params *DeleteParams) (*DeleteResult, error) {
 	defer observability.FuncCall(ctx)()
-	defer checkError(err)
-	res, err = cc.c.Delete(ctx, params)
 
-	return
+	res, err := cc.c.Delete(ctx, params)
+	checkError(err)
+
+	return res, err
 }
 
 // check interfaces
