873e07a2123db5c1be8e7983d03355a3620ddc10
Alexey Palazhchenko <alexey.palazhchenko@ferretdb.io>
2023-07-18 16:48:16 +0400

    Mark some tests as failing for SQLite (#3051)

--- internal/handlers/sqlite/msg_explain.go
+++ internal/handlers/sqlite/msg_explain.go
@@ -55,13 +55,21 @@ func (h *Handler) MsgExplain(ctx context.Context, msg *wire.OpMsg) (*wire.OpMsg,
 	cmd := params.Command
 	cmd.Set("$db", params.DB)
 
+	queryPlanner := types.MakeDocument(0)
+
 	var reply wire.OpMsg
 	must.NoError(reply.SetSections(wire.OpMsgSection{
 		Documents: []*types.Document{must.NotFail(types.NewDocument(
+			"queryPlanner", queryPlanner,
 			"explainVersion", "1",
 			"command", cmd,
-			"pushdown", !h.DisableFilterPushdown,
 			"serverInfo", serverInfo,
+
+			// our extensions
+			"pushdown", !h.DisableFilterPushdown,
+			"sortingPushdown", false,
+			"limitPushdown", false,
+
 			"ok", float64(1),
 		))},
 	}))
