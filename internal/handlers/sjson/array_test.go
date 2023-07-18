06b4ad43ea63f13df93f38eb0dca35fb9322a4a5
Iskander Sharipov <quasilyte@gmail.com>
2023-07-18 16:20:04 +0400

    improve sjson package fuzzing
    Previously, only FuzzDocument was reaching the marshal+unmarshal
    steps due to the document type assertion. If it wasn't a documentType,
    the rest of the fuzzing function was skipped.
    
    Since data validation is available only via `types.Document.ValidateData`,
    a helper `isValidDocumentData` function is introduced. It may go
    away in the future. But for now, it allows all sjson types to pass
    the validation.
    
    The other change is related to the way we fuzz sjson.
    Instead of generating both values and schemes at the same time,
    focus on one thing at the time: a fixed scheme against the generated
    and fixed documents against the generated schemes.
    
    This approach can be further improved by the increased number
    of relevant corpus seed entries, but that's a different issue (#3067).
    
    To answer the question is it any better: yes, it is.
    The old approach skipped any cases where either of those two
    was invalid; it was a very rare occasion for a fuzz runner
    to generate a valid JSON for both document and scheme.
    It's even more rare to get both of them even remotely compatible.
    
    Updates #1273

--- internal/handlers/sjson/array_test.go
+++ internal/handlers/sjson/array_test.go
@@ -59,6 +59,60 @@ var arrayTestCases = []testCase{
 		},
 		j: `[[],"Qg==",true,1627378542123,{},42.13,42,42,"foo",null]`,
 	}, {
+		// Unlike "array_all" case, this one passes the Fuzz checks successfully.
+		// Most notably, it doesn't have a nested array that is rejected by the data validation.
+		name: "array_without_nested",
+		v: convertArray(must.NotFail(types.NewArray(
+			"",
+			false,
+			must.NotFail(types.NewDocument("key", "value")),
+			-32589.300000101,
+			int32(349),
+			int64(-21424182),
+			types.Null,
+			"a string that is somewhat longer",
+		))),
+		sch: &elem{
+			Type: elemTypeArray,
+			Items: []*elem{
+				stringSchema,
+				boolSchema,
+				{
+					Type: elemTypeObject,
+					Schema: &schema{
+						Properties: map[string]*elem{
+							"key": stringSchema,
+						},
+						Keys: []string{"key"},
+					},
+				},
+				doubleSchema,
+				intSchema,
+				longSchema,
+				nullSchema,
+				stringSchema,
+			},
+		},
+		j: `["",false,{"key":"value"},-32589.300000101,349,-21424182,null,"a string that is somewhat longer"]`,
+	}, {
+		name: "array_all",
+		v:    convertArray(must.NotFail(types.NewArray("a"))),
+		sch: &elem{
+			Type:  elemTypeArray,
+			Items: []*elem{nullSchema},
+		},
+		j:    `["a"]`,
+		jErr: `sjson.unmarshalSingleValue: expected null, got "a"`,
+	}, {
+		name: "array10",
+		v:    convertArray(must.NotFail(types.NewArray(0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0))),
+		sch: &elem{
+			Type:  elemTypeArray,
+			Items: []*elem{doubleSchema, doubleSchema, doubleSchema, doubleSchema, doubleSchema, doubleSchema, doubleSchema, doubleSchema, doubleSchema, doubleSchema},
+		},
+		j:      `[0,1.0,2,3.0,4.0,5.0,6,7,8,9]`,
+		canonJ: `[0,1,2,3,4,5,6,7,8,9]`,
+	}, {
 		name: "EOF",
 		sch:  &elem{Type: elemTypeArray, Items: []*elem{}},
 		j:    `[`,
@@ -86,8 +140,12 @@ func TestArray(t *testing.T) {
 	testJSON(t, arrayTestCases, func() sjsontype { return new(arrayType) })
 }
 
-func FuzzArray(f *testing.F) {
-	fuzzJSON(f, arrayTestCases, func() sjsontype { return new(arrayType) })
+func FuzzArrayWithFixedSchemas(f *testing.F) {
+	fuzzJSONWithFixedSchemas(f, arrayTestCases, func() sjsontype { return new(arrayType) })
+}
+
+func FuzzArrayWithFixedDocuments(f *testing.F) {
+	fuzzJSONWithFixedDocuments(f, arrayTestCases, func() sjsontype { return new(arrayType) })
 }
 
 func BenchmarkArray(b *testing.B) {
