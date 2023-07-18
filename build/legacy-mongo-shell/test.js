3eb3aaf36272ad685daee6bcfd345d137e9e6d94
b1ron <goldingbyron@gmail.com>
2023-07-18 12:03:27 +0200

    add test

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -2,6 +2,10 @@
 
 (function() {
   'use strict';
+    
+  let res = db.runCommand({"isMaster": 1, "client": {"application": "foobar"}});
+  assert.commandFailed(res);
+  assert.eq(res.code, ErrorCodes.ClientMetadataCannotBeMutated, tojson(res));
 
   print('test.js passed!');
 })();
