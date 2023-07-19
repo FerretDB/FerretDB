5bcbf601e9e48bddbdfaa8ef04b844d449047292
Elena Grahovac <elena@grahovac.me>
2023-07-19 16:42:20 +0200

    Demonstrate a bug with unwind

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -3,5 +3,24 @@
 (function() {
   'use strict';
 
+  const coll = db.test;
+
+  coll.drop();
+
+  const init = [
+    {_id: 1, arr: [1, 2, 3], foo: 42},
+  ];
+
+  coll.insertMany(init);
+
+  const expected = [
+    { _id: 1, arr: 1, foo: 42 },
+    { _id: 1, arr: 2, foo: 42 },
+    { _id: 1, arr: 3, foo: 42 }
+  ];
+
+  const actual = coll.aggregate([{$unwind: "$arr"}]).toArray();
+  assert.eq(expected, actual);
+
   print('test.js passed!');
 })();
