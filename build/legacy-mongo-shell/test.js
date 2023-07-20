d71d503257daeac94c20df34dd4d04d99c769ab0
Elena Grahovac <elena@grahovac.me>
2023-07-20 08:15:21 +0200

    Demo problem with group

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -1,7 +1,26 @@
 // Please do not merge changes in this file.
 
+
 (function() {
   'use strict';
+
+  const coll = db.test;
+
+  coll.drop();
+
+  const init = [
+    {_id: 'double', v: 42.13},
+  ];
+
+  coll.insertMany(init);
+
+
+  const expected = [
+    { _id: { foo: 'double', bar: 42.13 } },
+  ];
+
+  const actual = coll.aggregate([ {$group: {_id: {foo: "$_id", bar: "$v"}}}]).toArray();
+  assert.eq(expected, actual);
 
   print('test.js passed!');
 })();
