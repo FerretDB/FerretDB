9af4b54c2beeb743e7be02fa2ae8737fa4152e2d
Elena Grahovac <elena@grahovac.me>
2023-07-20 08:26:39 +0200

    Demo problem with project

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -3,5 +3,24 @@
 (function() {
   'use strict';
 
+  const coll = db.test;
+
+  coll.drop();
+
+  const init = [
+    {_id: 'double', v: 42.13},
+  ];
+
+  coll.insertMany(init);
+
+
+  const expected = [
+    { _id: 42.13 },
+  ];
+
+  const actual = coll.coll.aggregate([ {$group: {_id: "$v"}}, {$project: {_id: "$_id"}}]).toArray();
+  assert.eq(expected, actual);
+
+
   print('test.js passed!');
 })();
