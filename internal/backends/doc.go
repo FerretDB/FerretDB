1c72efc362d215c0f23462dd07cffa8cb7d07594
Alexey Palazhchenko <alexey.palazhchenko@ferretdb.io>
2023-07-26 15:50:09 +0400

    Update comment

--- internal/backends/doc.go
+++ internal/backends/doc.go
@@ -16,7 +16,7 @@
 //
 // # Design principles.
 //
-//  1. Interfaces are relatively high-level and "fat".
+//  1. Interfaces are relatively high-level and "fat" (or not).
 //     We are generally doing one backend interface call per handler call.
 //     For example, `insert` command handler calls only
 //     `db.Database("database").Collection("collection").Insert(ctx, params)` FIXME method that would
@@ -33,4 +33,7 @@
 //     *Error values can't be wrapped or be present anywhere in the error chain.
 //     Contracts enforce *Error codes; they are not documented in the code comments
 //     but are visible in the contract's code (to avoid duplication).
+//
+// Figure it out, especially point number 1. Update, expand, etc.
+// TODO https://github.com/FerretDB/FerretDB/issues/3069
 package backends
