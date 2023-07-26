aa34f84e9b26907f6f6e0e34ba19071fa22b389c
Alexey Palazhchenko <alexey.palazhchenko@ferretdb.io>
2023-07-26 13:57:16 +0400

    Fix pushdown for `find` with `filter` and `limit`
    Closes #3113.

--- internal/handlers/pg/msg_find.go
+++ internal/handlers/pg/msg_find.go
@@ -76,7 +76,7 @@ func (h *Handler) MsgFind(ctx context.Context, msg *wire.OpMsg) (*wire.OpMsg, er
 	// Sorting requires fetching all documents and sorting them in memory unless `EnableSortPushdown` is set.
 	// Limit pushdown is not applied when `sort` is set but `EnableSortPushdown` is not set.
 	// Skip pushdown is not supported yet, limit pushdown is not applied when `skip` is non-zero value.
-	if (params.Sort.Len() == 0 || h.EnableSortPushdown) && params.Skip == 0 {
+	if params.Filter.Len() == 0 && (params.Sort.Len() == 0 || h.EnableSortPushdown) && params.Skip == 0 {
 		qp.Limit = params.Limit
 	}
 
