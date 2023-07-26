d3ddb0f90fc1fd9ce5a18aa25a6eccec24be3b00
b1ron <goldingbyron@gmail.com>
2023-07-26 15:56:27 +0200

    add test

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -3,5 +3,21 @@
 (function() {
   'use strict';
 
+  const t = db.foo;
+  t.drop();
+
+  // ping works with the apiVersion set.
+  assert.commandWorked(t.runCommand({ping: 1}));
+  assert.commandWorked(t.runCommand({ping: 1, apiVersion: '1'}));
+  jsTestLog("ping with apiVersion passed");
+
+  assert.commandWorked(t.runCommand({insert: "foo", documents: [{}]}));
+  assert.eq(1, t.count());
+
+  // CRUD operations do not work.
+  assert.commandWorked(t.runCommand({insert: "foo", documents: [{}], apiVersion: '1'}));
+  assert.eq(2, t.count());
+  jsTestLog("insert with apiVersion passed");
+
   print('test.js passed!');
 })();
