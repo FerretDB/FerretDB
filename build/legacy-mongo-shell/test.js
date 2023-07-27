5e07709e4df86b766376ade05837c0bd75657ffb
b1ron <goldingbyron@gmail.com>
2023-07-27 15:54:33 +0200

    add more commands

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -6,18 +6,54 @@
   const t = db.foo;
   t.drop();
 
-  // ping works with the apiVersion set.
+  // works.
   assert.commandWorked(t.runCommand({ping: 1}));
-  assert.commandWorked(t.runCommand({ping: 1, apiVersion: '1'}));
-  jsTestLog("ping with apiVersion passed");
+  assert.commandWorked(t.runCommand({ping: 1, apiVersion: '1', apiStrict: true, apiDeprecationErrors: true}));
+  jsTestLog("ping with Stable API parameters passed");
 
   assert.commandWorked(t.runCommand({insert: "foo", documents: [{}]}));
   assert.eq(1, t.count());
 
-  // CRUD operations do not work.
-  assert.commandWorked(t.runCommand({insert: "foo", documents: [{}], apiVersion: '1'}));
+  assert.commandWorked(t.runCommand({insert: "foo", documents: [{}], apiVersion: '1', apiStrict: true, apiDeprecationErrors: true}));
   assert.eq(2, t.count());
-  jsTestLog("insert with apiVersion passed");
+  jsTestLog("insert with Stable API parameters passed");
+
+  assert.commandWorked(t.runCommand({count: "foo", apiVersion: '1', apiStrict: true, apiDeprecationErrors: true}));
+  jsTestLog("count with Stable API parameters passed");
+
+  // works
+  assert.commandWorked(t.runCommand({aggregate: 'test', pipeline: [], cursor: {}, apiVersion: '1', apiStrict: true, apiDeprecationErrors: true}));
+  jsTestLog("aggregate with Stable API parameters passed");
+
+  // depends on the x.509 authentication mechanism
+  assert.commandWorked(db.auth({user: "username", pwd: "password", apiVersion: '1', apiStrict: true, apiDeprecationErrors: true}));
+  jsTestLog("authenticate with Stable API parameters passed");
+
+  // not implemented yet
+  assert.commandWorked(t.runCommand({collMod: "foo", apiVersion: '1', apiStrict: true, apiDeprecationErrors: true}));
+  jsTestLog("collMod with Stable API parameters passed");
+
+  // commitTransaction
+
+  // works
+  assert.commandWorked(t.runCommand({create: "bar", apiVersion: '1', apiStrict: true, apiDeprecationErrors: true}));
+  jsTestLog("collMod with Stable API parameters passed");
+
+  // works
+  assert.commandWorked(t.runCommand({createIndexes: "bar", indexes: [{key: {a: 1}, name: "a"}], apiVersion: '1', apiStrict: true, apiDeprecationErrors: true}));
+  jsTestLog("createIndexes with Stable API parameters passed");
+
+  assert.commandWorked(t.runCommand({delete: "bar", apiVersion: '1', apiStrict: true, apiDeprecationErrors: true}));
+  jsTestLog("delete with Stable API parameters passed");
+
+  assert.commandWorked(t.runCommand({drop: "bar", deletes: [], apiVersion: '1', apiStrict: true, apiDeprecationErrors: true}));
+  jsTestLog("drop with Stable API parameters passed");
+
+  assert.commandWorked(t.runCommand({dropDatabase: "test", apiVersion: '1', apiStrict: true, apiDeprecationErrors: true}));
+  jsTestLog("dropDatabase with Stable API parameters passed");
+
+  assert.commandWorked(t.runCommand({dropIndexes: "bar", index: "a_1", apiVersion: '1', apiStrict: true, apiDeprecationErrors: true}));
+  jsTestLog("dropIndexes with Stable API parameters passed");
 
   print('test.js passed!');
 })();
