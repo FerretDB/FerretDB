7b0d76ed00a2b24be701d547534170dd46d2de5b
Andrea Di Cesare <andrea@softinstigate.com>
2023-07-31 15:46:42 +0200

    :white_check_mark: Update test for issu 2803

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -1,7 +1,19 @@
 // Please do not merge changes in this file.
 
 (function() {
-  'use strict';
+    'use strict';
 
-  print('test.js passed!');
-})();
+    const coll = db.test;
+
+    const expected = { _id: 1, a: 1 };
+
+    coll.drop();
+
+    coll.insertOne({_id:1});
+
+    const actual = coll.findOneAndReplace({_id:1},{_id:1,a:2},{upsert:true,returnDocument:'after'})
+
+    assert.eq(expected, actual);
+
+    print('test issue 2830.js passed!');
+  })();
