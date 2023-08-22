dfad9ca14f8aab3c6ba3d95760736649f95b1836
b1ron <goldingbyron@gmail.com>
2023-08-22 11:31:25 +0200

    add test

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -3,5 +3,27 @@
 (function() {
   'use strict';
 
+  const t = db.foo;
+
+  t.drop();
+
+  const filter = {_id: 1};
+
+  t.insert({_id: 1, text: 'a', n: 42});
+
+  t.update(filter,
+      {$set: {text: 'b'}, $setOnInsert: {_id: 2, n: 42}},
+      {upsert: true},
+  );
+
+  assert.docEq([{_id: 1, text: 'b', n: 42}], t.find().toArray());
+
+  t.update(filter,
+      {$set: {n: -1}, $setOnInsert: {_id: 3, text: 'a'}},
+      {upsert: true},
+  );
+
+  assert.docEq([{_id: 1, text: 'b', n: -1}], t.find().toArray());
+
   print('test.js passed!');
 })();
