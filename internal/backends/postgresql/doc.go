4c3a4d625845622e60714e05d9373bc78c0698cb
Alexey Palazhchenko <alexey.palazhchenko@ferretdb.io>
2023-08-25 14:15:50 +0400

    Rename backend

--- internal/backends/postgresql/doc.go
+++ internal/backends/postgresql/doc.go
@@ -12,5 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package pg provides backend for PostgreSQL and compatible databases.
-package pg
+// Package postgresql provides backend for PostgreSQL and compatible databases.
+package postgresql
