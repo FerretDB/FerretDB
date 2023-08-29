889af91faa82bb03216809b3d32cf1b5368a5f65
Elena Grahovac <elena@grahovac.me>
2023-08-29 21:53:02 +0200

    wip

--- internal/handlers/sqlite/msg_getlog.go
+++ internal/handlers/sqlite/msg_getlog.go
@@ -16,12 +16,144 @@ package sqlite
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"strings"
+	"time"
 
+	"go.uber.org/zap"
+
+	"github.com/FerretDB/FerretDB/build/version"
+	"github.com/FerretDB/FerretDB/internal/handlers/commonerrors"
+	"github.com/FerretDB/FerretDB/internal/handlers/commonparams"
+	"github.com/FerretDB/FerretDB/internal/types"
+	"github.com/FerretDB/FerretDB/internal/util/lazyerrors"
+	"github.com/FerretDB/FerretDB/internal/util/logging"
 	"github.com/FerretDB/FerretDB/internal/util/must"
 	"github.com/FerretDB/FerretDB/internal/wire"
 )
 
 // MsgGetLog implements HandlerInterface.
 func (h *Handler) MsgGetLog(ctx context.Context, msg *wire.OpMsg) (*wire.OpMsg, error) {
-	return nil, notImplemented(must.NotFail(msg.Document()).Command())
+	document, err := msg.Document()
+	if err != nil {
+		return nil, lazyerrors.Error(err)
+	}
+
+	command := document.Command()
+
+	getLog, err := document.Get(command)
+	if err != nil {
+		return nil, lazyerrors.Error(err)
+	}
+
+	if _, ok := getLog.(types.NullType); ok {
+		return nil, commonerrors.NewCommandErrorMsg(
+			commonerrors.ErrMissingField,
+			`BSON field 'getLog.getLog' is missing but a required field`,
+		)
+	}
+
+	if _, ok := getLog.(string); !ok {
+		return nil, commonerrors.NewCommandError(
+			commonerrors.ErrTypeMismatch,
+			fmt.Errorf(
+				"BSON field 'getLog.getLog' is the wrong type '%s', expected type 'string'",
+				commonparams.AliasFromType(getLog),
+			),
+		)
+	}
+
+	var resDoc *types.Document
+
+	switch getLog {
+	case "*":
+		resDoc = must.NotFail(types.NewDocument(
+			"names", must.NotFail(types.NewArray("global", "startupWarnings")),
+			"ok", float64(1),
+		))
+
+	case "global":
+		log, err := logging.RecentEntries.GetArray(zap.DebugLevel)
+		if err != nil {
+			return nil, lazyerrors.Error(err)
+		}
+		resDoc = must.NotFail(types.NewDocument(
+			"log", log,
+			"totalLinesWritten", int64(log.Len()),
+			"ok", float64(1),
+		))
+
+	case "startupWarnings":
+		state := h.StateProvider.Get()
+
+		info := version.Get()
+
+		// it may be empty if no connection was established yet
+		hv, _, _ := strings.Cut(state.HandlerVersion, " ")
+		if hv != "" {
+			hv = " " + hv
+		}
+
+		startupWarnings := []string{
+			"Powered by FerretDB " + info.Version + " and PostgreSQL" + hv + ".",
+			"Please star us on GitHub: https://github.com/FerretDB/FerretDB.",
+		}
+
+		switch {
+		case state.Telemetry == nil:
+			startupWarnings = append(
+				startupWarnings,
+				"The telemetry state is undecided.",
+				"Read more about FerretDB telemetry and how to opt out at https://beacon.ferretdb.io.",
+			)
+		case state.UpdateAvailable:
+			startupWarnings = append(
+				startupWarnings,
+				fmt.Sprintf(
+					"A new version available! The latest version: %s. The current version: %s.",
+					state.LatestVersion, info.Version,
+				),
+			)
+		}
+
+		var log types.Array
+
+		for _, line := range startupWarnings {
+			b, err := json.Marshal(map[string]any{
+				"msg":  line,
+				"tags": []string{"startupWarnings"},
+				"s":    "I",
+				"c":    "STORAGE",
+				"id":   42000,
+				"ctx":  "initandlisten",
+				"t": map[string]string{
+					"$date": time.Now().UTC().Format("2006-01-02T15:04:05.999Z07:00"),
+				},
+			})
+			if err != nil {
+				return nil, lazyerrors.Error(err)
+			}
+
+			log.Append(string(b))
+		}
+		resDoc = must.NotFail(types.NewDocument(
+			"log", &log,
+			"totalLinesWritten", int64(log.Len()),
+			"ok", float64(1),
+		))
+
+	default:
+		return nil, commonerrors.NewCommandError(
+			commonerrors.ErrOperationFailed,
+			fmt.Errorf("no RecentEntries named: %s", getLog),
+		)
+	}
+
+	var reply wire.OpMsg
+	must.NoError(reply.SetSections(wire.OpMsgSection{
+		Documents: []*types.Document{resDoc},
+	}))
+
+	return &reply, nil
 }
