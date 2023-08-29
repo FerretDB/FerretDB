a2e17ffb2d3e182b2dcef04aebb7a4762da12425
Chi Fujii <chi.fujii@ferretdb.io>
2023-08-29 11:46:41 +0900

    implement distinct for SQLite

--- internal/handlers/sqlite/msg_distinct.go
+++ internal/handlers/sqlite/msg_distinct.go
@@ -16,12 +16,76 @@ package sqlite
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/FerretDB/FerretDB/internal/backends"
+	"github.com/FerretDB/FerretDB/internal/handlers/common"
+	"github.com/FerretDB/FerretDB/internal/handlers/commonerrors"
+	"github.com/FerretDB/FerretDB/internal/types"
+	"github.com/FerretDB/FerretDB/internal/util/iterator"
+	"github.com/FerretDB/FerretDB/internal/util/lazyerrors"
 	"github.com/FerretDB/FerretDB/internal/util/must"
 	"github.com/FerretDB/FerretDB/internal/wire"
 )
 
 // MsgDistinct implements HandlerInterface.
 func (h *Handler) MsgDistinct(ctx context.Context, msg *wire.OpMsg) (*wire.OpMsg, error) {
-	return nil, notImplemented(must.NotFail(msg.Document()).Command())
+	document, err := msg.Document()
+	if err != nil {
+		return nil, lazyerrors.Error(err)
+	}
+
+	params, err := common.GetDistinctParams(document, h.L)
+	if err != nil {
+		return nil, err
+	}
+
+	db, err := h.b.Database(params.DB)
+	if err != nil {
+		if backends.ErrorCodeIs(err, backends.ErrorCodeDatabaseNameIsInvalid) {
+			msg := fmt.Sprintf("Invalid namespace specified '%s.%s'", params.DB, params.Collection)
+			return nil, commonerrors.NewCommandErrorMsgWithArgument(commonerrors.ErrInvalidNamespace, msg, "find")
+		}
+
+		return nil, lazyerrors.Error(err)
+	}
+	defer db.Close()
+
+	c, err := db.Collection(params.Collection)
+	if err != nil {
+		if backends.ErrorCodeIs(err, backends.ErrorCodeCollectionNameIsInvalid) {
+			msg := fmt.Sprintf("Invalid collection name: %s", params.Collection)
+			return nil, commonerrors.NewCommandErrorMsgWithArgument(commonerrors.ErrInvalidNamespace, msg, "find")
+		}
+
+		return nil, lazyerrors.Error(err)
+	}
+
+	closer := iterator.NewMultiCloser()
+	defer closer.Close()
+
+	// TODO https://github.com/FerretDB/FerretDB/issues/3235
+	queryRes, err := c.Query(ctx, nil)
+	if err != nil {
+		return nil, lazyerrors.Error(err)
+	}
+
+	closer.Add(queryRes.Iter)
+
+	iter := common.FilterIterator(queryRes.Iter, closer, params.Filter)
+
+	distinct, err := common.FilterDistinctValues(iter, params.Key)
+	if err != nil {
+		return nil, lazyerrors.Error(err)
+	}
+
+	var reply wire.OpMsg
+	must.NoError(reply.SetSections(wire.OpMsgSection{
+		Documents: []*types.Document{must.NotFail(types.NewDocument(
+			"values", distinct,
+			"ok", float64(1),
+		))},
+	}))
+
+	return &reply, nil
 }
