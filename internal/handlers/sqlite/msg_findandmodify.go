bb06c65f95a96084eca937485820752711d92ceb
Chi Fujii <chi.fujii@ferretdb.io>
2023-09-05 17:23:50 +0900

    cleanup

--- internal/handlers/sqlite/msg_findandmodify.go
+++ internal/handlers/sqlite/msg_findandmodify.go
@@ -95,9 +95,6 @@ func (h *Handler) MsgFindAndModify(ctx context.Context, msg *wire.OpMsg) (*wire.
 
 // findAndModifyDocument finds and modifies a single document.
 func (h *Handler) findAndModifyDocument(ctx context.Context, params *common.FindAndModifyParams) (*findAndModifyResult, error) {
-	var modified int32
-	var updateExisting, upserted, value any
-
 	db, err := h.b.Database(params.DB)
 	if err != nil {
 		if backends.ErrorCodeIs(err, backends.ErrorCodeDatabaseNameIsInvalid) {
@@ -158,122 +155,50 @@ func (h *Handler) findAndModifyDocument(ctx context.Context, params *common.Find
 
 	_, v, err := iter.Next()
 	if errors.Is(err, iterator.ErrIteratorDone) {
-		value = types.Null
-
-		if params.Upsert {
-			doc := params.Update
-			if params.HasUpdateOperators {
-				doc = must.NotFail(types.NewDocument())
-				if _, err = common.UpdateDocument("findAndModify", doc, params.Update); err != nil {
-					return nil, err
-				}
-			}
-
-			upserted, _ = doc.Get("_id")
-			if upserted == nil {
-				upserted, err = params.Query.Get("_id")
-				if err != nil {
-					upserted = types.NewObjectID()
-				}
-
-				idDoc, ok := upserted.(*types.Document)
-				if ok {
-					var hasOp bool
-
-					if hasOp, err = common.HasQueryOperator(idDoc); err != nil {
-						return nil, err
-					}
-
-					if hasOp {
-						upserted = types.NewObjectID()
-					}
-				}
-
-				doc.Set("_id", upserted)
-			}
-
-			if err = doc.ValidateData(); err != nil {
-				var we *writeError
-
-				if we, err = handleValidationError(err); err != nil {
-					return nil, err
-				}
-
-				writeErrors.Append(we.Document())
-			}
-
-			if _, err = c.InsertAll(ctx, &backends.InsertAllParams{
-				Docs: []*types.Document{doc},
-			}); err != nil {
-				if backends.ErrorCodeIs(err, backends.ErrorCodeInsertDuplicateID) {
-					we := &writeError{
-						index:  int32(0),
-						code:   commonerrors.ErrDuplicateKeyInsert,
-						errmsg: fmt.Sprintf(`E11000 duplicate key error collection: %s.%s`, params.DB, params.Collection),
-					}
-					writeErrors.Append(we.Document())
-				}
-
-				return nil, lazyerrors.Error(err)
-			}
-
-			if params.ReturnNewDocument {
-				value = doc
-			}
-
-			modified = 1
+		if params.Remove {
+			return &findAndModifyResult{
+				modified: int32(0),
+				value:    types.Null,
+			}, nil
 		}
 
-		if !params.Remove {
-			updateExisting = false
+		if !params.Upsert {
+			return &findAndModifyResult{
+				modified:       int32(0),
+				updateExisting: false,
+				value:          types.Null,
+			}, nil
 		}
 
-		return &findAndModifyResult{
-			modified:       modified,
-			updateExisting: updateExisting,
-			upserted:       upserted,
-			value:          value,
-			writeErrors:    writeErrors,
-		}, nil
-	}
-
-	value = v
-
-	if params.Remove {
-		var delRes *backends.DeleteAllResult
-
-		if delRes, err = c.DeleteAll(ctx, &backends.DeleteAllParams{IDs: []any{must.NotFail(v.Get("_id"))}}); err != nil {
-			return nil, lazyerrors.Error(err)
-		}
-
-		modified = delRes.Deleted
-	} else {
-		// TODO https://github.com/FerretDB/FerretDB/issues/3040
 		doc := params.Update
 		if params.HasUpdateOperators {
-			doc = v.DeepCopy()
+			doc = must.NotFail(types.NewDocument())
 			if _, err = common.UpdateDocument("findAndModify", doc, params.Update); err != nil {
 				return nil, err
 			}
 		}
 
-		id := must.NotFail(v.Get("_id"))
-		updateID, _ := doc.Get("_id")
-		if updateID == nil {
-			doc.Set("_id", id)
-		}
+		upserted, _ := doc.Get("_id")
+		if upserted == nil {
+			upserted, err = params.Query.Get("_id")
+			if err != nil {
+				upserted = types.NewObjectID()
+			}
 
-		if updateID != nil && updateID != id {
-			return nil, commonerrors.NewCommandErrorMsgWithArgument(
-				commonerrors.ErrImmutableField,
-				fmt.Sprintf(
-					`Plan executor error during findAndModify :: caused `+
-						`by :: After applying the update, the (immutable) field `+
-						`'_id' was found to have been altered to _id: "%s"`,
-					updateID,
-				),
-				"findAndModify",
-			)
+			idDoc, ok := upserted.(*types.Document)
+			if ok {
+				var hasOp bool
+
+				if hasOp, err = common.HasQueryOperator(idDoc); err != nil {
+					return nil, err
+				}
+
+				if hasOp {
+					upserted = types.NewObjectID()
+				}
+			}
+
+			doc.Set("_id", upserted)
 		}
 
 		if err = doc.ValidateData(); err != nil {
@@ -286,23 +211,100 @@ func (h *Handler) findAndModifyDocument(ctx context.Context, params *common.Find
 			writeErrors.Append(we.Document())
 		}
 
-		updateRes, err := c.UpdateAll(ctx, &backends.UpdateAllParams{Docs: []*types.Document{doc}})
-		if err != nil {
+		if _, err = c.InsertAll(ctx, &backends.InsertAllParams{
+			Docs: []*types.Document{doc},
+		}); err != nil {
+			if backends.ErrorCodeIs(err, backends.ErrorCodeInsertDuplicateID) {
+				we := &writeError{
+					index:  int32(0),
+					code:   commonerrors.ErrDuplicateKeyInsert,
+					errmsg: fmt.Sprintf(`E11000 duplicate key error collection: %s.%s`, params.DB, params.Collection),
+				}
+				writeErrors.Append(we.Document())
+			}
+
 			return nil, lazyerrors.Error(err)
 		}
 
+		var value any = types.Null
 		if params.ReturnNewDocument {
 			value = doc
 		}
 
-		modified = updateRes.Updated
-		updateExisting = true
+		return &findAndModifyResult{
+			modified:       int32(1),
+			updateExisting: false,
+			upserted:       upserted,
+			value:          value,
+			writeErrors:    writeErrors,
+		}, nil
+	}
+
+	if params.Remove {
+		var delRes *backends.DeleteAllResult
+
+		if delRes, err = c.DeleteAll(ctx, &backends.DeleteAllParams{IDs: []any{must.NotFail(v.Get("_id"))}}); err != nil {
+			return nil, lazyerrors.Error(err)
+		}
+
+		return &findAndModifyResult{
+			modified: delRes.Deleted,
+			value:    v,
+		}, nil
+	}
+
+	// TODO https://github.com/FerretDB/FerretDB/issues/3040
+	doc := params.Update
+	if params.HasUpdateOperators {
+		doc = v.DeepCopy()
+		if _, err = common.UpdateDocument("findAndModify", doc, params.Update); err != nil {
+			return nil, err
+		}
+	}
+
+	id := must.NotFail(v.Get("_id"))
+
+	updateID, _ := doc.Get("_id")
+	if updateID == nil {
+		doc.Set("_id", id)
+	}
+
+	if updateID != nil && updateID != id {
+		return nil, commonerrors.NewCommandErrorMsgWithArgument(
+			commonerrors.ErrImmutableField,
+			fmt.Sprintf(
+				`Plan executor error during findAndModify :: caused `+
+					`by :: After applying the update, the (immutable) field `+
+					`'_id' was found to have been altered to _id: "%s"`,
+				updateID,
+			),
+			"findAndModify",
+		)
+	}
+
+	if err = doc.ValidateData(); err != nil {
+		var we *writeError
+
+		if we, err = handleValidationError(err); err != nil {
+			return nil, err
+		}
+
+		writeErrors.Append(we.Document())
+	}
+
+	updateRes, err := c.UpdateAll(ctx, &backends.UpdateAllParams{Docs: []*types.Document{doc}})
+	if err != nil {
+		return nil, lazyerrors.Error(err)
+	}
+
+	value := v
+	if params.ReturnNewDocument {
+		value = doc
 	}
 
 	return &findAndModifyResult{
-		modified:       modified,
-		updateExisting: updateExisting,
-		upserted:       upserted,
+		modified:       updateRes.Updated,
+		updateExisting: true,
 		value:          value,
 		writeErrors:    writeErrors,
 	}, nil
