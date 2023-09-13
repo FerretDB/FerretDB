ba1cba010e07b9b77b3c2a31fa98436faf6831aa
Elena Grahovac <elena@grahovac.me>
2023-09-13 21:26:34 +0200

    wip

--- integration/update_compat_test.go
+++ integration/update_compat_test.go
@@ -558,14 +558,12 @@ func TestUpdateCompat(t *testing.T) {
 			replace:     bson.D{{"_id", "new"}},
 			replaceOpts: options.Replace().SetUpsert(true),
 			resultType:  emptyResult,
-			//	skipForSQLite: "https://github.com/FerretDB/FerretDB/issues/3183",
 		},
 		"UpdateNonExistentUpsert": {
 			filter:     bson.D{{"_id", "non-existent"}},
 			update:     bson.D{{"$set", bson.D{{"v", int32(42)}}}},
 			updateOpts: options.Update().SetUpsert(true),
 			resultType: emptyResult,
-			// skipForSQLite: "https://github.com/FerretDB/FerretDB/issues/3183",
 		},
 	}
 
