28a98ae49d09fd3453351a5a6ef59b68f13d4b86
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2023-09-13 07:38:07 +0400

    Update internal/backends/postgresql/collection.go
    Co-authored-by: Chi Fujii <chi.fujii@ferretdb.io>

--- internal/backends/postgresql/collection.go
+++ internal/backends/postgresql/collection.go
@@ -75,7 +75,7 @@ func (c *collection) CreateIndexes(ctx context.Context, params *backends.CreateI
 }
 
 // DropIndexes implements backends.Collection interface.
-func (c *collection) DropIndexes(context.Context, *backends.DropIndexesParams) (*backends.DropIndexesResult, error) {
+func (c *collection) DropIndexes(ctx context.Context, params *backends.DropIndexesParams) (*backends.DropIndexesResult, error) {
 	panic("not implemented")
 }
 
