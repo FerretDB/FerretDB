fa40cfdca7e2bba230fad908ae79c75d17c2c1c3
b1ron <goldingbyron@gmail.com>
2023-09-13 11:03:44 +0200

    add script

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -3,5 +3,15 @@
 (function() {
   'use strict';
 
+  const t = db.multi;
+  t.drop();
+
+  t.save({a: []});
+  t.save({a: 1});
+  t.save({a: []});
+
+  assert.writeError(t.update({}, {$push: {a: 2}}, false, true));
+  assert.eq(1, t.count({a: 2}));
+
   print('test.js passed!');
 })();
