882deaa8adf3ef6b6796b256c234962a6e5af038
Chi Fujii <chi.fujii@ferretdb.io>
2023-09-26 17:15:20 +0900

    use transaction from pgx package

--- internal/backends/postgresql/metadata/pool/transaction.go
+++ internal/backends/postgresql/metadata/pool/transaction.go
@@ -20,56 +20,19 @@ import (
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 
-	"github.com/FerretDB/FerretDB/internal/util/lazyerrors"
 	"github.com/FerretDB/FerretDB/internal/util/observability"
 )
 
 // InTransaction uses pool p and wraps the given function f in a transaction.
 //
 // If f returns an error or context is canceled, the transaction is rolled back.
-func InTransaction(ctx context.Context, p *pgxpool.Pool, f func(tx pgx.Tx) error) (err error) {
+func InTransaction(ctx context.Context, p *pgxpool.Pool, f func(tx pgx.Tx) error) error {
 	defer observability.FuncCall(ctx)()
 
-	var pgTx pgx.Tx
-
-	if pgTx, err = p.BeginTx(ctx, pgx.TxOptions{}); err != nil {
-		err = lazyerrors.Error(err)
-		return
+	if err := pgx.BeginFunc(ctx, p, f); err != nil {
+		// do not wrap error because the caller of f depends on it in some cases
+		return err
 	}
 
-	var done bool
-
-	defer func() {
-		// It is not enough to check `err == nil` there,
-		// because in tests `f` could contain testify/require.XXX or `testing.TB.FailNow()` calls
-		// that call `runtime.Goexit()`, leaving `err` unset in `err = f(tx)` below.
-		// This situation would hang a test.
-		//
-		// As a bonus, checking a separate variable also handles any panics in `f`,
-		// including `panic(nil)` that is problematic for tests too.
-		if done {
-			return
-		}
-
-		if err == nil {
-			err = lazyerrors.Errorf("transaction was not committed")
-		}
-
-		// ensure rollback even if context is canceled
-		_ = pgTx.Rollback(context.WithoutCancel(ctx))
-	}()
-
-	if err = f(pgTx); err != nil {
-		// do not wrap f's error because the caller depends on it in some cases
-		return
-	}
-
-	if err = pgTx.Commit(ctx); err != nil {
-		err = lazyerrors.Error(err)
-		return
-	}
-
-	done = true
-
-	return
+	return nil
 }
