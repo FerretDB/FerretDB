e4de4c34e624eadd2e4175714a8e31ba3caa8613
Chi Fujii <chi.fujii@ferretdb.io>
2023-10-10 15:11:15 +0900

    add compat test for testing capped collection in collstats

--- integration/commands_administration_compat_test.go
+++ integration/commands_administration_compat_test.go
@@ -18,13 +18,16 @@ import (
 	"math"
 	"testing"
 
+	"github.com/AlekSi/pointer"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
 
 	"github.com/FerretDB/FerretDB/integration/setup"
 	"github.com/FerretDB/FerretDB/integration/shareddata"
 	"github.com/FerretDB/FerretDB/internal/util/must"
+	"github.com/FerretDB/FerretDB/internal/util/testutil"
 )
 
 func TestCommandsAdministrationCompatCollStatsWithScale(t *testing.T) {
@@ -93,6 +96,68 @@ func TestCommandsAdministrationCompatCollStatsWithScale(t *testing.T) {
 			compatFactor := must.NotFail(compatDoc.Get("scaleFactor"))
 
 			assert.Equal(t, compatFactor, targetFactor)
+		})
+	}
+}
+
+func TestCommandsAdministrationCompatCollStatsCappedCollection(t *testing.T) {
+	t.Skip("https://github.com/FerretDB/FerretDB/issues/3458")
+
+	t.Parallel()
+
+	s := setup.SetupCompatWithOpts(t, &setup.SetupCompatOpts{
+		Providers:                []shareddata.Provider{},
+		AddNonExistentCollection: true,
+	})
+
+	ctx, targetCollection, compatCollection := s.Ctx, s.TargetCollections[0], s.CompatCollections[0]
+
+	for name, tc := range map[string]struct { //nolint:vet // for readability
+		opts *options.CreateCollectionOptions
+	}{
+		"Size": {
+			opts: &options.CreateCollectionOptions{
+				Capped:       pointer.ToBool(true),
+				SizeInBytes:  pointer.ToInt64(1000),
+				MaxDocuments: pointer.ToInt64(10),
+			},
+		},
+		"MaxDocuments": {
+			opts: &options.CreateCollectionOptions{
+				Capped:       pointer.ToBool(true),
+				SizeInBytes:  pointer.ToInt64(1000),
+				MaxDocuments: pointer.ToInt64(10),
+			},
+		},
+	} {
+		name, tc := name, tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			cName := testutil.CollectionName(t) + name
+			targetErr := targetCollection.Database().CreateCollection(ctx, cName, tc.opts)
+			require.NoError(t, targetErr)
+
+			compatErr := compatCollection.Database().CreateCollection(ctx, cName, tc.opts)
+			require.NoError(t, compatErr)
+
+			require.Equal(t, compatCollection.Name(), targetCollection.Name())
+			command := bson.D{{"collStats", targetCollection.Name()}}
+
+			var targetRes bson.D
+			targetErr = targetCollection.Database().RunCommand(ctx, command).Decode(&targetRes)
+			require.NoError(t, targetErr)
+
+			var compatRes bson.D
+			targetErr = compatCollection.Database().RunCommand(ctx, command).Decode(&targetRes)
+			require.NoError(t, targetErr)
+
+			targetDoc := ConvertDocument(t, targetRes)
+			compatDoc := ConvertDocument(t, compatRes)
+
+			assert.Equal(t, must.NotFail(compatDoc.Get("capped")), must.NotFail(targetDoc.Get("capped")))
+			assert.Equal(t, must.NotFail(compatDoc.Get("max")), must.NotFail(targetDoc.Get("max")))
+			assert.Equal(t, must.NotFail(compatDoc.Get("maxSize")), must.NotFail(targetDoc.Get("maxSize")))
 		})
 	}
 }
