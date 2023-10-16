b4446936ab2d53b0c01795a9bf9f8393af5b33b4
Chi Fujii <chi.fujii@ferretdb.io>
2023-10-16 20:21:49 +0900

    Add more data to output of `collStats` and `dbStats` (#3538)
    Closes #2447.
    Closes #3521.

--- integration/commands_administration_compat_test.go
+++ integration/commands_administration_compat_test.go
@@ -21,10 +21,12 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
 
 	"github.com/FerretDB/FerretDB/integration/setup"
 	"github.com/FerretDB/FerretDB/integration/shareddata"
 	"github.com/FerretDB/FerretDB/internal/util/must"
+	"github.com/FerretDB/FerretDB/internal/util/testutil"
 )
 
 func TestCommandsAdministrationCompatCollStatsWithScale(t *testing.T) {
@@ -93,6 +95,73 @@ func TestCommandsAdministrationCompatCollStatsWithScale(t *testing.T) {
 			compatFactor := must.NotFail(compatDoc.Get("scaleFactor"))
 
 			assert.Equal(t, compatFactor, targetFactor)
+		})
+	}
+}
+
+func TestCommandsAdministrationCompatCollStatsCappedCollection(t *testing.T) {
+	t.Skip("https://github.com/FerretDB/FerretDB/issues/2447")
+
+	t.Parallel()
+
+	s := setup.SetupCompatWithOpts(t, &setup.SetupCompatOpts{
+		Providers:                []shareddata.Provider{},
+		AddNonExistentCollection: true,
+	})
+
+	ctx, targetCollection, compatCollection := s.Ctx, s.TargetCollections[0], s.CompatCollections[0]
+
+	for name, tc := range map[string]struct { //nolint:vet // for readability
+		sizeInBytes  int64 // also sets capped true if it is greater than zero
+		maxDocuments int64 // maxDocuments is set if sizeInBytes is greater than zero
+	}{
+		"Size": {
+			sizeInBytes: 1000,
+		},
+		"MaxDocuments": {
+			sizeInBytes:  1000,
+			maxDocuments: 10,
+		},
+	} {
+		name, tc := name, tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			cName := testutil.CollectionName(t) + name
+			opts := options.CreateCollection()
+
+			if tc.sizeInBytes > 0 {
+				opts.SetCapped(true)
+				opts.SetSizeInBytes(tc.sizeInBytes)
+
+				if tc.maxDocuments > 0 {
+					opts.SetMaxDocuments(tc.maxDocuments)
+				}
+			}
+
+			targetErr := targetCollection.Database().CreateCollection(ctx, cName, opts)
+			require.NoError(t, targetErr)
+
+			compatErr := compatCollection.Database().CreateCollection(ctx, cName, opts)
+			require.NoError(t, compatErr)
+
+			require.Equal(t, compatCollection.Name(), targetCollection.Name())
+			command := bson.D{{"collStats", targetCollection.Name()}}
+
+			var targetRes bson.D
+			targetErr = targetCollection.Database().RunCommand(ctx, command).Decode(&targetRes)
+			require.NoError(t, targetErr)
+
+			var compatRes bson.D
+			targetErr = compatCollection.Database().RunCommand(ctx, command).Decode(&targetRes)
+			require.NoError(t, targetErr)
+
+			targetDoc := ConvertDocument(t, targetRes)
+			compatDoc := ConvertDocument(t, compatRes)
+
+			assert.Equal(t, must.NotFail(compatDoc.Get("capped")), must.NotFail(targetDoc.Get("capped")))
+			assert.Equal(t, must.NotFail(compatDoc.Get("max")), must.NotFail(targetDoc.Get("max")))
+			assert.Equal(t, must.NotFail(compatDoc.Get("maxSize")), must.NotFail(targetDoc.Get("maxSize")))
 		})
 	}
 }
