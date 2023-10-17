3ee741a1cae6f7e5ec8081b28d774fe9c1789ef8
Alexey Palazhchenko <alexey.palazhchenko@ferretdb.io>
2023-10-17 15:17:08 +0400

    Add filter pushdown for `_id: <string>` for SQLite

--- integration/query_comparison_compat_test.go
+++ integration/query_comparison_compat_test.go
@@ -263,7 +263,7 @@ func TestQueryComparisonCompatImplicit(t *testing.T) {
 		},
 		"IDString": {
 			filter:         bson.D{{"_id", "string"}},
-			resultPushdown: pgPushdown,
+			resultPushdown: allPushdown,
 		},
 		"IDObjectID": {
 			filter:         bson.D{{"_id", primitive.NilObjectID}},
