29402967fcde1e461c0332055726e3af303ef0d7
Alexey Palazhchenko <alexey.palazhchenko@ferretdb.io>
2023-10-17 11:23:25 +0400

    Remove old TODOs (#3595)

--- internal/handlers/common/filter.go
+++ internal/handlers/common/filter.go
@@ -1484,7 +1484,6 @@ func filterFieldValueByTypeCode(fieldValue any, code commonparams.TypeCode) (boo
 
 // filterFieldExprElemMatch handles {field: {$elemMatch: value}}.
 // Returns false if doc value is not an array.
-// TODO https://github.com/FerretDB/FerretDB/issues/364
 func filterFieldExprElemMatch(doc *types.Document, filterKey, filterSuffix string, exprValue any) (bool, error) {
 	expr, ok := exprValue.(*types.Document)
 	if !ok {
