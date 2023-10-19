20f104c1d862808a22c4947451a7b60503585d47
b1ron <goldingbyron@gmail.com>
2023-10-19 15:25:12 +0200

    add script

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -3,5 +3,22 @@
 (function() {
   'use strict';
 
+  // test uniqueness of _id
+  const t = db.foo;
+  t.drop();
+
+  let res;
+
+  res = t.save({_id: 3}); // A
+
+  assert.commandWorked(res);
+
+  res = t.insert({_id: 4, x: 99}); // B
+  assert.commandWorked(res);
+
+  // this should yield an error but we end up modifying A
+  res = t.update({_id: 4}, {_id: 3, x: 99});
+  assert.writeError(res);
+
   print('test.js passed!');
 })();
