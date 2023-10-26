79d6bfc12955a364ec38f791a8a998abe3fa57e5
Aditya <aditya.salunkh919@gmail.com>
2023-10-27 00:31:45 +0530

    added 4 graphs for prometheus metrics

--- internal/util/debug/debug.go
+++ internal/util/debug/debug.go
@@ -20,7 +20,6 @@ import (
 	"context"
 	_ "expvar" // for metrics
 	"log"
-	"math"
 	"math/rand"
 	"net"
 	"net/http"
@@ -51,9 +50,10 @@ func RunHandler(ctx context.Context, addr string, r prometheus.Registerer, l *za
 
 	opts := []statsviz.Option{
 		statsviz.Root("/debug/graphs"),
-		statsviz.TimeseriesPlot(scatterPlot()),
-		statsviz.TimeseriesPlot(barPlot()),
-		statsviz.TimeseriesPlot(stackedPlot()),
+		statsviz.TimeseriesPlot(ferretdb_postgresql_metadata_databases_barPlot()),
+		statsviz.TimeseriesPlot(ferretdb_postgresql_pool_size_barPlot()),
+		statsviz.TimeseriesPlot(promhttp_metric_handler_requests_total_stackPlot()),
+		statsviz.TimeseriesPlot(promhttp_metric_handler_requests_in_flight_scatterPlot()),
 		// TODO https://github.com/FerretDB/FerretDB/issues/3600
 	}
 	must.NoError(statsviz.Register(http.DefaultServeMux, opts...))
@@ -117,90 +117,122 @@ func RunHandler(ctx context.Context, addr string, r prometheus.Registerer, l *za
 	l.Sugar().Info("Debug server stopped.")
 }
 
-func scatterPlot() statsviz.TimeSeriesPlot {
-	// Describe the 'sine' time series.
-	sine := statsviz.TimeSeries{
-		Name:     "short sin",
+func promhttp_metric_handler_requests_in_flight_scatterPlot() statsviz.TimeSeriesPlot {
+	scrapes := statsviz.TimeSeries{
+		Name:     "prometheus http scrape",
 		Unitfmt:  "%{y:.4s}B",
-		GetValue: updateSine,
+		GetValue: updateScrapes,
 	}
 
-	// Build a new plot, showing our sine time series
 	plot, err := statsviz.TimeSeriesPlotConfig{
-		Name:  "sine",
-		Title: "Sine",
-		Type:  statsviz.Scatter,
-		InfoText: `This is an example of a 'scatter' type plot, showing a single time series.<br>
-InfoText field (this) accepts any HTML tags like <b>bold</b>, <i>italic</i>, etc.`,
-		YAxisTitle: "y unit",
-		Series:     []statsviz.TimeSeries{sine},
+		Name:       "scrapes",
+		Title:      "prometheus http request Count",
+		Type:       statsviz.Scatter,
+		InfoText:   "Helps Visualizing Current number of scrapes being served.",
+		YAxisTitle: "Scrapes",
+		Series:     []statsviz.TimeSeries{scrapes},
 	}.Build()
 	if err != nil {
 		log.Fatalf("failed to build timeseries plot: %v", err)
 	}
-
 	return plot
 }
 
-func barPlot() statsviz.TimeSeriesPlot {
-	// Describe the 'user logins' time series.
-	logins := statsviz.TimeSeries{
-		Name:     "user logins",
+func ferretdb_postgresql_metadata_databases_barPlot() statsviz.TimeSeriesPlot {
+	databaseCount := statsviz.TimeSeries{
+		Name:     "Database count",
 		Unitfmt:  "%{y:.4s}",
-		GetValue: logins,
+		GetValue: dbCount,
 	}
-
-	// Describe the 'user signins' time series.
-	signins := statsviz.TimeSeries{
-		Name:     "user signins",
-		Unitfmt:  "%{y:.4s}",
-		GetValue: signins,
-	}
-
-	// Build a new plot, showing both time series at once.
 	plot, err := statsviz.TimeSeriesPlotConfig{
-		Name:  "users",
-		Title: "Users",
-		Type:  statsviz.Bar,
-		InfoText: `This is an example of a 'bar' type plot, showing 2 time series.<br>
-InfoText field (this) accepts any HTML tags like <b>bold</b>, <i>italic</i>, etc.`,
-		YAxisTitle: "users",
-		Series:     []statsviz.TimeSeries{logins, signins},
+		Name:       "dbCount",
+		Title:      "Postgresql MetaData Database Count",
+		Type:       statsviz.Bar,
+		YAxisTitle: "Database Count",
+		Series:     []statsviz.TimeSeries{databaseCount},
 	}.Build()
 	if err != nil {
 		log.Fatalf("failed to build timeseries plot: %v", err)
 	}
-
 	return plot
 }
 
-func stackedPlot() statsviz.TimeSeriesPlot {
-	// Describe the 'user logins' time series.
-	logins := statsviz.TimeSeries{
-		Name:     "user logins",
+func ferretdb_postgresql_pool_size_barPlot() statsviz.TimeSeriesPlot {
+	poolSize := statsviz.TimeSeries{
+		Name:     "Postgresql Pool size",
 		Unitfmt:  "%{y:.4s}",
+		GetValue: poolSize,
+	}
+	plot, err := statsviz.TimeSeriesPlotConfig{
+		Name:       "poolSize",
+		Title:      "postgresql pool size",
+		Type:       statsviz.Bar,
+		YAxisTitle: "Pool Size",
+		Series:     []statsviz.TimeSeries{poolSize},
+	}.Build()
+	if err != nil {
+		log.Fatalf("failed to build timeseries plot: %v", err)
+	}
+	return plot
+}
+
+func promhttp_metric_handler_requests_total_stackPlot() statsviz.TimeSeriesPlot {
+	code200 := statsviz.TimeSeries{
+		Name:     "Code 200",
+		Unitfmt:  "%{y:.4s}B",
 		Type:     statsviz.Bar,
-		GetValue: logins,
+		GetValue: codeCountGen,
 	}
 
-	// Describe the 'user signins' time series.
-	signins := statsviz.TimeSeries{
-		Name:     "user signins",
-		Unitfmt:  "%{y:.4s}",
+	code503 := statsviz.TimeSeries{
+		Name:     "Code 503",
+		Unitfmt:  "%{y:.4s}B",
 		Type:     statsviz.Bar,
-		GetValue: signins,
+		GetValue: codeCountGen,
 	}
 
-	// Build a new plot, showing both time series at once.
+	code500 := statsviz.TimeSeries{
+		Name:     "Code 500",
+		Unitfmt:  "%{y:.4s}B",
+		Type:     statsviz.Bar,
+		GetValue: codeCountGen,
+	}
+
+	code201 := statsviz.TimeSeries{
+		Name:     "Code 201",
+		Unitfmt:  "%{y:.4s}B",
+		Type:     statsviz.Bar,
+		GetValue: codeCountGen,
+	}
+
+	code202 := statsviz.TimeSeries{
+		Name:     "Code 202",
+		Unitfmt:  "%{y:.4s}B",
+		Type:     statsviz.Bar,
+		GetValue: codeCountGen,
+	}
+
+	code203 := statsviz.TimeSeries{
+		Name:     "Code 203",
+		Unitfmt:  "%{y:.4s}B",
+		Type:     statsviz.Bar,
+		GetValue: codeCountGen,
+	}
+
+	code204 := statsviz.TimeSeries{
+		Name:     "Code 204",
+		Unitfmt:  "%{y:.4s}B",
+		Type:     statsviz.Bar,
+		GetValue: codeCountGen,
+	}
+
 	plot, err := statsviz.TimeSeriesPlotConfig{
-		Name:    "users-stack",
-		Title:   "Stacked Users",
-		Type:    statsviz.Bar,
-		BarMode: statsviz.Stack,
-		InfoText: `This is an example of a 'bar' plot showing 2 time series stacked on top of each other with <b>BarMode:Stack</b>.<br>
-InfoText field (this) accepts any HTML tags like <b>bold</b>, <i>italic</i>, etc.`,
-		YAxisTitle: "users",
-		Series:     []statsviz.TimeSeries{logins, signins},
+		Name:       "prometheus metric handler request count",
+		Title:      "Prometheus metric Count(Status Code)",
+		Type:       statsviz.Bar,
+		BarMode:    statsviz.Stack,
+		YAxisTitle: "status codes",
+		Series:     []statsviz.TimeSeries{code200, code201, code202, code203, code204, code500, code503},
 	}.Build()
 	if err != nil {
 		log.Fatalf("failed to build timeseries plot: %v", err)
@@ -209,17 +241,19 @@ InfoText field (this) accepts any HTML tags like <b>bold</b>, <i>italic</i>, etc
 	return plot
 }
 
-var val = 0.
-
-func updateSine() float64 {
-	val += 0.5
-	return math.Sin(val)
+// dummy functions for simulating prometheus metrics, spot refactoring TBD with actual expanded metrics.
+func updateScrapes() float64 {
+	return rand.Float64() * 100
 }
 
-func logins() float64 {
-	return (rand.Float64() + 2) * 1000
+func dbCount() float64 {
+	return rand.Float64() * 10
 }
 
-func signins() float64 {
-	return (rand.Float64() + 1.5) * 100
+func poolSize() float64 {
+	return rand.Float64()
+}
+
+func codeCountGen() float64 {
+	return rand.Float64()
 }
