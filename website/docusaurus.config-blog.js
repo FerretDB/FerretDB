184ec0406517c02267a52f4f135bf2cb7f844121
Alexander Tobi Fashakin <alexander.fashakin@ferretdb.io>
2023-11-02 10:17:11 +0100

    Add blog

--- website/docusaurus.config-blog.js
+++ website/docusaurus.config-blog.js
@@ -25,7 +25,7 @@ const config = {
 
   scripts: [
     {src: 'https://plausible.io/js/script.js', defer: true, "data-domain": "blog.ferretdb.io"},
-    {src: "/codapi/snippet.js", async: false }
+    {src: "/codapi/snippet.js", defer: true }
   ],
 
   stylesheets: [
