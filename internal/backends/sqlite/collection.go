541ec44f1ad1cfb32b3ca605a57efed442a1b429
noisersup <patryk@kwiatek.xyz>
2023-11-08 20:58:19 +0100

    wip

--- internal/backends/sqlite/collection.go
+++ internal/backends/sqlite/collection.go
@@ -439,32 +439,23 @@ func (c *collection) Compact(ctx context.Context, params *backends.CompactParams
 
 // Capped implements backends.Collection interface.
 func (c *collection) Capped(ctx context.Context) (bool, error) {
-	return false, nil
-	//db, err := c.r.DatabaseGetExisting(ctx, c.dbName)
-	//if err != nil {
-	//	return false, lazyerrors.Error(err)
-	//}
+	db := c.r.DatabaseGetExisting(ctx, c.dbName)
+	if db == nil {
+		return false, backends.NewError(
+			backends.ErrorCodeCollectionDoesNotExist,
+			lazyerrors.Errorf("no ns %s.%s", c.dbName, c.name),
+		)
+	}
 
-	//if db == nil {
-	//	return false, backends.NewError(
-	//		backends.ErrorCodeCollectionDoesNotExist,
-	//		lazyerrors.Errorf("no ns %s.%s", c.dbName, c.name),
-	//	)
-	//}
+	coll := c.r.CollectionGet(ctx, c.dbName, c.name)
+	if coll == nil {
+		return false, backends.NewError(
+			backends.ErrorCodeCollectionDoesNotExist,
+			lazyerrors.Errorf("no ns %s.%s", c.dbName, c.name),
+		)
+	}
 
-	//coll, err := c.r.CollectionGet(ctx, c.dbName, c.name)
-	//if err != nil {
-	//	return false, lazyerrors.Error(err)
-	//}
-
-	//if coll == nil {
-	//	return false, backends.NewError(
-	//		backends.ErrorCodeCollectionDoesNotExist,
-	//		lazyerrors.Errorf("no ns %s.%s", c.dbName, c.name),
-	//	)
-	//}
-
-	//return coll.Capped(), nil
+	return coll.Capped(), nil
 }
 
 // ListIndexes implements backends.Collection interface.
