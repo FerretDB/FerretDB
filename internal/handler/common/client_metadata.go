af3619770e01e9e45884404489420bda94a4d9a2
Alexey Palazhchenko <alexey.palazhchenko@ferretdb.io>
2023-11-27 16:12:32 +0400

    Clean-up old code for multiple handlers

--- internal/handler/common/client_metadata.go
+++ internal/handler/common/client_metadata.go
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package common provides common code for all handlers.
 package common
 
 import (
