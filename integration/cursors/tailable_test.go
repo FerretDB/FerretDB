8e39756dd6463698d12baf944376587f1f105366
Alexey Palazhchenko <alexey.palazhchenko@ferretdb.io>
2023-12-05 18:10:36 +0400

    Move tests for tailbable cursors

---
+++ integration/cursors/tailable_test.go
@@ -0,0 +1,62 @@
+// Copyright 2021 FerretDB Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cursors
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"github.com/FerretDB/FerretDB/integration"
+	"github.com/FerretDB/FerretDB/integration/setup"
+	"github.com/FerretDB/FerretDB/integration/shareddata"
+)
+
+func TestTailable(t *testing.T) {
+	t.Parallel()
+
+	t.Run("NonCapped", func(t *testing.T) {
+		t.Parallel()
+
+		ctx, collection := setup.Setup(t, shareddata.Scalars)
+
+		cursor, err := collection.Find(ctx, bson.D{}, options.Find().SetCursorType(options.Tailable))
+		expected := mongo.CommandError{
+			Code: 2,
+			Name: "BadValue",
+			Message: "error processing query: " +
+				"ns=TestQueryTailable-NonCapped.TestQueryTailable-NonCappedTree: " +
+				"$and\nSort: {}\nProj: {}\n " +
+				"tailable cursor requested on non capped collection",
+		}
+		integration.AssertEqualAltCommandError(t, expected, "tailable cursor requested on non capped collection", err)
+		assert.Nil(t, cursor)
+
+		cursor, err = collection.Find(ctx, bson.D{}, options.Find().SetCursorType(options.TailableAwait))
+		expected = mongo.CommandError{
+			Code: 2,
+			Name: "BadValue",
+			Message: "error processing query: " +
+				"ns=TestQueryTailable-NonCapped.TestQueryTailable-NonCappedTree: " +
+				"$and\nSort: {}\nProj: {}\n " +
+				"tailable cursor requested on non capped collection",
+		}
+		integration.AssertEqualAltCommandError(t, expected, "tailable cursor requested on non capped collection", err)
+		assert.Nil(t, cursor)
+	})
+}
