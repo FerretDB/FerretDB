a0890418eda006a34b900696736e93a3af7e9a35
Alexey Palazhchenko <alexey.palazhchenko@ferretdb.io>
2023-12-05 20:03:34 +0400

    Fix test

--- integration/cursors/tailable_test.go
+++ integration/cursors/tailable_test.go
@@ -35,28 +35,17 @@ func TestTailable(t *testing.T) {
 
 		ctx, collection := setup.Setup(t, shareddata.Scalars)
 
-		cursor, err := collection.Find(ctx, bson.D{}, options.Find().SetCursorType(options.Tailable))
-		expected := mongo.CommandError{
-			Code: 2,
-			Name: "BadValue",
-			Message: "error processing query: " +
-				"ns=TestQueryTailable-NonCapped.TestQueryTailable-NonCappedTree: " +
-				"$and\nSort: {}\nProj: {}\n " +
-				"tailable cursor requested on non capped collection",
+		for _, ct := range []options.CursorType{options.Tailable, options.TailableAwait} {
+			cursor, err := collection.Find(ctx, bson.D{}, options.Find().SetCursorType(ct))
+			expected := mongo.CommandError{
+				Code: 2,
+				Name: "BadValue",
+				Message: "error processing query: " +
+					"ns=TestTailable-NonCapped.TestTailable-NonCappedTree: $and\nSort: {}\nProj: {}\n " +
+					"tailable cursor requested on non capped collection",
+			}
+			integration.AssertEqualAltCommandError(t, expected, "tailable cursor requested on non capped collection", err)
+			assert.Nil(t, cursor)
 		}
-		integration.AssertEqualAltCommandError(t, expected, "tailable cursor requested on non capped collection", err)
-		assert.Nil(t, cursor)
-
-		cursor, err = collection.Find(ctx, bson.D{}, options.Find().SetCursorType(options.TailableAwait))
-		expected = mongo.CommandError{
-			Code: 2,
-			Name: "BadValue",
-			Message: "error processing query: " +
-				"ns=TestQueryTailable-NonCapped.TestQueryTailable-NonCappedTree: " +
-				"$and\nSort: {}\nProj: {}\n " +
-				"tailable cursor requested on non capped collection",
-		}
-		integration.AssertEqualAltCommandError(t, expected, "tailable cursor requested on non capped collection", err)
-		assert.Nil(t, cursor)
 	})
 }
