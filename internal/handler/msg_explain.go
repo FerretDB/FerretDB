13d2680d718c074109d91611bbc46bc6d5181701
Patryk Kwiatek <patryk@kwiatek.xyz>
2023-12-07 19:28:19 +0100

    Allow sorting by `$natural` (#3822)
    Closes #3638.

--- internal/handler/msg_explain.go
+++ internal/handler/msg_explain.go
@@ -120,14 +120,26 @@ func (h *Handler) MsgExplain(ctx context.Context, msg *wire.OpMsg) (*wire.OpMsg,
 		cInfo = cList.Collections[i]
 	}
 
-	capped := cInfo.Capped()
-
 	switch {
 	case h.DisablePushdown:
 		// Pushdown disabled
-	case params.Sort.Len() == 0 && capped:
+	case params.Sort.Len() == 0 && cInfo.Capped():
 		// Pushdown default recordID sorting for capped collections
 		qp.Sort = must.NotFail(types.NewDocument("$natural", int64(1)))
+	case params.Sort.Len() == 1:
+		if params.Sort.Keys()[0] != "$natural" {
+			break
+		}
+
+		if !cInfo.Capped() {
+			return nil, handlererrors.NewCommandErrorMsgWithArgument(
+				handlererrors.ErrNotImplemented,
+				"$natural sort for non-capped collection is not supported.",
+				"explain",
+			)
+		}
+
+		qp.Sort = params.Sort
 	}
 
 	// Limit pushdown is not applied if:
