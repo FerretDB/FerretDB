02ba70ba911384dffedabbe62b95f184c0e9480e
noisersup <patryk@kwiatek.xyz>
2023-12-12 16:13:02 +0100

    wip

--- integration/cursors/nsakey_test.go
+++ integration/cursors/nsakey_test.go
@@ -7,8 +7,10 @@ import (
 	"github.com/FerretDB/FerretDB/integration/setup"
 	"github.com/FerretDB/FerretDB/integration/shareddata"
 	"github.com/FerretDB/FerretDB/internal/types"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 //func TestSomethingElse(t *testing.T) {
@@ -36,10 +38,21 @@ func TestSomething(t *testing.T) {
 
 	coll, ctx := s.Collection, s.Ctx
 
+	sort := bson.D{{"v", 1}}
+
+	opts := options.Find().SetSort(sort)
+	cur, err := coll.Find(ctx, bson.D{{}}, opts)
+	require.NoError(t, err)
+
+	var expectedRes []bson.D
+	require.NoError(t, cur.All(ctx, &expectedRes))
+
+	expectedDocs := integration.ConvertDocuments(t, expectedRes)
+
 	var res bson.D
-	err := coll.Database().RunCommand(ctx, bson.D{
+	err = coll.Database().RunCommand(ctx, bson.D{
 		{"find", coll.Name()},
-		{"sort", bson.D{{"v", 1}}},
+		{"sort", sort},
 		{"batchSize", 1},
 	}).Decode(&res)
 
@@ -57,6 +70,34 @@ func TestSomething(t *testing.T) {
 	require.NotNil(t, cursorID)
 
 	v, _ = cursor.Get("firstBatch")
-	require.NotNil(t, v)
+	assert.Equal(t, 1, v.(*types.Array).Len())
 
+	actualdoc, _ := v.(*types.Array).Get(0)
+
+	require.Equal(t, expectedDocs[0], actualdoc)
+
+	for i := 1; i < len(expectedDocs); i++ {
+		var getMoreRes bson.D
+		err = coll.Database().RunCommand(ctx, bson.D{
+			{"getMore", cursorID},
+			{"collection", coll.Name()},
+			{"batchSize", 1},
+		}).Decode(&getMoreRes)
+		require.NoError(t, err)
+
+		getMoreDoc := integration.ConvertDocument(t, res)
+
+		path, err := types.NewPathFromString("cursor.firstBatch")
+		require.NoError(t, err)
+
+		getMore, err := getMoreDoc.GetByPath(path)
+		require.NoError(t, err)
+
+		resDocs := getMore.(*types.Array)
+		require.Equal(t, 1, resDocs.Len())
+
+		doc, _ := resDocs.Get(0)
+
+		assert.Equal(t, expectedDocs[i], doc, res)
+	}
 }
