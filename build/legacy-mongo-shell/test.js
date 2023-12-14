ab8e2968e1face9e616453faf4d1f01491c7c21c
Elena Grahovac <elena@grahovac.me>
2023-12-14 17:34:55 +0100

    wip

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -3,5 +3,11 @@
 (function() {
   'use strict';
 
-  print('test.js passed!');
+  const coll = db.test;
+
+  coll.drop();
+
+  coll.createIndexes([ { foo: 1 }, { bar: 1 }] )
+
+  coll.createIndexes([ { foo: 1 }, {var: 1}, { bar: 1 }] ) // FerretDB panics here
 })();
