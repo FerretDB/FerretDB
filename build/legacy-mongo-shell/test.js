9d9dcdbf4e5c1463e92aa002184d8edf2dffc071
Wazir Ahmed <wazirahmedf@gmail.com>
2023-12-14 15:19:50 +0000

    Add test to validate upsert behaviour with filter fields
    Signed-off-by: Wazir Ahmed <wazirahmedf@gmail.com>

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -3,5 +3,25 @@
 (function() {
   'use strict';
 
+  const coll = db.test;
+
+  coll.drop();
+
+  const filter = {"_id": 1, "f1.x": {$eq: 2}, "f1.y": {$ne: 3}}
+  const update = {$set: {f2: 4}}
+  const opts   = {upsert: true}
+
+  coll.updateOne(filter, update, opts);
+
+  // With `upsert: true`, filter fields without any operator (or)
+  // fields with `$eq` operator should be added to the final document
+  const expected = [
+    {_id: 1, f1: {x: 2}, f2: 4},
+  ];
+
+  const actual = coll.find().toArray();
+
+  assert.eq(expected, actual);
+
   print('test.js passed!');
 })();
