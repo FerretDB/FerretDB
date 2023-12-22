b83166f2f749115c58c79992a59be7b0b30e922b
b1ron <goldingbyron@gmail.com>
2023-12-23 00:26:25 +0100

    more

--- internal/handler/msg_saslcontinue.go
+++ internal/handler/msg_saslcontinue.go
@@ -16,10 +16,67 @@ package handler
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/FerretDB/FerretDB/internal/handler/common"
+	"github.com/FerretDB/FerretDB/internal/handler/handlererrors"
+	"github.com/FerretDB/FerretDB/internal/util/lazyerrors"
 	"github.com/FerretDB/FerretDB/internal/wire"
+	"go.uber.org/zap"
 )
 
+// MsgSASLContinue implements `saslContinue` command.
 func (h *Handler) MsgSASLContinue(ctx context.Context, msg *wire.OpMsg) (*wire.OpMsg, error) {
+	document, err := msg.Document()
+	if err != nil {
+		return nil, lazyerrors.Error(err)
+	}
+
+	dbName, err := common.GetRequiredParam[string](document, "$db")
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO https://github.com/FerretDB/FerretDB/issues/3008
+
+	// database name typically is either "$external" or "admin"
+	// we can't use it to query the database
+	_ = dbName
+
+	mechanism, err := common.GetRequiredParam[string](document, "mechanism")
+	if err != nil {
+		return nil, lazyerrors.Error(err)
+	}
+
+	var response string
+
+	switch {
+	case mechanism == "SCRAM-SHA-256":
+		// TODO SCRAM negotiation
+		response, err = saslStartSCRAM(document)
+		if err != nil {
+			return nil, err
+		}
+
+	// to reduce connection overhead time, clients may use a hello command to complete their authentication exchange
+	// if so, the saslStart command may be embedded under the speculativeAuthenticate field
+	case document.Has("speculativeAuthenticate"):
+		// TODO SCRAM negotiation
+		response, err = saslStartSCRAM(document)
+		if err != nil {
+			return nil, err
+		}
+
+	default:
+		msg := fmt.Sprintf("Unsupported authentication mechanism %q.\n", mechanism) +
+			"See https://docs.ferretdb.io/security/authentication/ for more details."
+		return nil, handlererrors.NewCommandErrorMsgWithArgument(handlererrors.ErrAuthenticationFailed, msg, "mechanism")
+	}
+
+	// {"payload": "n,,n=username,r=xv/n+51PvakMHhHjIa8va/hXQnzZ/n3W"}
+	h.L.Debug(
+		"SCRAM", zap.String("response", response),
+	)
+
 	return nil, nil
 }
