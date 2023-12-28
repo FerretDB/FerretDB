5a9273a58423032eac3dbaf2d50eedc95e09065f
noisersup <patryk@kwiatek.xyz>
2023-12-28 15:25:57 +0100

    wip

--- internal/handler/msg_getmore.go
+++ internal/handler/msg_getmore.go
@@ -229,7 +229,11 @@ func (h *Handler) MsgGetMore(ctx context.Context, msg *wire.OpMsg) (*wire.OpMsg,
 
 	case cursor.TailableAwait:
 		if nextBatch.Len() == 0 {
-			nextBatch, err = h.awaitData(ctx, c, maxTimeMS, batchSize)
+			nextBatch, err = h.awaitData(ctx, &awaitDataParmas{
+				cursor:    c,
+				batchSize: batchSize,
+				maxTimeMS: maxTimeMS,
+			})
 			if err != nil {
 				return nil, lazyerrors.Error(err)
 			}
@@ -274,18 +278,22 @@ func (h *Handler) makeNextBatch(c *cursor.Cursor, batchSize int64) (*types.Array
 	return nextBatch, nil
 }
 
+// awaitDataParmas contains parameters that can be passed to awaitData function.
+type awaitDataParmas struct {
+	cursor    *cursor.Cursor
+	maxTimeMS int64
+	batchSize int64
+}
+
 // awaitData stops the goroutine, and waits for a new data for the cursor.
 // If there's a new document, or the maxTimeMS have passed it returns the nextBatch.
-func (h *Handler) awaitData(
-	ctx context.Context,
-	c *cursor.Cursor,
-	maxTimeMS, batchSize int64,
-) (resBatch *types.Array, err error) {
+func (h *Handler) awaitData(ctx context.Context, params *awaitDataParmas) (resBatch *types.Array, err error) {
+	c := params.cursor
 	data := c.Data.(*findCursorData)
 
 	closer := iterator.NewMultiCloser()
 
-	sleepDur := time.Duration(maxTimeMS) * time.Millisecond
+	sleepDur := time.Duration(params.maxTimeMS) * time.Millisecond
 	ctx, cancel := context.WithTimeout(ctx, sleepDur)
 
 	defer func() {
@@ -328,12 +336,12 @@ func (h *Handler) awaitData(
 			return
 		}
 
-		resBatch, err = h.makeNextBatch(c, batchSize)
+		resBatch, err = h.makeNextBatch(c, params.batchSize)
 		if err != nil {
 			return
 		}
 
-		if maxTimeMS > 10 {
+		if params.maxTimeMS > 10 {
 			time.Sleep(10 * time.Millisecond)
 		}
 	}
