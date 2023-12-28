7e62eda4a295ad59fec67084b98cb65e86d11a42
Elena Grahovac <elena@grahovac.me>
2023-12-28 10:18:30 +0100

    wip

--- integration/oplog/update_oplog_test.go
+++ integration/oplog/update_oplog_test.go
@@ -34,15 +34,17 @@ func TestOplogUpdate(t *testing.T) {
 
 	for name, tc := range map[string]struct {
 		update         bson.D
+		filter         bson.D
 		expectedDiffV1 *types.Document
 		expectedDiffV2 *types.Document
 	}{
 		"set": {
-			update:         bson.D{{"$set", bson.D{{"a", 1}}}},
-			expectedDiffV1: must.NotFail(types.NewDocument("$set", must.NotFail(types.NewDocument("a", int64(1))))),
-			expectedDiffV2: must.NotFail(types.NewDocument("i", must.NotFail(types.NewDocument("a", int64(1))))),
+			update:         bson.D{{"$set", bson.D{{"a", int32(1)}}}},
+			filter:         bson.D{{"_id", "array"}},
+			expectedDiffV1: must.NotFail(types.NewDocument("a", int32(1))),
+			expectedDiffV2: must.NotFail(types.NewDocument("i", must.NotFail(types.NewDocument("a", int32(1))))),
 		},
-		"unset": {
+		/*"unset": {
 			update:         bson.D{{"$unset", bson.D{{"a", 1}}}},
 			expectedDiffV1: must.NotFail(types.NewDocument("$unset", must.NotFail(types.NewDocument("a", int64(1))))),
 			expectedDiffV2: must.NotFail(types.NewDocument("d", must.NotFail(types.NewDocument("a", int64(1))))),
@@ -61,13 +63,13 @@ func TestOplogUpdate(t *testing.T) {
 		},
 		"push": {
 			update: bson.D{{"$push", bson.D{{"a", 1}}}},
-		},
+		},*/
 	} {
 		name, tc := name, tc
 
 		t.Run(name, func(t *testing.T) {
 			t.Run("UpdateOne", func(t *testing.T) {
-				_, err = coll.UpdateOne(ctx, bson.D{}, tc.update)
+				_, err = coll.UpdateOne(ctx, tc.filter, tc.update)
 				require.NoError(t, err)
 
 				var lastOplogEntry bson.D
@@ -76,29 +78,45 @@ func TestOplogUpdate(t *testing.T) {
 
 				actual := integration.ConvertDocument(t, lastOplogEntry)
 
-				version := must.NotFail(actual.Get("v")).(int64)
+				o := must.NotFail(actual.Get("o")).(*types.Document)
+				version := must.NotFail(o.Get("$v")).(int32)
 				if version == 2 {
-					diff := must.NotFail(must.NotFail(actual.Get("o")).(*types.Document).Get("diff")).(*types.Document)
+					diff := must.NotFail(o.Get("diff")).(*types.Document)
 					assert.Equal(t, tc.expectedDiffV2, diff)
 				} else {
-					diff := must.NotFail(actual.Get("o")).(*types.Document)
+					diff := must.NotFail(o.Get("$set")).(*types.Document)
 					assert.Equal(t, tc.expectedDiffV1, diff)
+
+					o2 := must.NotFail(o.Get("o2")).(*types.Document)
+					expectedO2 := must.NotFail(types.NewDocument("_id", must.NotFail(o2.Get("_id"))))
+					assert.Equal(t, expectedO2, o2)
 				}
+
+				/*	o := must.NotFail(actual.Get("o")).(*types.Document)
+					if o.Has("diff") {
+						diff := must.NotFail(o.Get("diff")).(*types.Document)
+						assert.Equal(t, tc.expectedDiffV2, diff)
+					} else {
+						diff := must.NotFail(o.Get("$set")).(*types.Document)
+						assert.Equal(t, tc.expectedDiffV1, diff)
+
+						o2 := must.NotFail(o.Get("o2")).(*types.Document)
+						expectedO2 := must.NotFail(types.NewDocument("_id", must.NotFail(o2.Get("_id"))))
+						assert.Equal(t, expectedO2, o2)
+					}*/
 
 				unsetUnusedOplogFields(actual)
 				actual.Remove("o")
+				actual.Remove("o2")
 				expected, err := types.NewDocument(
-					"op", "d",
+					"op", "u",
 					"ns", ns,
 					"ts", must.NotFail(actual.Get("ts")).(types.Timestamp),
-					"v", version,
+					"v", int64(2),
 				)
 				require.NoError(t, err)
 				assert.Equal(t, expected, actual)
 			})
-
-			//t.Run("UpdateMany", func(t *testing.T) {
-			//})
 		})
 	}
 }
