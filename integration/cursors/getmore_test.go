a860b04825c1849798a2d10fe837ccd110149133
noisersup <patryk@kwiatek.xyz>
2024-01-01 19:41:44 +0100

    fix

--- integration/cursors/getmore_test.go
+++ integration/cursors/getmore_test.go
@@ -12,11 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// TODO
-// - maxTimeMS for find before tailable cursor
-// - maxTimeMS for find before awaitdata cursor
-// - maxTimeMS for find before standard cursor
-
 package cursors
 
 import (
