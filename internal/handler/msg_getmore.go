789bb7e340a0a501027155349c70b9d3e0767317
noisersup <patryk@kwiatek.xyz>
2024-01-01 19:45:47 +0100

    wip

--- internal/handler/msg_getmore.go
+++ internal/handler/msg_getmore.go
@@ -90,6 +90,14 @@ func (h *Handler) MsgGetMore(ctx context.Context, msg *wire.OpMsg) (*wire.OpMsg,
 
 	v, _ = document.Get("maxTimeMS")
 
+	maxTimeMSPresent := v != nil
+
+	// GetOptionalParam cannot be used to set default value, as we need to return error if
+	// maxTimeMS was provided for non-awaitData cursors.
+	if v == nil {
+		v = int64(1000)
+	}
+
 	// cannot use other existing handlerparams function, they return different error codes
 	maxTimeMS, err := handlerparams.GetWholeNumberParam(v)
 	if err != nil {
@@ -152,18 +160,12 @@ func (h *Handler) MsgGetMore(ctx context.Context, msg *wire.OpMsg) (*wire.OpMsg,
 		)
 	}
 
-	if v != nil && c.Type != cursor.TailableAwait {
+	if maxTimeMSPresent && c.Type != cursor.TailableAwait {
 		return nil, handlererrors.NewCommandErrorMsgWithArgument(
 			handlererrors.ErrBadValue,
 			"cannot set maxTimeMS on getMore command for a non-awaitData cursor",
 			document.Command(),
 		)
-	}
-
-	// GetOptionalParam cannot be used to set default value, as we need to return error if
-	// maxTimeMS was provided for non-awaitData cursors.
-	if v == nil {
-		v = int64(1000)
 	}
 
 	v, _ = document.Get("batchSize")
