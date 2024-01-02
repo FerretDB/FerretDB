e97da4d07204e45a51fddf5f44060ccf6cfab12a
b1ron <goldingbyron@gmail.com>
2024-01-02 12:40:03 +0100

    add test

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -3,5 +3,17 @@
 (function() {
   'use strict';
 
+  const t = db.foo;
+  t.drop();
+
+  t.insert({_id: 1, a: 1});
+  t.createIndex({a: 1});
+
+  // a_1 already exists but command does not fail
+  assert.commandWorked(t.createIndex({a: 1}));
+
+  // _id_ already exists but command fails
+  assert.commandWorked(t.createIndex({_id: 1}));
+
   print('test.js passed!');
 })();
