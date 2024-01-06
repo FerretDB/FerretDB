0792bfd2ca34b735f61a2bb70e3864bd19f2f2ad
b1ron <goldingbyron@gmail.com>
2024-01-06 15:08:49 +0100

    add test

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -3,5 +3,23 @@
 (function() {
   'use strict';
 
+  // non-capped collection
+  const t = db.foo;
+  t.drop();
+
+  t.insert({a: 1});
+  t.insert({a: 2});
+  t.insert({a: 3});
+  t.insert({a: 4});
+
+  expected = [1, 2, 3, 4];
+
+  got = [];
+  t.find({}).sort({$natural: 1}).forEach(function(d) {
+    got.push(d.a);
+  })
+
+  assert.eq(expected, got, '$natural sort returned wrong order')
+
   print('test.js passed!');
 })();
