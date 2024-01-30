842ec792ed673636cdc581c16ce233758d435ee3
Henrique Vicente <henriquevicente@gmail.com>
2024-01-30 01:15:07 +0100

    wip

--- internal/handler/msg_saslstart.go
+++ internal/handler/msg_saslstart.go
@@ -251,7 +251,9 @@ func (h *Handler) saslStartSCRAMSHA256(ctx context.Context, doc *types.Document)
 	must.NoError(err)
 
 	conv := scramServer.NewConversation()
+
 	resp, err := conv.Step(string(payload))
+
 	if err != nil {
 		return "", err
 	}
