394c75d567ee1883ecc99b21fc7713b3789c6bd4
Henrique Vicente <henriquevicente@gmail.com>
2024-02-01 10:21:47 +0100

    wip

---
+++ internal/util/password/scramsha256.go
@@ -0,0 +1,99 @@
+// Copyright 2021 FerretDB Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package password
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+
+	"github.com/FerretDB/FerretDB/internal/types"
+	"github.com/FerretDB/FerretDB/internal/util/lazyerrors"
+	"github.com/xdg-go/scram"
+)
+
+// scramSHA256Params represent password parameters for SCRAM-SHA-256 authentication.
+type scramSHA256Params struct {
+	iterationCount int
+	saltLen        int
+	serverKeyLen   int
+	storedKeyLen   int
+}
+
+// fixedScramSHA256Params represent fixed password parameters for SCRAM-SHA-256 authentication.
+var fixedScramSHA256Params = &scramSHA256Params{
+	iterationCount: 15_000,
+	saltLen:        45,
+	serverKeyLen:   32,
+	storedKeyLen:   32,
+}
+
+// scramSHA256HashParams hashes the password with the given salt and parameters,
+// and returns the document that should be stored.
+func scramSHA256HashParams(username, password string, salt []byte, params *scramSHA256Params) (*types.Document, error) {
+	if len(salt) != int(params.saltLen) {
+		return nil, lazyerrors.Errorf("unexpected salt length: %d", len(salt))
+	}
+
+	client, err := scram.SHA256.NewClient(username, password, "")
+	if err != nil {
+		return nil, lazyerrors.Error(err)
+	}
+
+	kf := scram.KeyFactors{
+		Salt:  string(salt),
+		Iters: params.iterationCount,
+	}
+
+	cred := client.GetStoredCredentials(kf)
+
+	if len(cred.StoredKey) != int(params.storedKeyLen) {
+		return nil, lazyerrors.Errorf("unexpected stored key length: %d", len(cred.StoredKey))
+	}
+
+	if len(cred.ServerKey) != int(params.serverKeyLen) {
+		return nil, lazyerrors.Errorf("unexpected server key length: %d", len(cred.ServerKey))
+	}
+
+	storedKey := base64.StdEncoding.EncodeToString(cred.StoredKey)
+	serverKey := base64.StdEncoding.EncodeToString(cred.ServerKey)
+
+	doc, err := types.NewDocument(
+		"storedKey", storedKey,
+		"iterationCount", int32(kf.Iters),
+		"salt", base64.StdEncoding.EncodeToString(salt),
+		"serverKey", serverKey,
+	)
+
+	if err != nil {
+		return nil, lazyerrors.Error(err)
+	}
+
+	return doc, nil
+}
+
+// SCRAMSHA256Hash computes SCRAM-SHA-256 credentials and returns the document that should be stored.
+func SCRAMSHA256Hash(username, password string) (*types.Document, error) {
+	salt := make([]byte, fixedScramSHA256Params.saltLen)
+	if _, err := rand.Read(salt); err != nil {
+		return nil, lazyerrors.Error(err)
+	}
+
+	doc, err := scramSHA256HashParams(username, password, salt, fixedScramSHA256Params)
+	if err != nil {
+		return nil, lazyerrors.Error(err)
+	}
+
+	return doc, nil
+}
