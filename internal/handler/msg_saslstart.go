474506748446f5a5a6b165c86d587a1e49610959
Henrique Vicente <henriquevicente@gmail.com>
2024-02-15 01:24:43 +0100

    wip

--- internal/handler/msg_saslstart.go
+++ internal/handler/msg_saslstart.go
@@ -177,7 +177,8 @@ func saslStartPlain(doc *types.Document) (string, string, error) {
 
 // scramCredentialLookup looks up an user's credentials in the database.
 func (h *Handler) scramCredentialLookup(ctx context.Context, username, dbName, mechanism string) (
-	*scram.StoredCredentials, error) {
+	*scram.StoredCredentials, error,
+) {
 	adminDB, err := h.b.Database("admin")
 	if err != nil {
 		return nil, lazyerrors.Error(err)
