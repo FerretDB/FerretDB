9db3718afbee65a7afc67322366760917fb3826e
Chi Fujii <chi.fujii@ferretdb.io>
2024-03-01 12:51:47 +0900

    fix logout test

--- integration/commands_authentication_test.go
+++ integration/commands_authentication_test.go
@@ -17,8 +17,14 @@ package integration
 import (
 	"testing"
 
+	"github.com/FerretDB/FerretDB/internal/types"
+	"github.com/FerretDB/FerretDB/internal/util/must"
+
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 
 	"github.com/FerretDB/FerretDB/integration/setup"
 	"github.com/FerretDB/FerretDB/internal/util/testutil"
@@ -27,12 +33,55 @@ import (
 func TestCommandsAuthenticationLogout(t *testing.T) {
 	t.Parallel()
 
-	ctx, collection := setup.Setup(t)
-	db := collection.Database()
+	s := setup.SetupWithOpts(t, nil)
+	ctx, db := s.Ctx, s.Collection.Database()
+	username, password, mechanism := "testuser", "testpass", "SCRAM-SHA-256"
 
-	// the test user logs out
+	err := db.RunCommand(ctx, bson.D{
+		{"createUser", username},
+		{"roles", bson.A{}},
+		{"pwd", password},
+		{"mechanisms", bson.A{mechanism}},
+	}).Err()
+	require.NoError(t, err, "cannot create user")
+
+	credential := options.Credential{
+		AuthMechanism: mechanism,
+		AuthSource:    db.Name(),
+		Username:      username,
+		Password:      password,
+	}
+
+	opts := options.Client().ApplyURI(s.MongoDBURI).SetAuth(credential)
+
+	client, err := mongo.Connect(ctx, opts)
+	require.NoError(t, err, "cannot connect to MongoDB")
+
+	t.Cleanup(func() {
+		require.NoError(t, client.Disconnect(ctx))
+	})
+
+	db = client.Database(db.Name())
+
 	var res bson.D
-	err := db.RunCommand(ctx, bson.D{{"logout", 1}}).Decode(&res)
+	err = db.RunCommand(ctx, bson.D{{"connectionStatus", 1}}).Decode(&res)
+	assert.NoError(t, err)
+
+	actualAuth := must.NotFail(ConvertDocument(t, res).Get("authInfo")).(*types.Document)
+	actualUsers := must.NotFail(actualAuth.Get("authenticatedUsers")).(*types.Array)
+
+	var hasUser bool
+	for i := 0; i < actualUsers.Len(); i++ {
+		actualUser := must.NotFail(must.NotFail(actualUsers.Get(i)).(*types.Document).Get("user"))
+		if actualUser == username {
+			hasUser = true
+			break
+		}
+	}
+
+	assert.True(t, hasUser, res)
+
+	err = db.RunCommand(ctx, bson.D{{"logout", 1}}).Decode(&res)
 	assert.NoError(t, err)
 
 	actual := ConvertDocument(t, res)
@@ -42,15 +91,22 @@ func TestCommandsAuthenticationLogout(t *testing.T) {
 	expected := ConvertDocument(t, bson.D{{"ok", float64(1)}})
 	testutil.AssertEqual(t, expected, actual)
 
-	// the test user logs out again, it has no effect
-	err = db.RunCommand(ctx, bson.D{{"logout", 1}}).Decode(&res)
+	err = db.RunCommand(ctx, bson.D{{"connectionStatus", 1}}).Decode(&res)
 	assert.NoError(t, err)
 
-	actual = ConvertDocument(t, res)
-	actual.Remove("$clusterTime")
-	actual.Remove("operationTime")
+	actualAuth = must.NotFail(ConvertDocument(t, res).Get("authInfo")).(*types.Document)
+	actualUsers = must.NotFail(actualAuth.Get("authenticatedUsers")).(*types.Array)
 
-	testutil.AssertEqual(t, expected, actual)
+	for i := 0; i < actualUsers.Len(); i++ {
+		actualUser := must.NotFail(must.NotFail(actualUsers.Get(i)).(*types.Document).Get("user"))
+		if actualUser == username {
+			assert.Fail(t, "user is still authenticated", res)
+		}
+	}
+
+	// the test user logs out again, it has no effect
+	err = db.RunCommand(ctx, bson.D{{"logout", 1}}).Err()
+	assert.NoError(t, err)
 }
 
 func TestCommandsAuthenticationLogoutTLS(t *testing.T) {
