5a5ca29f200ad0fa62e8d98df929f96f82fbcaa2
Alexey Palazhchenko <alexey.palazhchenko@ferretdb.io>
2024-03-06 14:19:39 +0400

    Use Go 1.22.1 (#4155)

--- tools/tools.go
+++ tools/tools.go
@@ -12,11 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Keep both old and new styles of build tags.
-
-//go:build go1.22
-// +build go1.22
-
 package tools
 
 import (
@@ -31,13 +26,6 @@ import (
 	_ "golang.org/x/vuln/cmd/govulncheck"
 	_ "mvdan.cc/gofumpt"
 )
-
-// Check that `go` in $PATH have the right version.
-// Catches problems like `/some/path/go generate` invocations where `/some/path/go` is 1.22+
-// (that's checked by the build tags above), but just `go` in $PATH (typically something like `/usr/bin/go`)
-// is an earlier version.
-
-//go:generate go run check.go
 
 //go:generate go build -v -o ../bin/ ./checkdocs
 //go:generate go build -v -o ../bin/ ./checkswitch
