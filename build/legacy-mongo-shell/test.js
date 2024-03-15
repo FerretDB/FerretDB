7c3909a14a0b53e8f3e8c268e9fbda1b5d57798e
b1ron <goldingbyron@gmail.com>
2024-03-15 14:05:02 +0100

    insert

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -1,14 +1,21 @@
+/* eslint-disable max-len */
 // Please do not merge changes in this file.
 
 (function() {
   'use strict';
 
-  const res = db.runCommand({ping: 1});
+  const t = db.foo;
+  t.drop();
+
+  let res = db.runCommand({ping: 1});
   assert.eq(res.ok, 1, 'ping failed');
-  
+
+  res = t.insert({});
+  assert.writeOK(res, 'insert failed');
+
   let port = 27017;
 
-  let roles = [];
+  const roles = [];
 
   if (db.getSiblingDB('admin').runCommand({getParameter: '*'}).wiredTigerConcurrentReadTransactions !== undefined) {
     roles.push({role: 'read', db: 'admin'});
@@ -16,12 +23,12 @@
   };
 
   // db.getSiblingDB('admin').system.users.remove({});
-  
+
   db.getSiblingDB('admin').createUser({user: 'username2', pwd: 'password2', roles: roles});
 
   const mongoClient = function(uri) {
     return new Mongo(uri);
-  }
+  };
 
   const uri = 'mongodb://username2:password2@host.docker.internal:' + port + '/?authMechanism=SCRAM-SHA-1';
 
