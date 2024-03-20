556c25a83f00177cd78d2073510f4167d8c0e8dd
noisersup <patryk@kwiatek.xyz>
2024-03-20 20:36:28 +0100

    wip

--- internal/driver/driver_test.go
+++ internal/driver/driver_test.go
@@ -73,13 +73,15 @@ func TestDriver(t *testing.T) {
 		require.NoError(t, err)
 	})
 
+	expectedDocs := must.NotFail(bson.NewArray(
+		must.NotFail(bson.NewDocument("v", int32(1), "_id", int32(0))),
+		must.NotFail(bson.NewDocument("v", int32(2), "_id", int32(1))),
+	))
+
 	t.Run("Insert", func(t *testing.T) {
 		doc := must.NotFail(bson.NewDocument(
 			"insert", "values",
-			"documents", must.NotFail(bson.NewArray(
-				must.NotFail(bson.NewDocument("v", int32(1), "_id", bson.ObjectID([]byte("65f83bddef2048e47170b641")))),
-				must.NotFail(bson.NewDocument("v", int32(2), "_id", bson.ObjectID([]byte("65f83bddef2048e47170b642")))),
-			)),
+			"documents", expectedDocs,
 			"ordered", true,
 			"lsid", must.NotFail(bson.NewDocument("id", lsid)),
 			"$db", dbName,
@@ -114,9 +116,10 @@ func TestDriver(t *testing.T) {
 	t.Run("Find", func(t *testing.T) {
 		doc := must.NotFail(bson.NewDocument(
 			"find", "values",
-			"filter", must.NotFail(bson.NewDocument("v", int32(1))),
+			//"filter", must.NotFail(bson.NewDocument("v", int32(1))),
+			"filter", must.NotFail(bson.NewDocument()),
 			"lsid", must.NotFail(bson.NewDocument("id", lsid)),
-			"batchSize", int32(1),
+			"batchSize", int32(2),
 			"$db", dbName,
 		))
 
@@ -145,7 +148,6 @@ func TestDriver(t *testing.T) {
 		require.NoError(t, err)
 
 		firstBatch := cursor.Get("firstBatch")
-
-		require.Equal(t, "", firstBatch)
+		require.Equal(t, must.NotFail(expectedDocs.Encode()), firstBatch)
 	})
 }
