8fda8d89944fa0cab496c1127dab7207d4e344d8
kropidlowsky <kropidlowsky@protonmail.com>
2024-04-23 23:59:54 +0200

    WIP

--- tools/github/client.go
+++ tools/github/client.go
@@ -15,14 +15,19 @@
 package github
 
 import (
+	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io/fs"
+	"net/http"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/FerretDB/gh"
 	"github.com/google/go-github/v57/github"
+	"github.com/rogpeppe/go-internal/lockedfile"
 )
 
 // cacheFile stores information regarding rate limiting and the status of issues.
@@ -101,4 +106,117 @@ func CacheFilePath(toolName string) (string, error) {
 	}
 
 	return filepath.Join(dir, "tmp", toolName, "cache.json"), nil
+}
+
+// IssueStatus returns issue status.
+// It uses cache.
+//
+// Returned error is something fatal.
+// On rate limit, the error is logged once and (issueOpen, nil) is returned.
+func (c *Client) IssueStatus(ctx context.Context, url, repo string, num int) (issueStatus, error) {
+	start := time.Now()
+
+	cache := &cacheFile{
+		Issues: make(map[string]issue),
+	}
+	cacheRes := "miss"
+
+	var res issueStatus
+
+	// fast path without any locks
+
+	data, err := os.ReadFile(c.cacheFilePath)
+	if err == nil {
+		_ = json.Unmarshal(data, cache)
+		res = cache.Issues[url].Status
+	}
+
+	if res != "" {
+		cacheRes = "fast hit"
+	} else {
+		// slow path
+
+		noUpdate := fmt.Errorf("no need to update the cache file")
+
+		err = lockedfile.Transform(c.cacheFilePath, func(data []byte) ([]byte, error) {
+			cache.Issues = make(map[string]issue)
+
+			if len(data) != 0 {
+				if err = json.Unmarshal(data, cache); err != nil {
+					return nil, err
+				}
+			}
+
+			if res = cache.Issues[url].Status; res != "" {
+				return nil, noUpdate
+			}
+
+			if res, err = c.checkIssueStatus(ctx, repo, num); err != nil {
+				var rle *github.RateLimitError
+				if !errors.As(err, &rle) {
+					return nil, fmt.Errorf("%s: %s", url, err)
+				}
+
+				if cache.RateLimitReached {
+					c.clientDebugF("Rate limit already reached: %s", url)
+					return nil, noUpdate
+				}
+
+				cache.RateLimitReached = true
+
+				msg := "Rate limit reached: " + err.Error()
+				if c.token == "" {
+					msg += "\nPlease set a GITHUB_TOKEN as described at " +
+						"https://github.com/FerretDB/FerretDB/blob/main/CONTRIBUTING.md#setting-a-github_token"
+				}
+				c.logf("%s", msg)
+			}
+
+			// unless rate limited
+			if res != "" {
+				cache.Issues[url] = issue{
+					RefreshedAt: time.Now(),
+					Status:      res,
+				}
+			}
+
+			return json.MarshalIndent(cache, "", "  ")
+		})
+
+		if errors.Is(err, noUpdate) {
+			cacheRes = "slow hit"
+			err = nil
+		}
+	}
+
+	c.cacheDebugF("%s: %s (%dms, %s)", url, res, time.Since(start).Milliseconds(), cacheRes)
+
+	// when rate limited
+	if err == nil && res == "" {
+		res = issueOpen
+	}
+
+	return res, err
+}
+
+// checkIssueStatus checks issue status via GitHub API.
+// It does not use cache.
+func (c *Client) checkIssueStatus(ctx context.Context, repo string, num int) (issueStatus, error) {
+	issue, resp, err := c.c.Issues.Get(ctx, "FerretDB", repo, num)
+	if err != nil {
+		if resp.StatusCode == http.StatusNotFound {
+			return issueNotFound, nil
+		}
+
+		return "", err
+	}
+
+	switch s := issue.GetState(); s {
+	case "open":
+		return issueOpen, nil
+	case "closed":
+		return issueClosed, nil
+	default:
+		return "", fmt.Errorf("unknown issue state: %q", s)
+	}
 }
--- tools/github/issue.go
+++ tools/github/issue.go
@@ -15,17 +15,9 @@
 package github
 
 import (
-	"context"
-	"encoding/json"
-	"errors"
 	"fmt"
 	"log"
-	"net/http"
-	"os"
 	"time"
-
-	"github.com/google/go-github/v57/github"
-	"github.com/rogpeppe/go-internal/lockedfile"
 )
 
 // issueStatus represents a known issue status.
@@ -42,119 +34,6 @@ const (
 type issue struct {
 	RefreshedAt time.Time   `json:"refreshedAt"`
 	Status      issueStatus `json:"status"`
-}
-
-// IssueStatus returns issue status.
-// It uses cache.
-//
-// Returned error is something fatal.
-// On rate limit, the error is logged once and (issueOpen, nil) is returned.
-func (c *Client) IssueStatus(ctx context.Context, url, repo string, num int) (issueStatus, error) {
-	start := time.Now()
-
-	cache := &cacheFile{
-		Issues: make(map[string]issue),
-	}
-	cacheRes := "miss"
-
-	var res issueStatus
-
-	// fast path without any locks
-
-	data, err := os.ReadFile(c.cacheFilePath)
-	if err == nil {
-		_ = json.Unmarshal(data, cache)
-		res = cache.Issues[url].Status
-	}
-
-	if res != "" {
-		cacheRes = "fast hit"
-	} else {
-		// slow path
-
-		noUpdate := fmt.Errorf("no need to update the cache file")
-
-		err = lockedfile.Transform(c.cacheFilePath, func(data []byte) ([]byte, error) {
-			cache.Issues = make(map[string]issue)
-
-			if len(data) != 0 {
-				if err = json.Unmarshal(data, cache); err != nil {
-					return nil, err
-				}
-			}
-
-			if res = cache.Issues[url].Status; res != "" {
-				return nil, noUpdate
-			}
-
-			if res, err = c.checkIssueStatus(ctx, repo, num); err != nil {
-				var rle *github.RateLimitError
-				if !errors.As(err, &rle) {
-					return nil, fmt.Errorf("%s: %s", url, err)
-				}
-
-				if cache.RateLimitReached {
-					c.clientDebugF("Rate limit already reached: %s", url)
-					return nil, noUpdate
-				}
-
-				cache.RateLimitReached = true
-
-				msg := "Rate limit reached: " + err.Error()
-				if c.token == "" {
-					msg += "\nPlease set a GITHUB_TOKEN as described at " +
-						"https://github.com/FerretDB/FerretDB/blob/main/CONTRIBUTING.md#setting-a-github_token"
-				}
-				c.logf("%s", msg)
-			}
-
-			// unless rate limited
-			if res != "" {
-				cache.Issues[url] = issue{
-					RefreshedAt: time.Now(),
-					Status:      res,
-				}
-			}
-
-			return json.MarshalIndent(cache, "", "  ")
-		})
-
-		if errors.Is(err, noUpdate) {
-			cacheRes = "slow hit"
-			err = nil
-		}
-	}
-
-	c.cacheDebugF("%s: %s (%dms, %s)", url, res, time.Since(start).Milliseconds(), cacheRes)
-
-	// when rate limited
-	if err == nil && res == "" {
-		res = issueOpen
-	}
-
-	return res, err
-}
-
-// checkIssueStatus checks issue status via GitHub API.
-// It does not use cache.
-func (c *Client) checkIssueStatus(ctx context.Context, repo string, num int) (issueStatus, error) {
-	issue, resp, err := c.c.Issues.Get(ctx, "FerretDB", repo, num)
-	if err != nil {
-		if resp.StatusCode == http.StatusNotFound {
-			return issueNotFound, nil
-		}
-
-		return "", err
-	}
-
-	switch s := issue.GetState(); s {
-	case "open":
-		return issueOpen, nil
-	case "closed":
-		return issueClosed, nil
-	default:
-		return "", fmt.Errorf("unknown issue state: %q", s)
-	}
 }
 
 // Validate checks the issues status and returns its message (if it is not open).
