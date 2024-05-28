990f1d228ad8f4057c68f88884bb4fe57f73bc8e
Alexey Palazhchenko <alexey.palazhchenko@ferretdb.io>
2024-05-28 14:46:47 +0400

    Refactor

--- cmd/ferretdb/main.go
+++ cmd/ferretdb/main.go
@@ -292,6 +292,21 @@ func setupLogger(stateProvider *state.Provider, format string) *zap.Logger {
 	return l
 }
 
+// checkFlags checks that CLI flags are not self-contradictory.
+func checkFlags(logger *zap.Logger) {
+	if cli.Setup.Username != "" && !cli.Test.EnableNewAuth {
+		logger.Sugar().Fatal("--setup-username requires --test-enable-new-auth")
+	}
+
+	if cli.Setup.Password != "" && cli.Setup.Username == "" {
+		logger.Sugar().Fatal("--setup-password requires --setup-username")
+	}
+
+	if cli.Test.DisablePushdown && cli.Test.EnableNestedPushdown {
+		logger.Sugar().Fatal("--test-disable-pushdown and --test-enable-nested-pushdown should not be set at the same time")
+	}
+}
+
 // runTelemetryReporter runs telemetry reporter until ctx is canceled.
 func runTelemetryReporter(ctx context.Context, opts *telemetry.NewReporterOpts) {
 	r, err := telemetry.NewReporter(opts)
@@ -347,6 +362,8 @@ func run() {
 
 	logger := setupLogger(stateProvider, cli.Log.Format)
 
+	checkFlags(logger)
+
 	if _, err := maxprocs.Set(maxprocs.Logger(logger.Sugar().Debugf)); err != nil {
 		logger.Sugar().Warnf("Failed to set GOMAXPROCS: %s.", err)
 	}
@@ -360,14 +377,6 @@ func run() {
 	}()
 
 	var wg sync.WaitGroup
-
-	if cli.Setup.Username != "" && !cli.Test.EnableNewAuth {
-		logger.Sugar().Fatal("--setup-username requires --test-enable-new-auth")
-	}
-
-	if cli.Test.DisablePushdown && cli.Test.EnableNestedPushdown {
-		logger.Sugar().Fatal("--test-disable-pushdown and --test-enable-nested-pushdown should not be set at the same time")
-	}
 
 	if cli.DebugAddr != "" && cli.DebugAddr != "-" {
 		wg.Add(1)
