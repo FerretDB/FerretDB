d7421766c52f8de2b7d54f3169c69f93c33684a8
Chi Fujii <chi.fujii@ferretdb.io>
2024-06-20 13:01:41 +0900

    use test database for creating user

--- integration/setup/listener.go
+++ integration/setup/listener.go
@@ -198,7 +198,7 @@ func setupListener(tb testtb.TB, ctx context.Context, logger *zap.Logger, opts *
 	}
 
 	if !opts.DisableNewAuth {
-		handlerOpts.SetupDatabase = testutil.DatabaseName(tb)
+		handlerOpts.SetupDatabase = "test"
 		handlerOpts.SetupUsername = "username"
 		handlerOpts.SetupPassword = password.WrapPassword("password")
 		handlerOpts.SetupTimeout = 1 * time.Second
