282e0a8c718595e4410f6e3fdb15a6348721867b
Alexey Palazhchenko <alexey.palazhchenko@ferretdb.io>
2024-06-26 13:20:09 +0400

    Avoid mutating map during iteration

--- internal/handler/commands.go
+++ internal/handler/commands.go
@@ -37,7 +37,7 @@ type command struct {
 
 // initCommands initializes the commands map for that handler instance.
 func (h *Handler) initCommands() {
-	h.commands = map[string]command{
+	h.commands = map[string]*command{
 		// sorted alphabetically
 		"aggregate": {
 			Handler: h.MsgAggregate,
@@ -252,23 +252,23 @@ func (h *Handler) initCommands() {
 
 	if h.EnableNewAuth {
 		// sorted alphabetically
-		h.commands["createUser"] = command{
+		h.commands["createUser"] = &command{
 			Handler: h.MsgCreateUser,
 			Help:    "Creates a new user.",
 		}
-		h.commands["dropAllUsersFromDatabase"] = command{
+		h.commands["dropAllUsersFromDatabase"] = &command{
 			Handler: h.MsgDropAllUsersFromDatabase,
 			Help:    "Drops all user from database.",
 		}
-		h.commands["dropUser"] = command{
+		h.commands["dropUser"] = &command{
 			Handler: h.MsgDropUser,
 			Help:    "Drops user.",
 		}
-		h.commands["updateUser"] = command{
+		h.commands["updateUser"] = &command{
 			Handler: h.MsgUpdateUser,
 			Help:    "Updates user.",
 		}
-		h.commands["usersInfo"] = command{
+		h.commands["usersInfo"] = &command{
 			Handler: h.MsgUsersInfo,
 			Help:    "Returns information about users.",
 		}
@@ -277,25 +277,18 @@ func (h *Handler) initCommands() {
 
 	for name, cmd := range h.commands {
 		if !cmd.anonymous {
-			cmd.Handler = h.authenticateWrapper(cmd)
-		}
+			h.commands[name].Handler = func(ctx context.Context, msg *wire.OpMsg) (*wire.OpMsg, error) {
+				if err := h.checkAuthentication(ctx); err != nil {
+					return nil, err
+				}
 
-		h.commands[name] = cmd
+				return cmd.Handler(ctx, msg)
+			}
+		}
 	}
 }
 
 // Commands returns a map of enabled commands.
-func (h *Handler) Commands() map[string]command {
+func (h *Handler) Commands() map[string]*command {
 	return h.commands
-}
-
-// authenticateWrapper wraps the command handler with the authentication check.
-func (h *Handler) authenticateWrapper(cmd command) func(context.Context, *wire.OpMsg) (*wire.OpMsg, error) {
-	return func(ctx context.Context, msg *wire.OpMsg) (*wire.OpMsg, error) {
-		if err := h.checkAuthentication(ctx); err != nil {
-			return nil, err
-		}
-
-		return cmd.Handler(ctx, msg)
-	}
 }
