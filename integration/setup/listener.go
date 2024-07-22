6312dab9728e47479b2cf43ff206105a26e795cc
Chi Fujii <chi.fujii@ferretdb.io>
2024-07-22 20:10:42 +0900

    Use `slog` in integration tests (#4481)
    Closes #4013.

--- integration/setup/listener.go
+++ integration/setup/listener.go
@@ -101,7 +101,7 @@ func listenerMongoDBURI(tb testtb.TB, hostPort, unixSocketPath, newAuthDB string
 
 // setupListener starts in-process FerretDB server that runs until ctx is canceled.
 // It returns basic MongoDB URI for that listener.
-func setupListener(tb testtb.TB, ctx context.Context, logger *zap.Logger, opts *BackendOpts) string {
+func setupListener(tb testtb.TB, ctx context.Context, logger *slog.Logger, opts *BackendOpts) string {
 	tb.Helper()
 
 	ctx, span := otel.Tracer("").Start(ctx, "setupListener")
@@ -176,7 +176,7 @@ func setupListener(tb testtb.TB, ctx context.Context, logger *zap.Logger, opts *
 	}
 
 	handlerOpts := &registry.NewHandlerOpts{
-		Logger:        logger,
+		Logger:        zap.L(),
 		SLogger:       slog.Default(), // TODO https://github.com/FerretDB/FerretDB/issues/4013
 		ConnMetrics:   listenerMetrics.ConnMetrics,
 		StateProvider: sp,
@@ -216,7 +216,7 @@ func setupListener(tb testtb.TB, ctx context.Context, logger *zap.Logger, opts *
 		Mode:           clientconn.NormalMode,
 		Metrics:        listenerMetrics,
 		Handler:        h,
-		Logger:         slog.Default(),
+		Logger:         slog.Default(), // TODO https://github.com/FerretDB/FerretDB/issues/4013
 		TestRecordsDir: testutil.TmpRecordsDir,
 	}
 
@@ -274,7 +274,7 @@ func setupListener(tb testtb.TB, ctx context.Context, logger *zap.Logger, opts *
 
 	uri := listenerMongoDBURI(tb, hostPort, unixSocketPath, handlerOpts.SetupDatabase, tlsAndAuth)
 
-	logger.Info("Listener started", zap.String("handler", handler), zap.String("uri", uri))
+	logger.InfoContext(ctx, "Listener started", slog.String("handler", handler), slog.String("uri", uri))
 
 	return uri
 }
