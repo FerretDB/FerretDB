ed1c0875a4711f955dde1c0c2e95fa46d606be33
Patryk Kwiatek <patryk.kwiatek@ferretdb.io>
2024-07-26 17:33:29 +0200

    wip

--- tools/checkdocs/checkdocs_test.go
+++ tools/checkdocs/checkdocs_test.go
@@ -109,6 +109,11 @@ func TestCheckSupportedCommands(t *testing.T) {
 			Payload:        "|                 | `closedIssueLink`          | ❌     | [Issue](https://github.com/FerretDB/FerretDB/issues/1) |",
 			ExpectedOutput: "linked issue https://github.com/FerretDB/FerretDB/issues/1 is closed\n",
 		},
+
+		"AnyLabelClosedIssue": {
+			Payload:        "[IssueLabel](https://github.com/FerretDB/FerretDB/issues/1)",
+			ExpectedOutput: "linked issue https://github.com/FerretDB/FerretDB/issues/1 is closed\n",
+		},
 	} {
 		t.Run(name, func(t *testing.T) {
 			r := strings.NewReader(tc.Payload)
