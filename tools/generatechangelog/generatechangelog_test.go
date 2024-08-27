33ded3de2e4d4c02b5294996a8753fa0b3418880
Elena Grahovac <elena@grahovac.me>
2024-08-27 14:59:24 +0200

    wip

--- tools/generatechangelog/generatechangelog_test.go
+++ tools/generatechangelog/generatechangelog_test.go
@@ -103,10 +103,10 @@ func TestGenerateChangelog(t *testing.T) {
 		"https://github.com/FerretDB/FerretDB/pull/4077\n" +
 		"- Add Tembo QA blog post by @Fashander in " +
 		"https://github.com/FerretDB/FerretDB/pull/4081\n" +
-		"- Update correct image link by @Fashander in " +
-		"https://github.com/FerretDB/FerretDB/pull/4116\n" +
 		"- Add Pulumi blog post by @Fashander in " +
 		"https://github.com/FerretDB/FerretDB/pull/4102\n" +
+		"- Update correct image link by @Fashander in " +
+		"https://github.com/FerretDB/FerretDB/pull/4116\n" +
 		"- Add Tembo to README by @AlekSi in " +
 		"https://github.com/FerretDB/FerretDB/pull/4168\n" +
 		"- Remove some closed issues from documentation by @AlekSi in " +
@@ -130,58 +130,58 @@ func TestGenerateChangelog(t *testing.T) {
 		"https://github.com/FerretDB/FerretDB/pull/4106\n" +
 		"- Use `bson2` package for wire queries and replies by @AlekSi in " +
 		"https://github.com/FerretDB/FerretDB/pull/4108\n" +
+		"- Replace `bson` with `bson2` in `wire` by @AlekSi in " +
+		"https://github.com/FerretDB/FerretDB/pull/4110\n" +
+		"- Support speculative authenticate by @chilagrow in " +
+		"https://github.com/FerretDB/FerretDB/pull/4111\n" +
+		"- Advertise SCRAM / SASL support in addition to PLAIN by @henvic in " +
+		"https://github.com/FerretDB/FerretDB/pull/4113\n" +
 		"- Ignore `maxTimeMS` argument in `count`, `insert`, `update`, `delete` by @farit2000 in " +
 		"https://github.com/FerretDB/FerretDB/pull/4121\n" +
 		"- Use correct salt length by @AlekSi in " +
 		"https://github.com/FerretDB/FerretDB/pull/4126\n" +
-		"- Skip stuck tailable cursor test by @chilagrow in " +
-		"https://github.com/FerretDB/FerretDB/pull/4131\n" +
-		"- Replace `bson` with `bson2` in `wire` by @AlekSi in " +
-		"https://github.com/FerretDB/FerretDB/pull/4110\n" +
-		"- Improve `OP_MSG` validity checks by @AlekSi in " +
-		"https://github.com/FerretDB/FerretDB/pull/4135\n" +
-		"- Support speculative authenticate by @chilagrow in " +
-		"https://github.com/FerretDB/FerretDB/pull/4111\n" +
-		"- Add MySQL backend by @adetunjii in " +
-		"https://github.com/FerretDB/FerretDB/pull/4137\n" +
 		"- Fix `saslContinue` crashing due to not found authentication conversation by @henvic in " +
 		"https://github.com/FerretDB/FerretDB/pull/4129\n" +
+		"- Skip stuck tailable cursor test by @chilagrow in " +
+		"https://github.com/FerretDB/FerretDB/pull/4131\n" +
+		"- Improve `OP_MSG` validity checks by @AlekSi in " +
+		"https://github.com/FerretDB/FerretDB/pull/4135\n" +
+		"- Add MySQL backend by @adetunjii in " +
+		"https://github.com/FerretDB/FerretDB/pull/4137\n" +
+		"- Add linter to check truncate tag in blog posts by @sbshah97 in " +
+		"https://github.com/FerretDB/FerretDB/pull/4139\n" +
 		"- Cleanup TODO for speculative authenticate by @chilagrow in " +
 		"https://github.com/FerretDB/FerretDB/pull/4143\n" +
 		"- Fix MySQL collection stats by @adetunjii in " +
 		"https://github.com/FerretDB/FerretDB/pull/4145\n" +
-		"- Use Go 1.22.1 by @AlekSi in " +
-		"https://github.com/FerretDB/FerretDB/pull/4155\n" +
-		"- Advertise SCRAM / SASL support in addition to PLAIN by @henvic in " +
-		"https://github.com/FerretDB/FerretDB/pull/4113\n" +
-		"- Add linter to check truncate tag in blog posts by @sbshah97 in " +
-		"https://github.com/FerretDB/FerretDB/pull/4139\n" +
-		"- Fix PLAIN mechanism authentication incorrectly working by @chilagrow in " +
-		"https://github.com/FerretDB/FerretDB/pull/4163\n" +
 		"- Improve `bson2` and `wire` logging by @AlekSi in " +
 		"https://github.com/FerretDB/FerretDB/pull/4148\n" +
-		"- Fix logging of deeply nested documents by @AlekSi in " +
-		"https://github.com/FerretDB/FerretDB/pull/4167\n" +
+		"- Use Go 1.22.1 by @AlekSi in " +
+		"https://github.com/FerretDB/FerretDB/pull/4155\n" +
 		"- Support localhost exception by @chilagrow in " +
 		"https://github.com/FerretDB/FerretDB/pull/4156\n" +
+		"- Use authentication enabled docker for integration test by @chilagrow in " +
+		"https://github.com/FerretDB/FerretDB/pull/4160\n" +
+		"- Fix PLAIN mechanism authentication incorrectly working by @chilagrow in " +
+		"https://github.com/FerretDB/FerretDB/pull/4163\n" +
+		"- Fix logging of deeply nested documents by @AlekSi in " +
+		"https://github.com/FerretDB/FerretDB/pull/4167\n" +
 		"- Do not use the flow style in the diff output by @AlekSi in " +
 		"https://github.com/FerretDB/FerretDB/pull/4170\n" +
 		"- Do not use `fjson` by @AlekSi in " +
 		"https://github.com/FerretDB/FerretDB/pull/4175\n" +
 		"- Remove `fjson` package by @AlekSi in " +
 		"https://github.com/FerretDB/FerretDB/pull/4176\n" +
-		"- Fix `speculativeAuthenticate` panic on empty database by @chilagrow in " +
-		"https://github.com/FerretDB/FerretDB/pull/4178\n" +
 		"- Move old `bson` package by @AlekSi in " +
 		"https://github.com/FerretDB/FerretDB/pull/4177\n" +
+		"- Fix `speculativeAuthenticate` panic on empty database by @chilagrow in " +
+		"https://github.com/FerretDB/FerretDB/pull/4178\n" +
 		"- Rename `bson2` to `bson` by @AlekSi in " +
 		"https://github.com/FerretDB/FerretDB/pull/4179\n" +
 		"- Move Docker build files by @AlekSi in " +
 		"https://github.com/FerretDB/FerretDB/pull/4180\n" +
 		"- Bump protobuf dependency to make CI happy by @AlekSi in " +
 		"https://github.com/FerretDB/FerretDB/pull/4187\n" +
-		"- Use authentication enabled docker for integration test by @chilagrow in " +
-		"https://github.com/FerretDB/FerretDB/pull/4160\n" +
 		"- Bump `pgx` by @AlekSi in " +
 		"https://github.com/FerretDB/FerretDB/pull/4190\n\n" +
 		"[All closed issues and pull requests](https://github.com/FerretDB/FerretDB/milestone/63?closed=1).\n" +
