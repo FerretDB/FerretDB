66a1278421c2d05b4b11f5d8409c2b653233b452
Patryk Kwiatek <patryk.kwiatek@ferretdb.io>
2025-01-28 13:49:58 +0100

    wip

--- internal/dataapi/listener_test.go
+++ internal/dataapi/listener_test.go
@@ -14,8 +14,46 @@
 
 package dataapi
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/FerretDB/FerretDB/v2/internal/documentdb"
+	"github.com/FerretDB/FerretDB/v2/internal/handler"
+	"github.com/FerretDB/FerretDB/v2/internal/util/logging"
+	"github.com/FerretDB/FerretDB/v2/internal/util/state"
+	"github.com/FerretDB/FerretDB/v2/internal/util/testutil"
+	"github.com/stretchr/testify/require"
+)
 
 func TestSmokeDataAPI(t *testing.T) {
+	const uri = "postgres://username:password@127.0.0.1:5432/postgres"
+	const addr = ""
 
+	sp, err := state.NewProvider("")
+	require.NoError(t, err)
+
+	l := testutil.Logger(t)
+
+	p, err := documentdb.NewPool(uri, l, sp)
+	require.NoError(t, err)
+
+	handlerOpts := &handler.NewOpts{
+		Pool: p,
+		Auth: true,
+
+		L:             logging.WithName(l, "handler"),
+		StateProvider: sp,
+	}
+
+	h, err := handler.New(handlerOpts)
+	require.NoError(t, err)
+
+	var lis *Listener
+	lis, err = Listen(&ListenOpts{
+		TCPAddr: addr,
+		L:       l,
+		Handler: h,
+	})
+
+	lis.Run()
 }
