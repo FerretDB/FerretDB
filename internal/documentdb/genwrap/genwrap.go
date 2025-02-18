d2676f69771bc87ff9443fca423298d7f5001f17
Patryk Kwiatek <patryk.kwiatek@ferretdb.io>
2025-02-18 14:35:21 +0100

    wip

--- internal/documentdb/genwrap/genwrap.go
+++ internal/documentdb/genwrap/genwrap.go
@@ -46,13 +46,13 @@ func main() {
 	ctx := context.Background()
 
 	if *schemasF == "" {
-		l.Log(ctx, logging.LevelFatal, "-schemas flag is empty.")
+		l.Log(ctx, logging.LevelFatal, "-schemas flag is empty")
 	}
 
 	// DOCUMENTDB_GEN_URL=postgres://username:password@127.0.0.1:5432/postgres
 	uri := os.Getenv("DOCUMENTDB_GEN_URL")
 	if uri == "" {
-		l.InfoContext(ctx, "DOCUMENTDB_GEN_URL not set, skipping code generation.")
+		l.InfoContext(ctx, "DOCUMENTDB_GEN_URL not set, skipping code generation")
 		os.Exit(0)
 	}
 
