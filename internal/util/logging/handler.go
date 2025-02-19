9dd80e08fb30178fa12442be495c519bd961dcb0
Patryk Kwiatek <patryk.kwiatek@ferretdb.io>
2025-02-19 18:29:35 +0100

    Revert "wip"
    This reverts commit e884682c4d755d1359a4eb9c002bda68afa77149.

--- internal/util/logging/handler.go
+++ internal/util/logging/handler.go
@@ -170,10 +170,15 @@ func (h *Handler) Handle(ctx context.Context, r slog.Record) error {
 	}
 
 	if h.checkMessages {
-		if strings.TrimSpace(r.Message) != r.Message {
+		switch {
+		case strings.TrimSpace(r.Message) != r.Message:
 			panic(fmt.Sprintf("message %q has leading/trailing spaces", r.Message))
-		}
-		if strings.TrimRight(r.Message, ".?!") != r.Message {
+
+		case strings.TrimRight(r.Message, ".?!") != r.Message:
+			if strings.TrimSuffix(r.Message, "/...") != r.Message {
+				break
+			}
+
 			panic(fmt.Sprintf("message %q ends with punctuation", r.Message))
 		}
 	}
