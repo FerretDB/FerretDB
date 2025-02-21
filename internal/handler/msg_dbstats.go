dc5a791516ce355559eaf857ef16d5fbd46448a4
Patryk Kwiatek <patryk.kwiatek@ferretdb.io>
2025-02-21 11:39:33 +0100

    wip

--- internal/handler/msg_dbstats.go
+++ internal/handler/msg_dbstats.go
@@ -19,13 +19,53 @@ import (
 
 	"github.com/FerretDB/wire"
 
-	"github.com/FerretDB/FerretDB/v2/internal/mongoerrors"
+	"github.com/FerretDB/FerretDB/v2/internal/documentdb/documentdb_api"
+	"github.com/FerretDB/FerretDB/v2/internal/util/lazyerrors"
 )
 
 // MsgDBStats implements `dbStats` command.
 //
 // The passed context is canceled when the client connection is closed.
 func (h *Handler) MsgDBStats(connCtx context.Context, msg *wire.OpMsg) (*wire.OpMsg, error) {
-	// TODO https://github.com/FerretDB/FerretDB-DocumentDB/issues/9
-	return nil, mongoerrors.New(mongoerrors.ErrNotImplemented, `"dbStats" is not implemented in DocumentDB yet`)
+	spec, err := msg.RawDocument()
+	if err != nil {
+		return nil, lazyerrors.Error(err)
+	}
+
+	if _, _, err = h.s.CreateOrUpdateByLSID(connCtx, spec); err != nil {
+		return nil, err
+	}
+
+	doc, err := spec.Decode()
+	if err != nil {
+		return nil, lazyerrors.Error(err)
+	}
+
+	dbName, err := getRequiredParam[string](doc, "$db")
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO
+	scale := float64(1)
+
+	// TODO
+	freeStorage := false
+
+	conn, err := h.Pool.Acquire()
+	if err != nil {
+		return nil, lazyerrors.Error(err)
+	}
+	defer conn.Release()
+
+	page, err := documentdb_api.DbStats(connCtx, conn.Conn(), h.L, dbName, scale, freeStorage)
+	if err != nil {
+		return nil, lazyerrors.Error(err)
+	}
+
+	if msg, err = wire.NewOpMsg(page); err != nil {
+		return nil, lazyerrors.Error(err)
+	}
+
+	return msg, nil
 }
