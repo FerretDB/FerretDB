2cb718e802892da853f99e50ef40a4bd5a4ab73e
Patryk Kwiatek <patryk.kwiatek@ferretdb.io>
2025-02-21 11:23:54 +0100

    wip

--- internal/util/logging/handler.go
+++ internal/util/logging/handler.go
@@ -172,10 +172,6 @@ func (h *Handler) Handle(ctx context.Context, r slog.Record) error {
 			panic(fmt.Sprintf("message %q has leading/trailing spaces", r.Message))
 
 		case strings.TrimRight(r.Message, ".?!") != r.Message:
-			if strings.TrimSuffix(r.Message, "/...") != r.Message {
-				break
-			}
-
 			panic(fmt.Sprintf("message %q ends with punctuation", r.Message))
 		}
 	}
