39c4ab55520eeb77f3114748602e8f43e4c861e8
Alexey Palazhchenko <alexey.palazhchenko@ferretdb.io>
2025-02-21 18:26:47 +0400

    Re-introduce embeddable package (#4778)

--- internal/util/state/provider_other.go
+++ internal/util/state/provider_other.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//go:build !unix
+
 package state
 
 // newProviderDirErr returns the state file access error as is.
