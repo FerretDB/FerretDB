b1f24eb8f5b6eab82335a86034b8b2b836366927
Patryk Kwiatek <patryk.kwiatek@ferretdb.io>
2025-03-04 18:27:19 +0100

    wip

--- cmd/envtool/envtool.go
+++ cmd/envtool/envtool.go
@@ -244,11 +244,12 @@ func main() {
 	}
 
 	opts := &logging.NewHandlerOpts{
-		Base:          "console",
-		Level:         level,
-		RemoveTime:    true,
-		RemoveSource:  true,
-		CheckMessages: true,
+		Base:         "console",
+		Level:        level,
+		RemoveTime:   true,
+		RemoveSource: true,
+		// the logger handles messages from the `go test` command output
+		CheckMessages: false,
 	}
 	logging.SetupDefault(opts, "")
 	logger := slog.Default()
