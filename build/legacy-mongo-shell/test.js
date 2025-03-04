038e2cdf7f6096fbfa28838ab5e2c6cfffc293fe
Patryk Kwiatek <patryk.kwiatek@ferretdb.io>
2025-03-04 16:17:54 +0100

    wip

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -5,24 +5,14 @@
 
   // Update the following example with your test.
 
-  const coll = db.test;
+	const res = db.runCommand({
+		listDatabases: 1,
+		filter: {"filter":{name:"nonexistent"}},
+	});
 
-  coll.drop();
 
-  const init = [
-    { _id: "double", v: 42.13 },
-    { _id: "double-whole", v: 42.0 },
-    { _id: "double-zero", v: 0.0 },
-  ];
-
-  coll.insertMany(init);
-
-  const query = { v: { $gt: 42.0 } };
-
-  const expected = [{ _id: "double", v: 42.13 }];
-
-  const actual = coll.find(query).toArray();
-  assert.eq(expected, actual);
+  assert.eq([], res.databases);
+  assert.eq(0,res.totalSize);
 
   print("test.js passed!");
 })();
