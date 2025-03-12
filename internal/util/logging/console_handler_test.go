b933f338f0eee3afd7daec4175da3d061936d622
Patryk Kwiatek <patryk.kwiatek@ferretdb.io>
2025-03-12 14:51:11 +0100

    cleanup testcases

--- internal/util/logging/console_handler_test.go
+++ internal/util/logging/console_handler_test.go
@@ -64,73 +64,41 @@ func TestConsoleHandlerEscapeCodes(t *testing.T) {
 		expected string
 		level    slog.Level
 	}{
-		"Debug-2": {
-			level:    slog.LevelDebug - 2,
-			expected: "\033[34mDEBUG-2\033[0m\tfoobar\n",
-		},
 		"Debug-1": {
 			level:    slog.LevelDebug - 1,
-			expected: "\033[34mDEBUG-1\033[0m\tfoobar\n",
+			expected: "\033[34m" + "DEBUG-1" + "\033[0m" + "\tfoobar\n",
 		},
 		"Debug": {
 			level:    slog.LevelDebug,
-			expected: "\033[34mDEBUG\033[0m\tfoobar\n",
+			expected: "\033[34m" + "DEBUG" + "\033[0m" + "\tfoobar\n",
 		},
 		"Debug+1": {
 			level:    slog.LevelDebug + 1,
-			expected: "\033[34mDEBUG+1\033[0m\tfoobar\n",
-		},
-		"Debug+2": {
-			level:    slog.LevelDebug + 2,
-			expected: "\033[34mDEBUG+2\033[0m\tfoobar\n",
+			expected: "\033[34m" + "DEBUG+1" + "\033[0m" + "\tfoobar\n",
 		},
 		"Info": {
 			level:    slog.LevelInfo,
-			expected: "\033[32mINFO\033[0m\tfoobar\n",
+			expected: "\033[32m" + "INFO" + "\033[0m" + "\tfoobar\n",
 		},
 		"Info+1": {
 			level:    slog.LevelInfo + 1,
-			expected: "\033[32mINFO+1\033[0m\tfoobar\n",
-		},
-		"Info+2": {
-			level:    slog.LevelInfo + 2,
-			expected: "\033[32mINFO+2\033[0m\tfoobar\n",
-		},
-		"Info+3": {
-			level:    slog.LevelInfo + 3,
-			expected: "\033[32mINFO+3\033[0m\tfoobar\n",
+			expected: "\033[32m" + "INFO+1" + "\033[0m" + "\tfoobar\n",
 		},
 		"Warn": {
 			level:    slog.LevelWarn,
-			expected: "\033[33mWARN\033[0m\tfoobar\n",
+			expected: "\033[33m" + "WARN" + "\033[0m" + "\tfoobar\n",
 		},
 		"Warn+1": {
 			level:    slog.LevelWarn + 1,
-			expected: "\033[33mWARN+1\033[0m\tfoobar\n",
-		},
-		"Warn+2": {
-			level:    slog.LevelWarn + 2,
-			expected: "\033[33mWARN+2\033[0m\tfoobar\n",
-		},
-		"Warn+3": {
-			level:    slog.LevelWarn + 3,
-			expected: "\033[33mWARN+3\033[0m\tfoobar\n",
+			expected: "\033[33m" + "WARN+1" + "\033[0m" + "\tfoobar\n",
 		},
 		"Error": {
 			level:    slog.LevelError,
-			expected: "\033[31mERROR\033[0m\tfoobar\n",
+			expected: "\033[31m" + "ERROR" + "\033[0m" + "\tfoobar\n",
 		},
 		"DPanic": {
 			level:    LevelDPanic,
-			expected: "\033[31mERROR+1\033[0m\tfoobar\n",
-		},
-		"Panic": {
-			level:    LevelPanic,
-			expected: "\033[31mERROR+2\033[0m\tfoobar\n",
-		},
-		"Fatal": {
-			level:    LevelFatal,
-			expected: "\033[31mERROR+3\033[0m\tfoobar\n",
+			expected: "\033[31m" + "ERROR+1" + "\033[0m" + "\tfoobar\n",
 		},
 	} {
 		t.Run(name, func(t *testing.T) {
