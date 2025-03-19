21f327036a6e7da04505f013ee100725a8f80de9
Alexey Palazhchenko <alexey.palazhchenko@ferretdb.io>
2025-03-19 14:56:45 +0400

    Add test script for `maxTimeMS`

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -9,20 +9,20 @@
 
   coll.drop();
 
-  const init = [
-    { _id: "double", v: 42.13 },
-    { _id: "double-whole", v: 42.0 },
-    { _id: "double-zero", v: 0.0 },
-  ];
+  const init = [{ _id: "array", v: [42.0] }];
 
   coll.insertMany(init);
 
-  const query = { v: { $gt: 42.0 } };
+  const failedTests = {};
 
-  const expected = [{ _id: "double", v: 42.13 }];
+  try {
+    const res = db.runCommand({ findAndModify: "test", maxTimeMS: "string" });
+    assert.commandFailedWithCode(res, 2);
+  } catch (e) {
+    failedTests["WrongMaxTimeMS"] = e;
+  }
 
-  const actual = coll.find(query).toArray();
-  assert.eq(expected, actual);
+  assert.eq(failedTests, {}, "tests failed");
 
   print("test.js passed!");
 })();
