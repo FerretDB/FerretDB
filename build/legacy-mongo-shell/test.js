e5403586cea9cf7d74e61174781cb1bbb8e6aaaa
Chi Fujii <chi.fujii@ferretdb.io>
2025-03-25 12:00:31 +0900

    add js test for maxtimeMS zero value

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -3,26 +3,8 @@
 (function () {
   "use strict";
 
-  // Update the following example with your test.
-
-  const coll = db.test;
-
-  coll.drop();
-
-  const init = [
-    { _id: "double", v: 42.13 },
-    { _id: "double-whole", v: 42.0 },
-    { _id: "double-zero", v: 0.0 },
-  ];
-
-  coll.insertMany(init);
-
-  const query = { v: { $gt: 42.0 } };
-
-  const expected = [{ _id: "double", v: 42.13 }];
-
-  const actual = coll.find(query).toArray();
-  assert.eq(expected, actual);
+  const actual = db.runCommand({ find: "test", "maxTimeMS": 0});
+  assert.commandWorked(actual);
 
   print("test.js passed!");
 })();
