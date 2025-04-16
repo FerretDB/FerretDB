dab77aa28dc022616560ff7b62f16f0a2dcd6d8f
Alexey Palazhchenko <alexey.palazhchenko@ferretdb.io>
2025-04-16 14:33:39 +0400

    Improve checking

--- build/ferretdb/evaluation/entrypoint.sh
+++ build/ferretdb/evaluation/entrypoint.sh
@@ -2,8 +2,15 @@
 
 set -e
 
-test -n "$POSTGRES_PASSWORD" || (echo "POSTGRES_PASSWORD must be set" && false)
-test "${POSTGRES_DB:-postgres}" = "postgres" || (echo "POSTGRES_DB must be set to 'postgres' or unset" && false)
+if [ -z "$POSTGRES_PASSWORD" ]; then
+    echo "Error: POSTGRES_PASSWORD must be set"
+    exit 1
+fi
+
+if [ "${POSTGRES_DB:-postgres}" != "postgres" ]; then
+    echo "Error: POSTGRES_DB must be set to 'postgres' or unset"
+    exit 1
+fi
 
 export FERRETDB_POSTGRESQL_URL="postgresql://${POSTGRES_USER:-postgres}:${POSTGRES_PASSWORD}@127.0.0.1:5432/postgres"
 
