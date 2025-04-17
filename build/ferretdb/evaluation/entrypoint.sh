e3352e63ac40aaa30a0473e85cc0fbfd7046e725
Chi Fujii <chi.fujii@ferretdb.io>
2025-04-17 13:24:39 +0900

    explicitly set postgresql db

--- build/ferretdb/evaluation/entrypoint.sh
+++ build/ferretdb/evaluation/entrypoint.sh
@@ -12,6 +12,10 @@ if [ "${POSTGRES_DB:-postgres}" != "postgres" ]; then
     exit 1
 fi
 
+# explicitly set POSTGRES_DB, because if POSTGRES_DB is unset it uses POSTGRES_USER value
+# see https://hub.docker.com/_/postgres
+export POSTGRES_DB="postgres"
+
 export FERRETDB_POSTGRESQL_URL="postgresql://${POSTGRES_USER:-postgres}:${POSTGRES_PASSWORD}@127.0.0.1:5432/postgres"
 
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
