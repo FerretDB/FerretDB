ecf10a8f57a7e2d967addeb65cd0b780dd4176e1
Alexey Palazhchenko <alexey.palazhchenko@ferretdb.io>
2025-04-21 22:40:02 +0400

    Update

--- internal/handler/msg_findandmodify.go
+++ internal/handler/msg_findandmodify.go
@@ -31,13 +31,12 @@ import (
 //
 // The passed context is canceled when the client connection is closed.
 func (h *Handler) MsgFindAndModify(connCtx context.Context, req *middleware.Request) (*middleware.Response, error) {
-	spec, err := req.OpMsg.RawDocument()
+	spec, err := req.OpMsg.DocumentRaw()
 	if err != nil {
 		return nil, lazyerrors.Error(err)
 	}
 
-	// TODO https://github.com/FerretDB/FerretDB-DocumentDB/issues/78
-	doc, err := spec.Decode()
+	doc, err := req.OpMsg.Document()
 	if err != nil {
 		return nil, lazyerrors.Error(err)
 	}
