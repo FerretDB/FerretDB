0e1446a4a07fe411a068a72311dbc524d79aa452
Patryk Kwiatek <patryk.kwiatek@ferretdb.io>
2025-04-21 15:49:00 +0200

    wip

---
+++ website/docs/guides/full-text-search/3-basic-text-search.response.js
@@ -0,0 +1,16 @@
+response = {
+  cursor: {
+    firstBatch: [
+      {
+        _id: 'pride_prejudice_1813',
+        title: 'Pride and Prejudice',
+        author: 'Jane Austen',
+        summary:
+          'The novel follows the story of Elizabeth Bennet, a spirited young woman navigating love, societal expectations, and family drama in 19th-century England.'
+      }
+    ],
+    id: Long(0),
+    ns: 'db.books'
+  },
+  ok: Double(1)
+}
---
+++ website/docs/guides/ttl-indexes/1-create-ttl-index.response.js
@@ -0,0 +1,1 @@
+response = { numIndexesBefore: 1, numIndexesAfter: 2, createdCollectionAutomatically: true, ok: Double(1) }
