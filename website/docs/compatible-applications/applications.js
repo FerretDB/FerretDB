0938f96f0ee1164b3ef6b3851a06a553e6c979ec
Alexander Tobi Fashakin <alexander.fashakin@ferretdb.io>
2025-04-25 10:55:03 -0400

    wip

---
+++ website/docs/compatible-applications/applications.js
@@ -0,0 +1,169 @@
+export const categorizedApps = {
+  'GUI / Client Tools': [
+    {
+      name: 'Mingo',
+      logo: '/img/integrations/mingo.png',
+      link: 'https://blog.ferretdb.io/using-mingo-analyze-visualize-ferretdb-data/'
+    },
+    {
+      name: 'Compass',
+      logo: '/img/integrations/abstract-logo.png',
+      link: 'https://docs.ferretdb.io/usage/integrations/overview/#gui-applications'
+    },
+    {
+      name: 'NoSQLBooster',
+      logo: '/img/integrations/abstract-logo.png',
+      link: 'https://blog.ferretdb.io/mongodb-gui-using-ferretdb-nosqlbooster/'
+    },
+    {
+      name: 'Studio 3T',
+      logo: '/img/integrations/abstract-logo.png',
+      link: 'https://blog.ferretdb.io/using-ferretdb-with-studio-3t/'
+    },
+    {
+      name: 'PgManage',
+      logo: '/img/integrations/abstract-logo.png',
+      link: 'https://blog.ferretdb.io/pgmanage-postgresql-gui-admin-ferretdb/'
+    },
+    {
+      name: 'MajorM',
+      logo: '/img/integrations/abstract-logo.png',
+      link: 'https://blog.ferretdb.io/query-filter-ferretdb-with-majorm/'
+    }
+  ],
+  'Deployment Platforms': [
+    {
+      name: 'Acorn',
+      logo: '/img/integrations/acorn.svg',
+      link: 'https://blog.ferretdb.io/how-to-package-and-deploy-ferretdb-with-acorn/'
+    },
+    {
+      name: 'Elestio',
+      logo: '/img/integrations/elestio.svg',
+      link: 'https://blog.ferretdb.io/easily-deploy-managed-ferretdb-elestio/'
+    },
+    { name: 'LeafCloud', logo: '/img/integrations/leafcloud.svg', link: '#' },
+    { name: 'Vultr', logo: '/img/integrations/vultr.svg', link: 'https://blog.ferretdb.io/deploy-ferretdb-on-vultr/' },
+    { name: 'Taikun CloudWorks', logo: '/img/integrations/taikun.svg', link: '#' },
+    {
+      name: 'KubeDB',
+      logo: '/img/integrations/kubedb.svg',
+      link: 'https://blog.ferretdb.io/deploy-ferretdb-in-kubernetes-using-kubedb-managed-postgres/'
+    },
+    {
+      name: 'Tembo',
+      logo: '/img/integrations/tembo.svg',
+      link: 'https://blog.ferretdb.io/run-ferretdb-on-tembo-managed-postgres/'
+    },
+    {
+      name: 'Aiven for PostgreSQL',
+      logo: '/img/integrations/aiven.svg',
+      link: 'https://blog.ferretdb.io/run-mongodb-workloads-aiven-postgresql/'
+    },
+    { name: 'Ubicloud Managed Postgres', logo: '/img/integrations/ubicould.svg', link: '#' }
+  ],
+  'Database Backends': [
+    {
+      name: 'StackGres',
+      logo: '/img/integrations/stackgres.svg',
+      link: 'https://blog.ferretdb.io/deploy-ferretdb-stackgres/'
+    },
+    {
+      name: 'CloudNativePG',
+      logo: '/img/integrations/cloudnativepg.svg',
+      link: 'https://blog.ferretdb.io/run-ferretdb-cloudnativepg-kubernetes/'
+    },
+    {
+      name: 'CyberTec Postgres',
+      logo: '/img/integrations/cybertec.svg',
+      link: 'https://blog.ferretdb.io/run-ferretdb-cybertec-postgres-kubernetes/'
+    },
+    {
+      name: 'Percona Distribution for PostgreSQL',
+      logo: '/img/integrations/percona.svg',
+      link: 'https://blog.ferretdb.io/run-ferretdb-percona-postgres-operator/'
+    },
+    {
+      name: 'Neon',
+      logo: '/img/integrations/neon.svg',
+      link: 'https://blog.ferretdb.io/running-ferretdb-on-neon-serverless-postgres/'
+    },
+    {
+      name: 'SQLite',
+      logo: '/img/integrations/sqlite.svg',
+      link: 'https://blog.ferretdb.io/how-ferretdb-supports-sqlite/'
+    },
+    { name: 'PGEdge', logo: '/img/integrations/pgedge.svg', link: '#' },
+    { name: 'Yugabyte', logo: '/img/integrations/yugabyte.svg', link: '#' }
+  ],
+  'Monitoring / Observability': [
+    {
+      name: 'Grafana',
+      logo: '/img/integrations/grafana.svg',
+      link: 'https://blog.ferretdb.io/grafana-monitoring-ferretdb/'
+    },
+    {
+      name: 'Coroot',
+      logo: '/img/integrations/coroot.svg',
+      link: 'https://blog.ferretdb.io/monitor-ferretdb-performance-using-coroot/'
+    },
+    {
+      name: 'Prometheus',
+      logo: '/img/integrations/prometheus.svg',
+      link: 'https://blog.ferretdb.io/prometheus-monitoring-ferretdb/'
+    },
+    {
+      name: 'OpenTelemetry',
+      logo: '/img/integrations/opentelemetry.svg',
+      link: 'https://blog.ferretdb.io/observability-ferretdb-opentelemetry/'
+    }
+  ],
+  'Data Migration': [
+    {
+      name: 'dsync',
+      logo: '/img/integrations/dsync.svg',
+      link: 'https://blog.ferretdb.io/migrate-mongodb-data-ferretdb-dsync/'
+    }
+  ],
+  'Authentication / Security': [
+    {
+      name: 'OpenZiti',
+      logo: '/img/integrations/openziti.svg',
+      link: 'https://blog.ferretdb.io/run-ferretdb-securely-using-openziti/'
+    },
+    {
+      name: 'CLA Assistant',
+      logo: '/img/integrations/cla-assistant.svg',
+      link: 'https://blog.ferretdb.io/using-cla-assistant-with-ferretdb/'
+    }
+  ],
+  'Frameworks / SDKs': [
+    {
+      name: 'Meteor.js',
+      logo: '/img/integrations/meteor.svg',
+      link: 'https://blog.ferretdb.io/ferretdb-meteor-mongodb-alternative/'
+    }
+  ],
+  Other: [
+    {
+      name: 'Docker',
+      logo: '/img/integrations/docker.svg',
+      link: 'https://blog.ferretdb.io/how-to-start-ferretdb-locally-with-docker/'
+    },
+    {
+      name: 'Tigris Data',
+      logo: '/img/integrations/tigris.svg',
+      link: 'https://blog.ferretdb.io/compare-ferretdb-tigris/'
+    },
+    {
+      name: 'Illa Cloud',
+      logo: '/img/integrations/illa-cloud.svg',
+      link: 'https://blog.ferretdb.io/build-internal-apps-illa-cloud-ferretdb/'
+    },
+    {
+      name: 'FastNetMon',
+      logo: '/img/integrations/fastnetmon.svg',
+      link: 'https://blog.ferretdb.io/fastnetmon-picked-ferretdb-saas-offering/'
+    }
+  ]
+}
