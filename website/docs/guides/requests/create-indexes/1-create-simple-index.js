3a6dc1a9580dfa5d100d9f8fec4604da8cb37981
Alexander Tobi Fashakin <alexander.fashakin@ferretdb.io>
2025-04-28 19:15:30 -0400

    update

--- website/docs/guides/requests/create-indexes/1-create-simple-index.js
+++
@@ -1,1 +0,0 @@
-db.runCommand({ createIndexes: 'books', indexes: [{ key: { title: 1 }, name: 'title_index' }] })
--- website/docs/guides/requests/create-indexes/2-create-unique-index.js
+++
@@ -1,1 +0,0 @@
-db.runCommand({ createIndexes: 'books', indexes: [{ key: { isbn: 1 }, name: 'unique_isbn_idx', unique: true }] })
