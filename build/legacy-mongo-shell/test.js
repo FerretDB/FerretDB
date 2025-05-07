fb186fe2ebcc6df425d616e90cb98641b0236683
Patryk Kwiatek <patryk.kwiatek@ferretdb.io>
2025-05-07 14:38:04 +0200

    wip

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -9,19 +9,11 @@
 
   coll.drop();
 
-  const init = [
-    { _id: "double", v: 42.13 },
-    { _id: "double-whole", v: 42.0 },
-    { _id: "double-zero", v: 0.0 },
-  ];
+  coll.insertOne({ _id: "timestamp-zero", v: Timestamp(0,0)});
 
-  coll.insertMany(init);
+  const expected = [{ _id: "timestamp-zero", v: Timestamp(0,0)}];
 
-  const query = { v: { $gt: 42.0 } };
-
-  const expected = [{ _id: "double", v: 42.13 }];
-
-  const actual = coll.find(query).toArray();
+  const actual = coll.find({}).toArray();
   assert.eq(expected, actual);
 
   print("test.js passed!");
