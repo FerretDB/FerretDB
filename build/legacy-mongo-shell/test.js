083b8eefa57179a08c4b7a9bed0b9a71ba5a84c0
Patryk Kwiatek <patryk.kwiatek@ferretdb.io>
2025-05-12 13:24:34 +0200

    wip

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -3,23 +3,22 @@
 (function () {
   "use strict";
 
-  // Update the following example with your test.
-
   const coll = db.test;
 
   coll.drop();
 
   const init = [
-    { _id: "double", v: 42.13 },
-    { _id: "double-whole", v: 42.0 },
-    { _id: "double-zero", v: 0.0 },
+    { _id: "binary", v: { $binary: { base64: "KgAN", subType: "80" } } },
+    { _id: "decimal128", v: { $numberDecimal: "42.13" } }
   ];
 
   coll.insertMany(init);
 
-  const query = { v: { $gt: 42.0 } };
+  const query = { v: { $bitsAnySet: 6 } };
 
-  const expected = [{ _id: "double", v: 42.13 }];
+  const expected = [
+    { _id: "binary", v: { $binary: { base64: "KgAN", subType: "80" } } }
+  ];
 
   const actual = coll.find(query).toArray();
   assert.eq(expected, actual);
