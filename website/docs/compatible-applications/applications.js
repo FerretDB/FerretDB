c96d4c3ad0945e8f8f55d6afe07e7554dfde49ca
Alexander Tobi Fashakin <alexander.fashakin@ferretdb.io>
2025-05-22 10:02:55 -0400

    update

--- website/docs/compatible-applications/applications.js
+++ website/docs/compatible-applications/applications.js
@@ -7,48 +7,53 @@ export const categorizedApps = {
     },
     {
       name: 'NoSQLBooster',
-      logo: '/img/integrations/abstract-logo.png',
+      logo: '/img/integrations/nosqlbooster-logo.png',
       link: 'https://blog.ferretdb.io/mongodb-gui-using-ferretdb-nosqlbooster/'
     },
     {
       name: 'Studio 3T',
-      logo: '/img/integrations/abstract-logo.png',
+      logo: '/img/integrations/studio3t-logo.png',
       link: 'https://blog.ferretdb.io/using-ferretdb-with-studio-3t/'
     },
     {
       name: 'MajorM',
-      logo: '/img/integrations/abstract-logo.png',
+      logo: '/img/integrations/majorm-logo.jpg',
       link: 'https://blog.ferretdb.io/query-filter-ferretdb-with-majorm/'
-    }
+    },
+    {
+      name: 'Mingo',
+      logo: '/img/integrations/mingo-logo.png',
+      link: 'https://blog.ferretdb.io/using-mingo-analyze-visualize-ferretdb-data/'
+    },
   ],
   'Deployment Platforms': [
     {
       name: 'Taikun CloudWorks',
-      logo: '/img/integrations/abstract-logo.png',
+      logo: '/img/integrations/taikun-logo.png',
       link: 'https://blog.ferretdb.io/run-mongodb-workloads-ferretdb-taikun-cloudworks/'
     }
   ],
   'Database Backends': [
     {
       name: 'CloudNativePG',
-      logo: '/img/integrations/abstract-logo.png',
+      logo: '/img/integrations/cnpg-logo.png',
       link: 'https://blog.ferretdb.io/run-ferretdb-cloudnativepg-kubernetes/'
     }
   ],
   'Monitoring / Observability': [
     {
       name: 'Grafana',
-      logo: '/img/integrations/abstract-logo.png',
+      logo: '/img/integrations/grafana-logo.png',
       link: 'https://blog.ferretdb.io/grafana-monitoring-ferretdb/'
     },
     {
       name: 'Coroot',
-      logo: '/img/integrations/abstract-logo.png',
+      logo: '/img/integrations/coroot-logo.png',
       link: 'https://blog.ferretdb.io/monitor-ferretdb-performance-using-coroot/'
     },
     {
       name: 'OpenTelemetry',
-      logo: '/img/integrations/abstract-logo.png',
+      logo: '/img/integrations/opentelemetry-logo.png',
       link: 'https://blog.ferretdb.io/otel-context-propagation-in-ferretdb/'
     }
   ],
