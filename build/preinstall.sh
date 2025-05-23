a5dad921215364a5f56399c8d1e1553cbdc8e7b4
andygoblins <andygoblins@gmx.com>
2025-05-23 10:06:26 -0500

    Fix useradd script
    adduser isn't compatible across deb/rpm distros

--- build/preinstall.sh
+++ build/preinstall.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
-adduser --system --no-create-home --group --disabled-password --quiet ferretdb
+# add ferretdb user/group (if it doesn't already exist)
+err_already_exists=9
+/usr/sbin/useradd --system --home-dir /nonexistent --no-create-home --shell /usr/sbin/nologin --user-group ferretdb
+err=$?
+if [ $err -ne 0 ] && [ $err -ne $err_already_exists ]; then
+	exit $err
+fi
