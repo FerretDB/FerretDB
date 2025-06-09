afb07f22449d8b93befa5cd8e231437cc53023c6
singerslew <singerslew@lynntech.com>
2025-06-09 12:29:32 -0500

    Repro lookup + unwind

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -10,18 +10,95 @@
   coll.drop();
 
   const init = [
-    { _id: "double", v: 42.13 },
-    { _id: "double-whole", v: 42.0 },
-    { _id: "double-zero", v: 0.0 },
+    {
+      _id: ObjectId('684211003840ee692afcbc1c'),
+      meta: {
+        annotations: [
+          {
+            label_id: '9b260dd55ab61a7600000000',
+          }
+        ]
+      },
+      data: 'dahta'
+    },
+    {
+      _id: ObjectId('684211003840ee692afcbc1d'),
+      meta: {
+        annotations: [
+          {
+            label_id: '9b260dd55ab61a7600000000',
+          }
+        ]
+      },
+      data: 'data'
+    }
   ];
 
   coll.insertMany(init);
 
-  const query = { v: { $gt: 42.0 } };
+  const query = [
+    {
+      "$group": {
+        "_id": "$meta.annotations.label_id"
+      }
+    },
+    {
+      "$lookup": {
+        "from": "collection",
+        "let": {
+          "class": "$_id"
+        },
+        "pipeline": [
+          {
+            "$match": {
+              "$expr": {
+                "$eq": [
+                  "$meta.annotations.label_id",
+                  "$$class"
+                ]
+              }
+            }
+          },
+          {
+            "$limit": 4
+          },
+          {
+            "$project": {
+              "_id": 1,
+              "data": "$data"
+            }
+          }
+        ],
+        "as": "instance"
+      }
+    },
+    {
+      "$unwind": "$instance"
+    }
+  ];
 
-  const expected = [{ _id: "double", v: 42.13 }];
+  const expected = [
+    {
+      "_id": [
+        "9b260dd55ab61a7600000000"
+      ],
+      "instance": {
+        "_id": ObjectId("684211003840ee692afcbc1c"),
+        "data": "dahta"
+      }
+    },
+    {
+      "_id": [
+        "9b260dd55ab61a7600000000"
+      ],
+      "instance": {
+        "_id": ObjectId("684211003840ee692afcbc1d"),
+        "data": "data"
+      }
+    }
+  ];
 
-  const actual = coll.find(query).toArray();
+  const actual = coll.aggregate(query).toArray();
   assert.eq(expected, actual);
 
   print("test.js passed!");
