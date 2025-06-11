86d55eff68d05f7f93b1e2fd72ad73eeefd09894
Alexey Palazhchenko <alexey.palazhchenko@ferretdb.io>
2025-06-11 14:02:25 +0400

    Move images

--- website/docs/compatible-applications/applications.js
+++ website/docs/compatible-applications/applications.js
@@ -2,72 +2,73 @@ export const categorizedApps = {
   'GUI Client Tools': [
     // Add compass blog post [https://github.com/FerretDB/FerretDB/issues/1982]
     //   name: 'Compass',
-    //   logo: '/img/integrations/compass-logo.png',
-    //   link: 'https://docs.ferretdb.io/usage/integrations/overview/#gui-applications'
+    //   logo: '/img/compatible-applications/compass-logo.png',
+    //   link: 'https://docs.ferretdb.io/usage/compatible-applications/overview/#gui-applications'
     // },
+
     {
       name: 'NoSQLBooster',
-      logo: '/img/integrations/nosqlbooster-logo.png',
+      logo: '/img/compatible-applications/nosqlbooster-logo.png',
       link: 'https://blog.ferretdb.io/mongodb-gui-using-ferretdb-nosqlbooster/'
     },
     {
       name: 'Studio 3T',
-      logo: '/img/integrations/studio3t-logo.png',
+      logo: '/img/compatible-applications/studio3t-logo.png',
       link: 'https://blog.ferretdb.io/using-ferretdb-with-studio-3t/'
     },
     {
       name: 'MajorM',
-      logo: '/img/integrations/majorm-logo.png',
+      logo: '/img/compatible-applications/majorm-logo.png',
       link: 'https://blog.ferretdb.io/query-filter-ferretdb-with-majorm/'
     },
     {
       name: 'Mingo',
-      logo: '/img/integrations/mingo-logo.png',
+      logo: '/img/compatible-applications/mingo-logo.png',
       link: 'https://blog.ferretdb.io/using-mingo-analyze-visualize-ferretdb-data/'
     }
   ],
   'Deployment Platforms': [
     {
       name: 'Taikun CloudWorks',
-      logo: '/img/integrations/taikun-logo.png',
+      logo: '/img/compatible-applications/taikun-logo.png',
       link: 'https://blog.ferretdb.io/run-mongodb-workloads-ferretdb-taikun-cloudworks/'
     }
   ],
   'PostgreSQL Operators': [
     {
       name: 'CloudNativePG',
-      logo: '/img/integrations/cnpg-logo.png',
+      logo: '/img/compatible-applications/cnpg-logo.png',
       link: 'https://blog.ferretdb.io/run-ferretdb-postgres-documentdb-extension-cnpg-kubernetes/'
     }
   ],
   'Monitoring and Observability': [
     {
       name: 'Grafana',
-      logo: '/img/integrations/grafana-logo.png',
+      logo: '/img/compatible-applications/grafana-logo.png',
       link: 'https://blog.ferretdb.io/grafana-monitoring-ferretdb/'
     },
     {
       name: 'Coroot',
-      logo: '/img/integrations/coroot-logo.png',
+      logo: '/img/compatible-applications/coroot-logo.png',
       link: 'https://blog.ferretdb.io/monitor-ferretdb-performance-using-coroot/'
     },
     {
       name: 'OpenTelemetry',
-      logo: '/img/integrations/opentelemetry-logo.png',
+      logo: '/img/compatible-applications/opentelemetry-logo.png',
       link: 'https://blog.ferretdb.io/otel-context-propagation-in-ferretdb/'
     }
   ],
   'Data Migration': [
     {
       name: 'dsync',
-      logo: '/img/integrations/adiom-logo.png',
+      logo: '/img/compatible-applications/adiom-logo.png',
       link: 'https://blog.ferretdb.io/migrate-mongodb-data-ferretdb-dsync/'
     }
   ],
   Others: [
     {
       name: 'Novu',
-      logo: '/img/integrations/novu-logo.png',
+      logo: '/img/compatible-applications/novu-logo.png',
       link: 'https://blog.ferretdb.io/powering-notification-infrastructure-novu-ferretdb/'
     }
   ]
