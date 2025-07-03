a07a9f59c71683548a97d77f8addc87b8573b322
Chi Fujii <chi.fujii@ferretdb.io>
2025-07-03 16:52:12 +0900

    remove test for now

--- internal/dataapi/listener_test.go
+++ internal/dataapi/listener_test.go
@@ -303,63 +303,6 @@ func TestBasicAuthDoesNotPersist(t *testing.T) {
 	})
 }
 
-func TestBearerToken(t *testing.T) {
-	t.Parallel()
-
-	addr, db := setupDataAPI(t, true)
-	coll := testutil.CollectionName(t)
-	ctx := testutil.Ctx(t)
-
-	findURI := "http://" + addr + "/action/find"
-	findReq := bytes.NewBuffer([]byte(
-		`{
-			"database": "` + db + `",
-			"collection": "` + coll + `",
-			"filter": {}
-		}`,
-	))
-	bearerToken := "BEARER_TOKEN"
-
-	t.Run("BearerToken", func(t *testing.T) {
-		req, err := http.NewRequestWithContext(ctx, http.MethodPost, findURI, findReq)
-		require.NoError(t, err)
-
-		req.Header.Set("Content-Type", "application/json")
-		req.Header.Add("Authorization", "Bearer "+bearerToken)
-
-		res, err := http.DefaultClient.Do(req)
-		require.NoError(t, err)
-		t.Cleanup(func() {
-			require.NoError(t, res.Body.Close())
-		})
-
-		assert.Equal(t, http.StatusOK, res.StatusCode)
-
-		body, err := io.ReadAll(res.Body)
-		require.NoError(t, err)
-		assert.JSONEq(t, `{"documents":[]}`, string(body))
-	})
-
-	t.Run("BearerTokenPersists", func(t *testing.T) {
-		req, err := http.NewRequestWithContext(ctx, http.MethodPost, findURI, findReq)
-		require.NoError(t, err)
-
-		req.Header.Set("Content-Type", "application/json")
-
-		res, err := http.DefaultClient.Do(req)
-		require.NoError(t, err)
-		t.Cleanup(func() {
-			require.NoError(t, res.Body.Close())
-		})
-
-		assert.Equal(t, http.StatusOK, res.StatusCode)
-
-		body, err := io.ReadAll(res.Body)
-		require.NoError(t, err)
-		assert.JSONEq(t, `{"documents":[]}`, string(body))
-	})
-}
-
 // postJSON sends POST request with provided JSON to data API under provided uri.
 // It handles necessary headers, as well as authentication.
 func postJSON(tb testing.TB, uri, jsonBody string) (*http.Response, error) {
