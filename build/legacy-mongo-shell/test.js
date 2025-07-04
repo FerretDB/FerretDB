45ad4bd0b1534c21ce5c78fbf7c3f0d16205e278
Chi Fujii <chi.fujii@ferretdb.io>
2025-07-04 11:33:31 +0900

    fmt

--- build/legacy-mongo-shell/test.js
+++ build/legacy-mongo-shell/test.js
@@ -3,9 +3,9 @@
 (function () {
   "use strict";
 
-  const res = db.runCommand({dropUser: "non-existent-user"})
+  const res = db.runCommand({ dropUser: "non-existent-user" });
 
-  assert.commandFailedWithCode(res, 11)
+  assert.commandFailedWithCode(res, 11);
 
   print("test.js passed!");
 })();
