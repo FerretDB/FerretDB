9e6677403ac64070edbe87dd4ce6fa2d1c8e7255
Chi Fujii <chi.fujii@ferretdb.io>
2025-07-07 14:58:22 +0900

    separate different auth

--- internal/dataapi/server/server.go
+++ internal/dataapi/server/server.go
@@ -54,123 +54,153 @@ type Server struct {
 	m sync.Map
 }
 
-// AuthMiddleware authenticates request using bearer token authentication and basic authentication.
-// If bearer token is provided, the validity of the token is checked to authenticate the request.
-// If basic authentication is provided, it performs a SCRAM-SHA-256 handshake,
-// and generates a bearer token, and returns it in the response header.
+// AuthMiddleware authenticates the request using bearer token or basic authentication.
+// If bearer token is provided, it authenticates using bearer token.
+// Otherwise, basic auth is used and upon successful authentication
+// it sets a bearer token in the response header.
 // After a successful authentication, it calls the next handler.
 func (s *Server) AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ctx := r.Context()
-
-		auth := r.Header.Get("Authorization")
-		if bearer := strings.HasPrefix(auth, "Bearer "); bearer {
-			bearerToken := strings.TrimPrefix(auth, "Bearer ")
-
-			if _, ok := s.m.Load(bearerToken); !ok {
-				http.Error(w, "token is not valid", http.StatusUnauthorized)
+		if bearer := strings.HasPrefix(r.Header.Get("Authorization"), "Bearer "); bearer {
+			if ok := s.bearerAuth(w, r); !ok {
 				return
 			}
 
 			next.ServeHTTP(w, r)
-
 			return
 		}
 
-		username, password, ok := r.BasicAuth()
-
-		if !ok {
-			writeError(w, errorNoAuthenticationSpecified, http.StatusBadRequest)
+		if ok := s.basicAuth(w, r); !ok {
 			return
 		}
 
-		if password == "" || username == "" {
-			writeError(w, errorMissingAuthenticationParameter, http.StatusBadRequest)
+		if err := s.setBearerTokenHeader(w); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
-		client, err := scram.SHA256.NewClient(username, password, "")
-		if err != nil {
-			http.Error(w, lazyerrors.Error(err).Error(), http.StatusBadRequest)
-			return
-		}
-
-		conv := client.NewConversation()
-
-		payload, err := conv.Step("")
-		if err != nil {
-			http.Error(w, lazyerrors.Error(err).Error(), http.StatusBadRequest)
-			return
-		}
-
-		msg := must.NotFail(prepareRequest(
-			"saslStart", int32(1),
-			"mechanism", "SCRAM-SHA-256",
-			"payload", wirebson.Binary{B: []byte(payload)},
-			// use skipEmptyExchange to complete the handshake with one `saslStart` and one `saslContinue`
-			"options", wirebson.MustDocument("skipEmptyExchange", true),
-			"$db", "admin",
-		))
-
-		res, err := s.handler.Handle(ctx, msg)
-		if err != nil {
-			http.Error(w, lazyerrors.Error(err).Error(), http.StatusUnauthorized)
-			return
-		}
-
-		resDoc := must.NotFail(must.NotFail(res.OpMsg.DocumentRaw()).Decode())
-		convID := resDoc.Get("conversationId").(int32)
-
-		payloadBytes := resDoc.Get("payload").(wirebson.Binary).B
-
-		payload, err = conv.Step(string(payloadBytes))
-		if err != nil {
-			http.Error(w, lazyerrors.Error(err).Error(), http.StatusUnauthorized)
-			return
-		}
-
-		msg = must.NotFail(prepareRequest(
-			"saslContinue", int32(1),
-			"conversationId", convID,
-			"payload", wirebson.Binary{B: []byte(payload)},
-			"$db", "admin",
-		))
-
-		res, err = s.handler.Handle(ctx, msg)
-		if err != nil {
-			http.Error(w, lazyerrors.Error(err).Error(), http.StatusUnauthorized)
-			return
-		}
-
-		resDoc = must.NotFail(must.NotFail(res.OpMsg.DocumentRaw()).Decode())
-		if !resDoc.Get("done").(bool) {
-			http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
-			return
-		}
-
-		payloadBytes = resDoc.Get("payload").(wirebson.Binary).B
-
-		if _, err = conv.Step(string(payloadBytes)); err != nil {
-			http.Error(w, lazyerrors.Error(err).Error(), http.StatusUnauthorized)
-			return
-		}
-
-		if !conv.Valid() {
-			http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
-			return
-		}
-
-		b := make([]byte, 32)
-		if _, err = rand.Read(b); err != nil {
-			http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusInternalServerError)
-		}
-
-		token := fmt.Sprintf("%x", b)
-		s.m.Store(token, true)
-		w.Header().Set("Authorization", "Bearer "+token)
-
-		next.ServeHTTP(w, r.WithContext(ctx))
+		next.ServeHTTP(w, r)
 	})
+}
+
+// setBearerTokenHeader generates a new bearer token, stores it,
+// and sets it in the response header.
+func (s *Server) setBearerTokenHeader(w http.ResponseWriter) error {
+	b := make([]byte, 32)
+	if _, err := rand.Read(b); err != nil {
+		return err
+	}
+
+	token := fmt.Sprintf("%x", b)
+	s.m.Store(token, true)
+	w.Header().Set("Authorization", "Bearer "+token)
+
+	return nil
+}
+
+// bearerAuth checks if the request has a valid bearer token.
+// If the token is valid, it returns true. Otherwise, it writes an error response
+// and returns false.
+func (s *Server) bearerAuth(w http.ResponseWriter, r *http.Request) bool {
+	auth := r.Header.Get("Authorization")
+	token := strings.TrimPrefix(auth, "Bearer ")
+
+	if _, ok := s.m.Load(token); !ok {
+		http.Error(w, "token is not valid", http.StatusUnauthorized)
+		return false
+	}
+
+	return true
+}
+
+// basicAuth handles basic authentication using SCRAM-SHA-256 mechanism.
+// It returns true if authentication is successful, otherwise it writes an error response
+// and returns false.
+func (s *Server) basicAuth(w http.ResponseWriter, r *http.Request) bool {
+	username, password, ok := r.BasicAuth()
+
+	if !ok {
+		writeError(w, errorNoAuthenticationSpecified, http.StatusBadRequest)
+		return false
+	}
+
+	if password == "" || username == "" {
+		writeError(w, errorMissingAuthenticationParameter, http.StatusBadRequest)
+		return false
+	}
+
+	client, err := scram.SHA256.NewClient(username, password, "")
+	if err != nil {
+		http.Error(w, lazyerrors.Error(err).Error(), http.StatusBadRequest)
+		return false
+	}
+
+	conv := client.NewConversation()
+
+	payload, err := conv.Step("")
+	if err != nil {
+		http.Error(w, lazyerrors.Error(err).Error(), http.StatusBadRequest)
+		return false
+	}
+
+	msg := must.NotFail(prepareRequest(
+		"saslStart", int32(1),
+		"mechanism", "SCRAM-SHA-256",
+		"payload", wirebson.Binary{B: []byte(payload)},
+		// use skipEmptyExchange to complete the handshake with one `saslStart` and one `saslContinue`
+		"options", wirebson.MustDocument("skipEmptyExchange", true),
+		"$db", "admin",
+	))
+
+	res, err := s.handler.Handle(r.Context(), msg)
+	if err != nil {
+		http.Error(w, lazyerrors.Error(err).Error(), http.StatusUnauthorized)
+		return false
+	}
+
+	resDoc := must.NotFail(must.NotFail(res.OpMsg.DocumentRaw()).Decode())
+	convID := resDoc.Get("conversationId").(int32)
+
+	payloadBytes := resDoc.Get("payload").(wirebson.Binary).B
+
+	payload, err = conv.Step(string(payloadBytes))
+	if err != nil {
+		http.Error(w, lazyerrors.Error(err).Error(), http.StatusUnauthorized)
+		return false
+	}
+
+	msg = must.NotFail(prepareRequest(
+		"saslContinue", int32(1),
+		"conversationId", convID,
+		"payload", wirebson.Binary{B: []byte(payload)},
+		"$db", "admin",
+	))
+
+	res, err = s.handler.Handle(r.Context(), msg)
+	if err != nil {
+		http.Error(w, lazyerrors.Error(err).Error(), http.StatusUnauthorized)
+		return false
+	}
+
+	resDoc = must.NotFail(must.NotFail(res.OpMsg.DocumentRaw()).Decode())
+	if !resDoc.Get("done").(bool) {
+		http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+		return false
+	}
+
+	payloadBytes = resDoc.Get("payload").(wirebson.Binary).B
+
+	if _, err = conv.Step(string(payloadBytes)); err != nil {
+		http.Error(w, lazyerrors.Error(err).Error(), http.StatusUnauthorized)
+		return false
+	}
+
+	if !conv.Valid() {
+		http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+		return false
+	}
+
+	return true
 }
 
 // writeJSONResponse marshals provided res document into extended JSON and
