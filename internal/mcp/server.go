a30bdef9d083eef886a60c784d1cec00922fbac7
Chi Fujii <chi.fujii@ferretdb.io>
2025-07-08 16:13:52 +0900

    update mux path

--- internal/mcp/server.go
+++ internal/mcp/server.go
@@ -52,7 +52,7 @@ func New(ctx context.Context, opts *ServerOpts) (*Server, error) {
 	s := server.NewMCPServer("FerretDB", version.Get().Version)
 
 	for _, t := range opts.ToolHandler.initTools() {
-		s.AddTool(t.tool, withLog(withConnInfo(t.handleFunc), opts.L))
+		s.AddTool(t.tool, withLog(t.handleFunc, opts.L))
 	}
 
 	sseServer := server.NewSSEServer(s,
@@ -71,8 +71,8 @@ func New(ctx context.Context, opts *ServerOpts) (*Server, error) {
 		messageHandler = srv.AuthMiddleware(messageHandler)
 	}
 
-	mux.Handle("/mcp/sse", sseHandler)
-	mux.Handle("/mcp/message", messageHandler)
+	mux.Handle("/sse", sseHandler)
+	mux.Handle("/message", messageHandler)
 
 	httpSrv := &http.Server{
 		Handler:  mux,
@@ -81,7 +81,19 @@ func New(ctx context.Context, opts *ServerOpts) (*Server, error) {
 			return ctx
 		},
 		ConnContext: func(ctx context.Context, c net.Conn) context.Context {
-			return conninfo.Ctx(ctx, conninfo.New())
+			connCtx, cancel := context.WithCancel(ctx)
+			opts.L.DebugContext(ctx, fmt.Sprintf("MCP connection %s started", c.RemoteAddr()))
+
+			ci := conninfo.New()
+
+			go func() {
+				<-connCtx.Done()
+				ci.Close()
+				cancel()
+				opts.L.DebugContext(ctx, fmt.Sprintf("MCP %s finalized", c.RemoteAddr()))
+			}()
+
+			return conninfo.Ctx(connCtx, ci)
 		},
 	}
 
@@ -112,17 +124,6 @@ func (s *Server) Serve(ctx context.Context) error {
 	s.opts.L.InfoContext(ctx, "MCP server stopped")
 
 	return nil
-}
-
-// withConnInfo wraps the next handler with [*conninfo.ConnInfo] context and closes it once the handler is executed.
-func withConnInfo(next server.ToolHandlerFunc) server.ToolHandlerFunc {
-	return func(ctx context.Context, request mcp.CallToolRequest) (*mcp.CallToolResult, error) {
-		connInfo := conninfo.New()
-
-		defer connInfo.Close()
-
-		return next(conninfo.Ctx(ctx, connInfo), request)
-	}
 }
 
 // withLog wraps the next handler with logging of request, response and error.
