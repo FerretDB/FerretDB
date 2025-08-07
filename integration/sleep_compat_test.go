03c188dbd14a55382069afaa80f82a19e83c2a3c
Patryk Kwiatek <patryk.kwiatek@ferretdb.io>
2025-08-07 16:39:55 +0200

    wip

---
+++ integration/sleep_compat_test.go
@@ -0,0 +1,74 @@
+// Copyright 2021 FerretDB Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package integration
+
+import (
+	"testing"
+	"time"
+
+	"github.com/FerretDB/FerretDB/v2/integration/setup"
+	"github.com/FerretDB/FerretDB/v2/integration/shareddata"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+func TestSleepCompat(t *testing.T) {
+	t.Parallel()
+
+	s := setup.SetupCompatWithOpts(t, &setup.SetupCompatOpts{
+		Providers: shareddata.Providers{shareddata.Bools},
+	})
+
+	ctx := s.Ctx
+	targetDB := s.TargetCollections[0].Database().Client().Database("admin")
+	compatDB := s.CompatCollections[0].Database().Client().Database("admin")
+
+	var targetRes, compatRes bson.D
+
+	timeBefore := time.Now()
+	targetErr := targetDB.RunCommand(ctx, bson.D{
+		{"sleep", int32(1)},
+		{"millis", int32(1000)},
+	}).Decode(&targetRes)
+
+	targetDuration := time.Since(timeBefore)
+
+	timeBefore = time.Now()
+	compatErr := compatDB.RunCommand(ctx, bson.D{
+		{"sleep", int32(1)},
+		{"millis", int32(1000)},
+	}).Decode(&compatRes)
+
+	compatDuration := time.Since(timeBefore)
+
+	if targetErr != nil {
+		t.Logf("Target error: %v", targetErr)
+		t.Logf("Compat error: %v", compatErr)
+
+		targetErr = UnsetRaw(t, targetErr)
+		compatErr = UnsetRaw(t, compatErr)
+		assert.Equal(t, compatErr, targetErr)
+		return
+	}
+	require.NoError(t, compatErr, "compat error; target returned no error")
+
+	t.Logf("Compat (expected) result: %v", compatRes)
+	t.Logf("Target (actual)   result: %v", targetRes)
+
+	AssertEqualDocuments(t, compatRes, targetRes)
+
+	assert.InDelta(t, compatDuration.Milliseconds(), targetDuration.Milliseconds(), 100, "Compat and target sleep durations should be approximately equal")
+}
