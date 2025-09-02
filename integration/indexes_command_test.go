38450a8852c2858078ab0679cd4354be13e03715
Patryk Kwiatek <patryk.kwiatek@ferretdb.io>
2025-09-02 18:20:14 +0200

    wip

--- integration/indexes_command_test.go
+++ integration/indexes_command_test.go
@@ -15,7 +15,10 @@
 package integration
 
 import (
+	"context"
+	"errors"
 	"testing"
+	"time"
 
 	"github.com/FerretDB/wire/wirebson"
 	"github.com/stretchr/testify/assert"
@@ -26,6 +29,7 @@ import (
 
 	"github.com/FerretDB/FerretDB/v2/integration/setup"
 	"github.com/FerretDB/FerretDB/v2/integration/shareddata"
+	"github.com/FerretDB/FerretDB/v2/internal/util/ctxutil"
 )
 
 // TestListIndexesCommandNonExistentNS tests that the listIndexes command returns a particular error
@@ -898,4 +902,45 @@ func TestReIndexErrors(t *testing.T) {
 			AssertEqualAltCommandError(t, *tc.err, tc.altMessage, err)
 		})
 	}
+}
+func TestCreateIndexesExpireAfterRemovesExpiredDocs(t *testing.T) {
+	t.Parallel()
+
+	ctx, collection := setup.Setup(t)
+
+	_, err := collection.Indexes().CreateOne(ctx, mongo.IndexModel{
+		Keys:    bson.D{{Key: "expireAt", Value: 1}},
+		Options: options.Index().SetExpireAfterSeconds(1),
+	})
+	require.NoError(t, err)
+
+	expired := bson.D{{Key: "_id", Value: "expired"}, {Key: "expireAt", Value: time.Now().Add(-5 * time.Minute)}}
+	fresh := bson.D{{Key: "_id", Value: "fresh"}, {Key: "expireAt", Value: time.Now().Add(10 * time.Minute)}}
+
+	_, err = collection.InsertMany(ctx, []any{expired, fresh})
+	require.NoError(t, err)
+
+	count, err := collection.CountDocuments(ctx, bson.D{})
+	require.NoError(t, err)
+	require.Equal(t, int64(2), count)
+
+	ctx, cancel := context.WithTimeout(ctx, 90*time.Second)
+	defer cancel()
+
+	for ctx.Err() == nil {
+		err = collection.FindOne(ctx, bson.D{{Key: "_id", Value: "expired"}}).Err()
+		if errors.Is(err, mongo.ErrNoDocuments) {
+			break
+		}
+
+		require.NoError(t, err)
+		ctxutil.Sleep(ctx, 2*time.Second)
+	}
+
+	err = collection.FindOne(ctx, bson.D{{Key: "_id", Value: "expired"}}).Err()
+	require.Error(t, err)
+	require.ErrorIs(t, err, mongo.ErrNoDocuments)
+
+	err = collection.FindOne(ctx, bson.D{{Key: "_id", Value: "fresh"}}).Err()
+	require.NoError(t, err)
 }
