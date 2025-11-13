ebb4d6c9f7de22461fd1b0e8975bf2e4efcec1cb
Alexey Palazhchenko <alexey.palazhchenko@ferretdb.io>
2025-11-13 06:27:26 +0400

    Back to development (#5543)

--- build/version/documentdb.go
+++ build/version/documentdb.go
@@ -18,10 +18,10 @@ import "runtime"
 
 const (
 	// DocumentDB is a version of DocumentDB this version of FerretDB is compatible with.
-	DocumentDB = "0.107.0 gitref: HEAD sha:e63835403d buildId:0"
+	DocumentDB = "0.107.0 gitref: ferretdb sha:e63835403d buildId:0"
 
 	// DocumentDBURL points to the release page of the DocumentDB version above.
-	DocumentDBURL = "https://github.com/FerretDB/documentdb/releases/tag/v0.107.0-ferretdb-2.7.0"
+	DocumentDBURL = "https://github.com/FerretDB/documentdb/releases/tag/v0.108.0-ferretdb-2.8.0"
 )
 
 // DocumentDBSafeToUpdate represents versions of DocumentDB that FerretDB can update.
@@ -32,6 +32,7 @@ var DocumentDBSafeToUpdate = []string{
 	"0.105.0 gitref: HEAD sha:8453d93b buildId:0",   // v2.4.0
 	"0.106.0 gitref: HEAD sha:beb9d25d98 buildId:0", // v2.5.0
 	// FerretDB v2.6.0 wasn't released
+	"0.107.0 gitref: HEAD sha:e63835403d buildId:0", // v2.7.0
 }
 
 // PostgreSQLTest is a version of PostgreSQL used by tests.
